0e5122348ab7be06991bde35a16cdab37d1e7ee1
mike <mike@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-01-01 14:36:29 +0000

    adding the warbler config file and hacking out a line in application.rb - nedss now runs on tomcat
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@96 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/app/controllers/application.rb
+++ nedss-poc/rails/nedss/app/controllers/application.rb
@@ -6,5 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '2d3bed8e7cbfb7957951219c8ef78101'
+  # protect_from_forgery # :secret => '2d3bed8e7cbfb7957951219c8ef78101'
+  # MJH 30-DEC just hacking for now - may be a rails bug - researching
+  # See http://wiki.csinitiative.com/moin/NedssRailsPoc/PackagingAsWarFile
 end
