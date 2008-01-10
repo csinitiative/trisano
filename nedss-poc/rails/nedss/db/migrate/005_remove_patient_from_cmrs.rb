907bd9556db409330277ac7d83c60475ab283894
pete <pete@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-01-10 20:20:48 +0000

    Added self.down reversions
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@106 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/db/migrate/005_remove_patient_from_cmrs.rb
+++ nedss-poc/rails/nedss/db/migrate/005_remove_patient_from_cmrs.rb
@@ -17,5 +17,19 @@ class RemovePatientFromCmrs < ActiveRecord::Migration
   end
 
   def self.down
+    add_column :cmrs, :last_name, :string, :limit => 40
+    add_column :cmrs, :first_name, :string, :limit => 40 
+    add_column :cmrs, :date_of_birth, :date
+    add_column :cmrs, :age, :integer, :limit => 4
+    add_column :cmrs, :street_address, :string, :limit => 100 
+    add_column :cmrs, :city, :string, :limit => 40 
+    add_column :cmrs, :state, :string, :limit => 2 
+    add_column :cmrs, :zip_code, :string, :limit => 5 
+    add_column :cmrs, :country, :string, :limit => 40
+    add_column :cmrs, :county, :string, :limit => 40
+    add_column :cmrs, :phone_number, :string, :limit => 20
+    add_column :cmrs, :gender, :string, :limit => 1
+    add_column :cmrs, :race, :string, :limit => 20
+    add_column :cmrs, :ethnicity, :string, :limit => 20
   end
 end
