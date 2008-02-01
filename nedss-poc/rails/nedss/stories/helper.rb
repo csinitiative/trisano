37d4689f0deb4afe1a63784eb7ff7bac467dd342
ed <ed@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-02-01 22:28:50 +0000

    Adding a simple story test. It's not really an integration test, just a first stab at using RSpec's story test runner
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@214 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/stories/helper.rb
+++ nedss-poc/rails/nedss/stories/helper.rb
@@ -1,3 +1,9 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'spec/rails/story_adapter'
+
+##
+# Run a story file relative to the stories directory
+def run_local_story(filename)
+  run File.join(File.dirname(__FILE__), filename)
+end
