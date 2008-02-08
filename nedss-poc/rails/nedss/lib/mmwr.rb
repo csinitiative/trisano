7591f85f55eac542bd6793fcfa276fb72575499c
mike <mike@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-02-08 23:21:48 +0000

    checking in shim of mmwr class and spec
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@242 b21a84b9-e453-0410-92a3-c4f515f7ae45

---
+++ nedss-poc/rails/nedss/lib/mmwr.rb
@@ -0,0 +1,7 @@
+class Mmwr
+
+  def werks?
+    true
+  end
+
+end
---
+++ nedss-poc/rails/nedss/spec/lib/mmwr_spec.rb
@@ -0,0 +1,14 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Mmwr do
+  before do
+    @mmwr = Mmwr.new
+  end
+
+  it "should be awesome" do
+    @mmwr.should be_werks
+  end
+
+
+end
+
