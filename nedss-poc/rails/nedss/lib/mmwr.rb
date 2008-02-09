916d3d82a2685dc3eaf5936cb0492f21fe2a1a7e
mike <mike@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-02-09 16:06:47 +0000

    adding mmwr epi_date_used
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@244 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/lib/mmwr.rb
+++ nedss-poc/rails/nedss/lib/mmwr.rb
@@ -1,7 +1,29 @@
 class Mmwr
 
-  def werks?
-    true
+  def initialize(epi_dates)
+    @epi_dates = epi_dates
+    #TODO validation of some sort
+  end
+
+  # Returns which epi date was used in calculation
+  # Priority :onsetdate, :diagnosisdate, :labresultdate, :firstreportdate   
+  def epi_date_used
+    if @epi_dates.has_key? :onsetdate
+      :onsetdate
+    elsif @epi_dates.has_key? :diagnosisdate
+      :diagnosisdate
+    elsif @epi_dates.has_key? :labresultdate
+      :labresultdate
+    elsif @epi_dates.has_key? :firstreportdate
+      :firstreportdate
+    else
+      :unknown
+      #TODO raise an error?
+    end
+  end
+
+  def calculation
+    1
   end
 
 end
--- nedss-poc/rails/nedss/spec/lib/mmwr_spec.rb
+++ nedss-poc/rails/nedss/spec/lib/mmwr_spec.rb
@@ -1,14 +1,46 @@
 require File.dirname(__FILE__) + '/../spec_helper'
+require 'date'
 
 describe Mmwr do
   before do
-    @mmwr = Mmwr.new
+
   end
 
-  it "should be awesome" do
-    @mmwr.should be_werks
+  it "should be 1" do
+    epi_dates = { :onsetdate => Time.now }
+    @mmwr = Mmwr.new(epi_dates)
+    @mmwr.calculation.should == 1
   end
 
+  it "should be onsetdate" do
+    epi_dates = { :onsetdate => Time.now}
+    @mmwr = Mmwr.new(epi_dates)
+    @mmwr.epi_date_used.should == :onsetdate
+  end
+
+  it "should be onsetdate not diagnosisdate" do
+    epi_dates = { :onsetdate => Time.now, :diagnosisdate => Time.now }
+    @mmwr = Mmwr.new(epi_dates)
+    @mmwr.epi_date_used.should == :onsetdate
+  end
+
+  it "should be onsetdate" do
+    epi_dates = { :onsetdate => Time.now, :diagnosisdate => Time.now }
+    @mmwr = Mmwr.new(epi_dates)
+    @mmwr.epi_date_used.should == :onsetdate
+  end
+
+  it "should be diagnosisdate" do
+    epi_dates = { :diagnosisdate => Time.now, :labresultdate => Time.now }
+    @mmwr = Mmwr.new(epi_dates)
+    @mmwr.epi_date_used.should == :diagnosisdate
+  end
+
+  it "should be firstreportdate" do
+    epi_dates = { :firstreportdate => Time.now }
+    @mmwr = Mmwr.new(epi_dates)
+    @mmwr.epi_date_used.should == :firstreportdate
+  end
 
 end
 
