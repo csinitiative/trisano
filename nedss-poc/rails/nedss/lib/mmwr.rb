ce8946c843d213066d808ed753808343df68d3ec
mike <mike@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-02-10 00:32:36 +0000

    latest mmwr tests etc.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@246 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/lib/mmwr.rb
+++ nedss-poc/rails/nedss/lib/mmwr.rb
@@ -22,7 +22,19 @@ class Mmwr
     end
   end
 
+  # Business rules for assigning MMWR week
+  # The first day of any MMWR week is Sunday.  MMWR week numbering is sequential beginning with 1 
+  # and incrementing with each week to a maximum of 52 or 53.  MMWR week #1 of an MMWR year is the 
+  # first week of the year that has at least four days in the calendar year.  For example, if 
+  # January 1 occurs on a Sunday, Monday, Tuesday or Wednesday, the calendar week that includes 
+  # January 1 would be MMWR week #1.  If January 1 occurs on a Thursday, Friday, or Saturday, the 
+  # calendar week that includes January 1 would be the last MMWR week of the previous 
+  # year (#52 or #53).  Because of this rule, December 29, 30, and 31 could potentially 
+  # fall into MMWR week #1 of the following MMWR year.   
   def calculation
+    
+    calc_date = @epi_dates[epi_date_used]
+    
     1
   end
 
--- nedss-poc/rails/nedss/spec/lib/mmwr_spec.rb
+++ nedss-poc/rails/nedss/spec/lib/mmwr_spec.rb
@@ -7,40 +7,67 @@ describe Mmwr do
   end
 
   it "should be 1" do
-    epi_dates = { :onsetdate => Time.now }
+    epi_dates = { :onsetdate => DateTime.now }
     @mmwr = Mmwr.new(epi_dates)
     @mmwr.calculation.should == 1
   end
 
   it "should be onsetdate" do
-    epi_dates = { :onsetdate => Time.now}
+    epi_dates = { :onsetdate => DateTime.now}
     @mmwr = Mmwr.new(epi_dates)
     @mmwr.epi_date_used.should == :onsetdate
   end
 
   it "should be onsetdate" do
-    epi_dates = { :onsetdate => Time.now, :diagnosisdate => Time.now }
+    epi_dates = { :onsetdate => DateTime.now, :diagnosisdate => DateTime.now }
     @mmwr = Mmwr.new(epi_dates)
     @mmwr.epi_date_used.should == :onsetdate
   end
 
   it "should be labresultdate" do
-    epi_dates = { :firstreportdate => Time.now, :labresultdate => Time.now }
+    epi_dates = { :firstreportdate => DateTime.now, :labresultdate => DateTime.now }
     @mmwr = Mmwr.new(epi_dates)
     @mmwr.epi_date_used.should == :labresultdate
   end
 
   it "should be diagnosisdate" do
-    epi_dates = { :diagnosisdate => Time.now, :labresultdate => Time.now }
+    epi_dates = { :diagnosisdate => DateTime.now, :labresultdate => DateTime.now }
     @mmwr = Mmwr.new(epi_dates)
     @mmwr.epi_date_used.should == :diagnosisdate
   end
 
   it "should be firstreportdate" do
-    epi_dates = { :firstreportdate => Time.now }
+    epi_dates = { :firstreportdate => DateTime.now }
     @mmwr = Mmwr.new(epi_dates)
     @mmwr.epi_date_used.should == :firstreportdate
   end
 
+  it "should be 1 for Jan 01 2008" do 
+    #pending("get mmwr calculation working for Jan 01 2008")
+    epi_dates = { :onsetdate => DateTime.new(2008, 1, 1) }
+    @mmwr = Mmwr.new(epi_dates)
+    @mmwr.calculation.should == 1
+  end
+
+  it "should be 2 for Jan 06 2008" do
+    pending("get mmwr calculation working for Jan 06 2008")
+    epi_dates = { :onsetdate => DateTime.new(2008, 1, 6) }
+    @mmwr = Mmwr.new(epi_dates)
+    @mmwr.calculation.should == 2
+  end
+
+  it "should be 52 for Dec 31 2008" do 
+    pending("get mmwr calculation working for Dec 31 2008")
+    epi_dates = { :onsetdate => DateTime.new(2008, 12, 31) }
+    @mmwr = Mmwr.new(epi_dates)
+    @mmwr.calculation.should == 52
+  end
+
+  it "should be 52 for Jan 01 2009" do 
+    pending("get mmwr calculation working for Jan 01 2009")
+    epi_dates = { :onsetdate => DateTime.new(2009, 1, 1) }
+    @mmwr = Mmwr.new(epi_dates)
+    @mmwr.calculation.should == 52
+  end
 end
 
