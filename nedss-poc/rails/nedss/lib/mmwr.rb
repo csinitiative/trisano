47507aca69ab1659961f40a7d2c144df30ad101d
mike <mike@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-02-11 23:02:27 +0000

    passing all tests - need more tests and some clean up still
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@250 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/lib/mmwr.rb
+++ nedss-poc/rails/nedss/lib/mmwr.rb
@@ -8,7 +8,39 @@ class Mmwr
     @epi_dates = epi_dates        
     #TODO validation of some sort
     #TODO may want to overload so that you can just give it one date
+    #TODO some methods should be private
   end
+  
+  # Business rules for assigning MMWR week
+  # 
+  # The first day of any MMWR week is Sunday.  MMWR week numbering is sequential beginning with 1 
+  # and incrementing with each week to a maximum of 52 or 53. MMWR week #1 of an MMWR year is the 
+  # first week of the year that has at least four days in the calendar year.  For example, if 
+  # January 1 occurs on a Sunday, Monday, Tuesday or Wednesday, the calendar week that includes 
+  # January 1 would be MMWR week #1. If January 1 occurs on a Thursday, Friday, or Saturday, the 
+  # calendar week that includes January 1 would be the last MMWR week of the previous 
+  # year (52 or 53).  Because of this rule, December 29, 30, and 31 could potentially 
+  # fall into MMWR week #1 of the following MMWR year.   
+  def mmwr_week    
+    mmwr_week_range.mmwr_week.to_i
+  end  
+  
+  # Returns the MmwrDateRange that is in range.
+  def mmwr_week_range
+    calc_date = @epi_dates[epi_date_used]
+    
+    ranges = date_ranges    
+    
+    mmwr_date_range = nil
+    ranges.sort.each { | k, range | 
+      # puts "#{range.to_s} in_range: #{range.in_range(calc_date)}"
+      if range.in_range(calc_date)
+        mmwr_date_range = range
+      end
+    }
+    
+    mmwr_date_range
+  end  
 
   # Returns which epi date was used in calculation
   # Priority :onsetdate, :diagnosisdate, :labresultdate, :firstreportdate   
@@ -22,27 +54,80 @@ class Mmwr
     elsif @epi_dates.has_key? :firstreportdate
       :firstreportdate
     else
-      :unknown
-      #TODO raise an error?
+      raise ArgumentError, "No known date symbol found in @epi_dates #{@epi_dates}"
     end       
   end
+    
+  # Returns the number of MMWR weeks for the given year
+  def mmwr_weeks(year)
+    
+    first_mmwr_week = year_first_mmwr_week(year)    
+    first_day_of_year = year.beginning_of_year   
+    sunday = nil
+    saturday = nil
+    count = 0
+        
+    if first_mmwr_week == :first_week
+      sunday = first_day_of_year - first_day_of_year.wday
+      saturday = sunday + 6
+      count += 1      
+    elsif first_mmwr_week == :second_week
+      sunday = (first_day_of_year - first_day_of_year.wday) + 7
+      saturday = sunday + 6
+      count += 1
+    end
+          
+    until saturday >= DateTime.new(first_day_of_year.year, 12, 31)
+      sunday += 7
+      saturday += 7
+      count += 1
+    end
+    
+    return count
+  end
+  
+  # Returns a symbol indicating whether the MMWR week starts the first week
+  # of the year or the second.
+  def year_first_mmwr_week(*args)
+    
+    year = nil
+    case args.size
+    when 0
+      year = @epi_dates[epi_date_used]
+    when 1
+      year = args[0]
+    else
+      raise ArgumentError, "This method takes 0 or 1 arguments."
+    end
+    
+    if year.wday <= 3
+      return :first_week  
+    else     
+      return :second_week 
+    end
+  end  
+  
+  private
   
   # Creates a DateRange for each MMWR for the year.
-  def date_range
+  def date_ranges
+    # TODO lenght is now off
     date_ranges = Hash.new()
-    calc_date = @epi_dates[epi_date_used]        
+    calc_date = @epi_dates[epi_date_used]
+        
     first_day_of_year = calc_date.beginning_of_year    
-    first_mmwr_week = year_first_mmwr_week    
+    first_mmwr_week = year_first_mmwr_week(calc_date)    
     sunday = nil
     saturday = nil
     if first_mmwr_week == :first_week
       sunday = first_day_of_year - first_day_of_year.wday
       saturday = sunday + 6
-      date_ranges[1] = MmwrDateRange.new("1", sunday.year, sunday, saturday)
+      date_ranges[1] = MmwrDateRange.new(sunday.year, "1", sunday, saturday)
     elsif first_mmwr_week == :second_week
+      date_ranges[0] = last_mmwr_week_previous_year
       sunday = (first_day_of_year - first_day_of_year.wday) + 7
       saturday = sunday + 6
-      date_ranges[1] = MmwrDateRange.new("1", sunday.year, sunday, saturday)      
+      date_ranges[1] = MmwrDateRange.new(sunday.year, "1", sunday, saturday)      
     end
           
     count = 1
@@ -50,56 +135,43 @@ class Mmwr
       sunday += 7
       saturday += 7
       count += 1
-      date_ranges[count] = MmwrDateRange.new(count, sunday.year, sunday, saturday)          
+      date_ranges[count] = MmwrDateRange.new(sunday.year, count, sunday, saturday)          
     end
     
-    # puts "date_ranges"
-    # PP.pp(date_ranges)    
-
     return date_ranges
-  end
-  
-  # Returns a symbol indicating whether the MMWR week starts the first week
-  # of the year or the second.
-  def year_first_mmwr_week
-    calc_date = @epi_dates[epi_date_used]
-    if calc_date.wday <= 3
-      return :first_week  
-    else     
-      return :second_week 
-    end
   end  
-
-  # Business rules for assigning MMWR week
-  # 
-  # The first day of any MMWR week is Sunday.  MMWR week numbering is sequential beginning with 1 
-  # and incrementing with each week to a maximum of 52 or 53. MMWR week #1 of an MMWR year is the 
-  # first week of the year that has at least four days in the calendar year.  For example, if 
-  # January 1 occurs on a Sunday, Monday, Tuesday or Wednesday, the calendar week that includes 
-  # January 1 would be MMWR week #1. If January 1 occurs on a Thursday, Friday, or Saturday, the 
-  # calendar week that includes January 1 would be the last MMWR week of the previous 
-  # year (52 or 53).  Because of this rule, December 29, 30, and 31 could potentially 
-  # fall into MMWR week #1 of the following MMWR year.   
-  def calculation
-    
+  
+  # Returns a MmwrDateRange for the last week of the previous year
+  def last_mmwr_week_previous_year
     calc_date = @epi_dates[epi_date_used]
-    
-    # find date range calc_date is in
-    
-    1
-  end
-
+    prev_year = DateTime.new(calc_date.year - 1, 12, 31)
+    sunday = prev_year - prev_year.wday
+    MmwrDateRange.new(prev_year.year, mmwr_weeks(prev_year), sunday, sunday + 6)      
+  end  
+  
 end
 
 # Contains details on MMWR Week.
 class MmwrDateRange
-  
-  def initialize(mmwr_week, mmwr_year, start_date, end_date)
+  attr_accessor :mmwr_year, :mmwr_week, :start_date, :end_date
+  def initialize(mmwr_year, mmwr_week, start_date, end_date)
+    @mmwr_year = mmwr_year    
     @mmwr_week = mmwr_week
-    @mmwr_year = mmwr_year
     @start_date = start_date
     @end_date = end_date
   end
-  attr_accessor :mmwr_week, :mmwr_year, :start_date, :end_date
+  
+  # Checks if provided date is in range of start_date and end_date.
+  def in_range(calc_date)
+    if calc_date >= start_date && calc_date <= end_date
+      return true
+    else
+      return false
+    end
+  end
+  
+  def to_s
+    "MMWR Year: #{@mmwr_year} MMWR Week: #{@mmwr_week} Start Date: #{@start_date} End Date #{@end_date}"        
+  end
   
 end
--- nedss-poc/rails/nedss/spec/lib/mmwr_spec.rb
+++ nedss-poc/rails/nedss/spec/lib/mmwr_spec.rb
@@ -6,13 +6,6 @@ describe Mmwr do
 
   end
 
-  it "should be 1" do
-    pending("should be 1")
-    epi_dates = { :onsetdate => DateTime.now }
-    @mmwr = Mmwr.new(epi_dates)
-    @mmwr.calculation.should == 1
-  end
-
   it "should be onsetdate" do
     epi_dates = { :onsetdate => DateTime.now}
     @mmwr = Mmwr.new(epi_dates)
@@ -53,47 +46,34 @@ describe Mmwr do
     epi_dates = { :onsetdate => DateTime.new(2009, 1, 1) }
     @mmwr = Mmwr.new(epi_dates)
     @mmwr.year_first_mmwr_week.should == :second_week
-  end  
-  
-  it "should be 53 MMWR weeks for 2008" do
-    epi_dates = { :onsetdate => DateTime.new(2008, 1, 1) }
-    @mmwr = Mmwr.new(epi_dates)
-    @mmwr.date_range.length.should eql(53) 
-  end    
-  
-  it "should be 52 MMWR weeks for 2009" do
-    epi_dates = { :onsetdate => DateTime.new(2009, 1, 1) }
-    @mmwr = Mmwr.new(epi_dates)
-    @mmwr.date_range.length.should eql(52) 
-  end    
+  end      
 
   it "should be 1 for Jan 01 2008" do 
-    pending("get mmwr calculation working for Jan 01 2008")
     epi_dates = { :onsetdate => DateTime.new(2008, 1, 1) }
     @mmwr = Mmwr.new(epi_dates)
-    @mmwr.calculation.should == 1
+    @mmwr.mmwr_week.should == 1
   end
 
   it "should be 2 for Jan 06 2008" do
-    pending("get mmwr calculation working for Jan 06 2008")
     epi_dates = { :onsetdate => DateTime.new(2008, 1, 6) }
     @mmwr = Mmwr.new(epi_dates)
-    @mmwr.calculation.should == 2
+    @mmwr.mmwr_week.should == 2
   end
 
-  it "should be 52 for Dec 31 2008" do 
-    pending("get mmwr calculation working for Dec 31 2008")
+  it "should be 53 for Dec 31 2008" do 
     epi_dates = { :onsetdate => DateTime.new(2008, 12, 31) }
     @mmwr = Mmwr.new(epi_dates)
-    @mmwr.calculation.should == 52
+    @mmwr.mmwr_week.should == 53
   end
 
-  it "should be 52 for Jan 01 2009" do 
-    pending("get mmwr calculation working for Jan 01 2009")
+  it "should be year 2008 week 53 for Jan 01 2009" do     
     epi_dates = { :onsetdate => DateTime.new(2009, 1, 1) }
     @mmwr = Mmwr.new(epi_dates)
-    @mmwr.epi_date_used.should == :onsetdate
-    @mmwr.calculation.should == 52
+    mmwr_week_range = @mmwr.mmwr_week_range
+    year = mmwr_week_range.mmwr_year.to_i
+    year.should == 2008
+    week = mmwr_week_range.mmwr_week.to_i
+    week.should == 53
   end
 end
 
