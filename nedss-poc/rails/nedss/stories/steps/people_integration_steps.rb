3bd87459e70b35e309dfa23b7537b1ddd88a341b
ed <ed@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-02-11 23:31:48 +0000

    Adding integration and uat story directories; starting work on the people integration test.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@251 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/stories/steps/people_integration_steps.rb
+++ nedss-poc/rails/nedss/stories/steps/people_integration_steps.rb
@@ -1,16 +1,41 @@
 require 'mechanize'
 
 steps_for(:people_integration) do
-
+  
+  
   When("I view the people listing") do
     agent = WWW::Mechanize.new
-    @page = agent.get 'http://localhost:3000/people'
-    puts @page.title
-    pending("Really implement this")
+    @page = agent.get NEDSS_URL + "/people"
+  end
+  
+  Then("the page title should be '$page_title'") do |page_title|
+    @page.title.should eql(page_title)
   end
 
   Then("there should be a list of people") do
-    pending("Really implement this")
+    @page.links.text("Show").should have_at_least(2).items
   end
+  
+  Then("there should be a link to create a new person") do
+    @page.links.text("New person").should have_at_least(1).items
+  end
+  
+  When("I click the new person link") do
+    @page = @page.links.text("New person").first.click
+  end
+  
+  Then("I should be taken to the new person page") do
+    # Is there a good way to get at the URL? Seems fragile to go after a header
+    # to determine the page we're on.
+    @page.search("//h1").inner_text.should eql("New person")
+  end
+  
+#
+#  Given that I can enter a first name of 'Bob'
+#  And that I can enter a last name of 'Ford'
+#    
+#  When I click the create button
+#
+#  Then a new person should be created
 
 end
