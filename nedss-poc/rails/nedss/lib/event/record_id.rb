6aa6b53d9eb333a7b350cd16edb4990e91051c3a
mike <mike@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-02-15 05:44:50 +0000

    record id impl with passing tests
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@276 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/lib/event/record_id.rb
+++ nedss-poc/rails/nedss/lib/event/record_id.rb
@@ -20,8 +20,63 @@
 # 2008000005 - 5th NEDSS record entered in 2008; record entered directly into NEDSS
 # 2008900005 - 5th NETSS record entered in 2008; NETSS number 0005
 #
-class RecordId
-  def initialize(*args)
+
+class RecordId    
+
+  def initialize(*args)    
     
+    case args.size
+        
+    when 1
+      raise ArgumentError, "RecordId: Expected Integer" if !args[0].is_a?(Integer)
+      @count = args[0]
+      t = Time.now
+      @year = Date.new(t.year, t.month, t.day)
+      @system = :nedss
+    when 2
+      raise ArgumentError, "RecordId: Expected Date" if !args[0].is_a?(Date)
+      raise ArgumentError, "RecordId: Expected Integer" if !args[1].is_a?(Integer)
+      @year = args[0]
+      @count = args[1]
+      @system = :nedss
+    when 3
+      raise ArgumentError, "RecordId: Expected Date" if !args[0].is_a?(Date)
+      raise ArgumentError, "RecordId: Expected Integer" if !args[1].is_a?(Integer)
+      raise ArgumentError, "RecordId: Expected Symbol" if !args[2].is_a?(Symbol)
+      @year = args[0]
+      @count = args[1]      
+      @system = args[2]
+    else
+      raise ArgumentError, "RecordId initialize takes 1, 2, or 3 arguments."
+    end
+    
+  end
+  
+  def value    
+    val = @year.year.to_s + system_number
+    val +=  converted_count    
+  end
+
+  private 
+  
+  def system_number    
+    case @system
+    when :netss
+      "9"
+    when :tims
+      "8"
+    when :stdmis
+      "7"
+    when :hars
+      "6"
+    when :nedss
+      "5"
+    else
+      raise "Unknown @system: #{@system}"
+    end
+  end
+  
+  def converted_count
+    @count.to_s.rjust(5, "0")
   end
 end
--- nedss-poc/rails/nedss/spec/lib/event/record_id_spec.rb
+++ nedss-poc/rails/nedss/spec/lib/event/record_id_spec.rb
@@ -2,51 +2,51 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe RecordId do
   
-  it "should be 2007900032 for the 32nd NETSS record entered in 2007" do
-    pending
-    RecordId.new(Date.new(2007, 1, 1), 32, :netss).value.should == 2007900032
+  it "should handle bogus constructor args" do
+    lambda {RecordId.new}.should raise_error(ArgumentError, "RecordId initialize takes 1, 2, or 3 arguments.")
+    lambda {RecordId.new(1, 2, 3, 4)}.should raise_error(ArgumentError, "RecordId initialize takes 1, 2, or 3 arguments.")    
+    lambda {RecordId.new(String.new)}.should raise_error(ArgumentError, "RecordId: Expected Integer")
+    lambda {RecordId.new(String.new, 1)}.should raise_error(ArgumentError, "RecordId: Expected Date")
+    lambda {RecordId.new(Date.new(2007, 1, 1), String.new)}.should raise_error(ArgumentError, "RecordId: Expected Integer")
+    lambda {RecordId.new(Date.new(2007, 1, 1), 1, String.new)}.should raise_error(ArgumentError, "RecordId: Expected Symbol")
+
+  end  
+  
+  it "should be 2007900032 for the 32nd NETSS record entered in 2007" do        
+    RecordId.new(Date.new(2007, 1, 1), 32, :netss).value.should == "2007900032"    
   end
   
-  it "should be 2008000005 for the 5th record entered directly into NEDSS in 2008" do
-    pending
-    RecordId.new(5).value.should == 2008000005
+  it "should be 2008500005 for the 5th record entered directly into NEDSS in 2008" do    
+    RecordId.new(5).value.should == "2008500005"
   end
   
-  it "should be 2009000005 for the 5th record entered directly into NEDSS in 2009" do
-    pending
-    RecordId.new(Date.new(2009, 1, 1), 5).value.should == 2009000005
+  it "should be 2009500005 for the 5th record entered directly into NEDSS in 2009" do
+    RecordId.new(Date.new(2009, 1, 1), 5).value.should == "2009500005"
   end  
   
   it "should be 2050000001 for the 1st record entered directly into NEDSS in 2050" do
-    pending
-    RecordId.new(Date.new(2050, 1, 1), 1).value.should == 2050000001
+    RecordId.new(Date.new(2050, 1, 1), 1).value.should == "2050500001"
   end    
   
   it "should be 2008599999  for the 599,999th record entered directly into NEDSS in 2008" do
-    pending
-    RecordId.new(Date.new(2008, 1, 1), 99999).value.should == 2008599999
+    RecordId.new(Date.new(2008, 1, 1), 99999).value.should == "2008599999"
   end  
   
   it "should be 2008900005 for the 5th NETSS record entered in 2008" do
-    pending
-    RecordId.new(Date.new(2008, 1, 1), 5).value.should == 2008900005
+    RecordId.new(Date.new(2008, 1, 1), 5, :netss).value.should == "2008900005"
   end
   
   it "should be 2002899999 for the 99,999th TIMS record entered in 2002" do
-    pending
-    RecordId.new(Date.new(2002, 1, 1), 99999, :tims).value.should == 2002899999
+    RecordId.new(Date.new(2002, 1, 1), 99999, :tims).value.should == "2002899999"
   end
   
   it "should be 2002712345 for the 12,345 STD_MIS record entered in 2002" do
-    pending
-    RecordId.new(Date.new(2002, 1, 1), 12345, :std_mis).value.should == 2002712345
+    RecordId.new(Date.new(2002, 1, 1), 12345, :stdmis).value.should == "2002712345"
   end
 
   it "should be 1999654321 for the 54,321th HARS record entered in 1999" do
-    pending
-    RecordId.new(Date.new(1999, 1, 1), 54321, :hars).value.should == 1999654321
+    RecordId.new(Date.new(1999, 1, 1), 54321, :hars).value.should == "1999654321"
   end
-
 
 end
 
