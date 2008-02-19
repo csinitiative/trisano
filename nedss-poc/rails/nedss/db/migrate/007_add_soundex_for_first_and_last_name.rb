bcb513041be867f5f0b1b396dd6998013d18d86b
ed <ed@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-02-19 05:41:34 +0000

    Removing type information from remove_column calls
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@298 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/db/migrate/007_add_soundex_for_first_and_last_name.rb
+++ nedss-poc/rails/nedss/db/migrate/007_add_soundex_for_first_and_last_name.rb
@@ -5,7 +5,7 @@ class AddSoundexForFirstAndLastName < ActiveRecord::Migration
   end
 
   def self.down
-    remove_column :people, :first_name_soundex, :string
-    remove_column:people, :last_name_soundex, :string
+    remove_column :people, :first_name_soundex
+    remove_column:people, :last_name_soundex
   end
 end
