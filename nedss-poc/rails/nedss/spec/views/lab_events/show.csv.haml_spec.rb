d5ffdd254853b9cd26059218399af25442ce60de
claym <claym@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-02-19 21:02:33 +0000

    Modified show for lab event csv to properly handle the event_status code.  Added tests for show.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@307 b21a84b9-e453-0410-92a3-c4f515f7ae45

---
+++ nedss-poc/rails/nedss/spec/views/lab_events/show.csv.haml_spec.rb
@@ -0,0 +1,31 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "/lab_events/show.csv.haml" do
+  before(:each) do
+    lab_event_98 = mock_model(LabEvent, :id => 1, :event_name => 'Test', :event_onset_date => '2008-02-07', :event_status => 1901)
+    @event_status = mock_model(Code)
+    @event_status.stub!(:code_description).and_return('Open')
+    lab_event_98.stub!(:event_status).and_return(@event_status)
+    assigns[:lab_event] = lab_event_98
+  end
+
+  it "should render a csv template of the lab_events" do
+    render "/lab_events/show.csv.haml"
+  end
+
+  it "should render a csv in event_name,event_onset_date,event_status for a Test event" do
+    render "/lab_events/show.csv.haml"
+    response.should have_text(/Test,2008-02-07,Open$/)
+  end
+
+  it "should render a header column for with event_name,event_onset_date,event_status" do
+    render "/lab_events/show.csv.haml"
+    response.should have_text(/^event_name,event_onset_date,event_status/)
+  end
+
+  it "should render a csv in event_name,event_onset_date,event_status format" do
+    render "/lab_events/show.csv.haml"
+    response.should have_text("event_name,event_onset_date,event_status\nTest,2008-02-07,Open\n")
+  end
+
+end
