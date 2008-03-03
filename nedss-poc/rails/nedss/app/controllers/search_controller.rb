5ca4e27aadad3fd9c18dcc503c483e7164ac7760
ed <ed@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-03-03 02:13:33 +0000

    Simple tweaks to knock out minor debt: Search errors were displaying in green font, not red; moved the error message from the notice to the error portion of the flash.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@328 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/app/controllers/search_controller.rb
+++ nedss-poc/rails/nedss/app/controllers/search_controller.rb
@@ -1,21 +1,23 @@
 class SearchController < ApplicationController
-  
+
   def index
-   
+
     @people = []
-    
-    # Yes, last minute kludge. Where does the error go when you don't have a model?
-    flash[:notice] =""
+
+    # Is there some more elegant way to do this when you don't want the message hanging around?
+    flash[:error] = ""
     
     begin
       if !params[:name].blank? || !params[:birth_date].blank?
         @people = Person.find_by_ts(:fulltext_terms => params[:name], :birth_date => params[:birth_date])
       end
     rescue
-      # Debt? This is an error. Is there someplace better to stuff it?
-      flash[:notice] = "There was a problem with your search criteria. Please try again."
+      flash[:error] = "There was a problem with your search criteria. Please try again."
     end
-    
+        
   end
-  
+    
 end
+
+
+
