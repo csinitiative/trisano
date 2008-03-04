25a42ea5e2f7c03930b98beddd2f1adb0b7af600
ed <ed@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-03-04 23:40:52 +0000

    Converting existing search UAT stories from using Mechanize to exercising the Rails framework internally. The story text is still goofy without getting scoping going. Perhaps circle back later.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@346 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/stories/integration/steps/search_uat_steps.rb
+++ nedss-poc/rails/nedss/stories/integration/steps/search_uat_steps.rb
@@ -4,66 +4,57 @@ steps_for(:search_uat) do
   
     # This was done at great speed. Debt: 
     #   DRY this up.
-    #     Newing agent every time
     #   Figure out the scoping by scenarios
     #     So the whens/thens can be reused
     #     And story lines don't have to be unique
     
     When("I search for the known person '$person'") do |person|
-      agent = WWW::Mechanize.new
-      @page = agent.get NEDSS_URL + "/nedss/search/people?name=#{person}"
+      get "/search/people?name=#{person}"
     end
   
-    Then("known person '$person' should appear in the search results.") do |person|
-      @page.links.text("#{person}").should have_at_least(1).items
+    Then("known person '$person' should appear in the search results.") do |person|  
+      response.should have_text(/#{person}/)
     end
     
     When("I search for the non-existent person '$person'") do |person|
-      agent = WWW::Mechanize.new
-      @page = agent.get NEDSS_URL + "/nedss/search/people?name=#{person}"
+      get "/search/people?name=#{person}"
     end
-  
+    
     Then("non-existent person '$person' should not be returned.") do |person|
-      @page.links.text("#{person}").should have(0).items
+      response.should have_text(/no results/)
     end
     
     When("I search with the mispelled name '$name'") do |name|
-      agent = WWW::Mechanize.new
-      @page = agent.get NEDSS_URL + "/nedss/search/people?name=#{name}"
+      get "/search/people?name=#{name}"
     end
   
     Then("the correctly spelled name of '$person' should appear in the search results.") do |name|
-      @page.links.text("#{name}").should have_at_least(1).items
+      response.should have_text(/#{name}/)
     end
     
     When("I search with the transposed names '$name'") do |name|
-      agent = WWW::Mechanize.new
-      @page = agent.get NEDSS_URL + "/nedss/search/people?name=#{name}"
+      get "/search/people?name=#{name}"
     end
   
     Then("the non-tranposed name of '$name' should appear in the search results.") do |name|
-      @page.links.text("#{name}").should have_at_least(1).items
+      response.should have_text(/#{name}/)
     end
     
     When("I search for the person by birthdate '$person' by the correct birthdate of '$birthdate'") do |person, birthdate|
-      agent = WWW::Mechanize.new
-      @page = agent.get NEDSS_URL + "/nedss/search/people?name=#{person}&birth_date=#{birthdate}"
+        get "/search/people?name=#{person}&birth_date=#{birthdate}"
     end
   
-    # Scrape the birthday out, too
     Then("known person by birthdate '$person' with correct birthdate '$birthdate' should appear in the search results.") do |person, birthdate|
-      @page.links.text("#{person}").should have_at_least(1).items
+      response.should have_text(/#{person}/) 
+      response.should have_text(/#{birthdate}/)
     end
-
     
-#  Scenario: Search for users by incorrect birthdate
-#
-#    When I search for the person 'Groucho Marx' by the incorrect birthdate of 'incorrect birthdate'
-#    Then 'Groucho Marx' should not appear in the search results.
-#  
+    When("I search for the person by correct name '$person' but the incorrect birthdate of '$birthdate'") do |person, birthdate|
+        get "/search/people?name=#{person}&birth_date=#{birthdate}"
+    end
   
-  
-  
-  
+    Then("known person by incorrect birthdate '$person' should not appear in the search results.") do |person|
+      response.should have_text(/no results/)
+    end
     
 end
