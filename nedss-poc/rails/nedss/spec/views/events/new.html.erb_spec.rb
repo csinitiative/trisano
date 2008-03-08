e292a05952f32767d1fb8b4c4313545f8dd2bd92
pete <pete@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-03-08 18:21:35 +0000

    Fix failing tests
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@368 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/spec/views/events/new.html.erb_spec.rb
+++ nedss-poc/rails/nedss/spec/views/events/new.html.erb_spec.rb
@@ -8,15 +8,23 @@ describe "/cmrs/new.html.erb" do
     
     @participation = mock_model(Participation)
     @primary_entity =  mock_person_entity
+    @secondary_entity =  mock_person_entity
     @active_jurisdiction = mock_model(Participation)
+    @active_reporting_agency = mock_model(Participation)
     @active_hospital = mock_model(Participation)
     @hospitals_participation = mock_model(HospitalsParticipation)
+    @place = mock_model(Place)
     
     @event.stub!(:active_patient).and_return(@participation)
+    @event.stub!(:active_reporting_agency).and_return(@active_reporting_agency)
     @event.stub!(:active_jurisdiction).and_return(@active_jurisdiction)
     @event.stub!(:active_hospital).and_return(@active_hospital)
+    @place.stub!(:name).and_return("Joe's Lab")
+    @place.stub!(:entity_id).and_return(1)
     @participation.stub!(:active_primary_entity).and_return(@primary_entity)
     @active_jurisdiction.stub!(:secondary_entity_id).and_return(1)
+    @active_reporting_agency.stub!(:active_secondary_entity).and_return(@secondary_entity)
+    @secondary_entity.stub!(:place).and_return(@place)
     @active_hospital.stub!(:secondary_entity_id).and_return(13)
     @active_hospital.stub!(:hospitals_participation).and_return(@hospitals_participation)
     @hospitals_participation.stub!(:admission_date).and_return("2008-02-15")
