03aed42f65be9016e1ab46a9c5fac172afce7f06
ed <ed@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-03-11 22:42:52 +0000

    Adding nil checks to new finders, tyring to get specs to load for the build
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@386 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/app/models/code.rb
+++ nedss-poc/rails/nedss/app/models/code.rb
@@ -5,14 +5,17 @@ class Code < ActiveRecord::Base
   end
 
   def self.yes_id
-   find(:first, :conditions => "code_name = 'yesno' and the_code = 'Y'").id
+   code = find(:first, :conditions => "code_name = 'yesno' and the_code = 'Y'")
+   code.id unless code.nil?
   end
 
   def self.other_place_type_id
-   find(:first, :conditions => "code_name = 'placetype' and the_code = 'O'").id
+   code = find(:first, :conditions => "code_name = 'placetype' and the_code = 'O'")
+   code.id unless code.nil?
   end
 
   def self.unspecified_location_id
-   find(:first, :conditions => "code_name = 'location' and the_code = 'U'").id
+   code = find(:first, :conditions => "code_name = 'location' and the_code = 'U'")
+   code.id unless code.nil?
   end
 end
