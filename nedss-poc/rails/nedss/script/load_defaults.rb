6637cfb88c6411337ab61ef555a75024bde1faf0
pete <pete@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-03-19 17:05:16 +0000

    Adds hospitals support to db populator
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@432 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/script/load_defaults.rb
+++ nedss-poc/rails/nedss/script/load_defaults.rb
@@ -12,14 +12,27 @@ Code.transaction do
   end
 end
 
-# Diseases represented as an array of strings
+# Diseases are represented as an array of strings
 
 diseases = YAML::load_file "#{RAILS_ROOT}/db/defaults/diseases.yml"
 Disease.transaction do
   diseases.each do |disease|
-    p disease
     d = Disease.find_or_initialize_by_disease_name(:disease_name => disease)
-    d.disease_name = disease unless d.new_record?
-    d.save!
+    d.save! if d.new_record?
+  end
+end
+
+# Hospitals are represented as an array of strings
+
+hospitals = YAML::load_file "#{RAILS_ROOT}/db/defaults/hospitals.yml"
+Entity.transaction do
+  hospitals.each do |hospital|
+    hospital_type_id = Code.find_by_code_name_and_the_code("placetype", "H").id
+    h = Entity.find(:first, 
+                    :include => :places, 
+                    :select => "places.name", 
+                    :conditions => ["entities.entity_type = 'place' and places.place_type_id = ? and places.name = ?",
+                      hospital_type_id, hospital])
+    Entity.create(:entity_type => 'place', :place => {:name => hospital, :place_type_id => hospital_type_id}) if h.nil?
   end
 end
