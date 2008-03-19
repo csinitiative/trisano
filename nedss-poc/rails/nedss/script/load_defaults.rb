a530bf8c591e048a93ef0bb16d5d39906f513534
pete <pete@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-03-19 16:06:03 +0000

    Adds support for populating diseases table
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@431 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/script/load_defaults.rb
+++ nedss-poc/rails/nedss/script/load_defaults.rb
@@ -1,13 +1,25 @@
-p RAILS_ROOT
+# Codes represented as an array of hashes
 codes = YAML::load_file "#{RAILS_ROOT}/db/defaults/codes.yml"
 
-# Can't simply delete and replace as it will trigger a FK constraint
+# Can't simply delete all and insert as the delete may trigger a FK constraint
 Code.transaction do
   codes.each do |code|
-    c = Code.find_or_initialize_by_code_name_and_the_code(:code_name => code['code_name'], :the_code => code['the_code'], :code_description => code['code_description'])
+    c = Code.find_or_initialize_by_code_name_and_the_code(:code_name => code['code_name'], 
+                                                          :the_code => code['the_code'], 
+                                                          :code_description => code['code_description'])
     c.attributes = code unless c.new_record?
     c.save!
   end
 end
 
-p Code.find(:all)
+# Diseases represented as an array of strings
+
+diseases = YAML::load_file "#{RAILS_ROOT}/db/defaults/diseases.yml"
+Disease.transaction do
+  diseases.each do |disease|
+    p disease
+    d = Disease.find_or_initialize_by_disease_name(:disease_name => disease)
+    d.disease_name = disease unless d.new_record?
+    d.save!
+  end
+end
