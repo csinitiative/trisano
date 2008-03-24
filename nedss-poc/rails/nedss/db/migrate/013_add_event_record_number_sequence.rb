d56c6812cdfe70ab1e9b250487cc4dca8b57a50b
mike <mike@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-03-24 21:33:59 +0000

    donuts
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@455 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/db/migrate/013_add_event_record_number_sequence.rb
+++ nedss-poc/rails/nedss/db/migrate/013_add_event_record_number_sequence.rb
@@ -1,7 +1,7 @@
 class AddEventRecordNumberSequence < ActiveRecord::Migration
   
   def self.up
-    execute "CREATE SEQUENCE events_record_number_seq INCREMENT 1 START 2008000001 MINVALUE 2008500001 MAXVALUE 2008999999 CACHE 1;"
+    execute "CREATE SEQUENCE events_record_number_seq INCREMENT 1 START 2008000001 MINVALUE 2008000001 MAXVALUE 2008999999 CACHE 1;"
   end
   
   def self.down
