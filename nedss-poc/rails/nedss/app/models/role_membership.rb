b0a47dfada4c7918d57fb3d8adf2b7e5171011f6
ed <ed@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-03-25 21:17:25 +0000

    Minor tweak to role membership validation
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@465 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/app/models/role_membership.rb
+++ nedss-poc/rails/nedss/app/models/role_membership.rb
@@ -6,7 +6,7 @@ class RoleMembership < ActiveRecord::Base
   belongs_to :jurisdiction, :class_name => 'Entity', :foreign_key => :jurisdiction_id
   
    validates_uniqueness_of :user_id, :scope => [:role_id, :jurisdiction_id],
-     :message => "duplicate role membership is not permitted"
+     :message => "duplicate role membership is not permitted", :on => :create
   
   attr_accessor :should_destroy
   
