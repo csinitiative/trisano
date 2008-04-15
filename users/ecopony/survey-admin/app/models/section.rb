18c3008fb6461d4f2e61fd69e445b672acb90e18
ed <ed@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-04-15 17:40:44 +0000

    Fixing scoping on sections acts_as_list behavior, adding section list to the form builder view
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@564 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- users/ecopony/survey-admin/app/models/section.rb
+++ users/ecopony/survey-admin/app/models/section.rb
@@ -1,7 +1,7 @@
 class Section < ActiveRecord::Base
   belongs_to :form
   
-  acts_as_list :scope => "form_id"
+  acts_as_list :scope => :form
   
    validates_presence_of :name
 end
--- users/ecopony/survey-admin/spec/views/forms/edit.html.erb_spec.rb
+++ users/ecopony/survey-admin/spec/views/forms/edit.html.erb_spec.rb
@@ -4,12 +4,17 @@ describe "/forms/edit.html.erb" do
   include FormsHelper
   
   before do
+    
+    section = mock_model(Section)
+    section.stub!(:name).and_return("Tummy Ache Demographics")
+    
     @form = mock_model(Form)
     @form.stub!(:name).and_return("MyString")
     @form.stub!(:description).and_return("MyString")
     @form.stub!(:jurisdiction_id).and_return("1")
     @form.stub!(:disease_id).and_return("1")
     @form.stub!(:status).and_return("1")
+    @form.stub!(:sections).and_return([section])
     assigns[:form] = @form
   end
 
