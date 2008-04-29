e488aa55e89cddd60d7d065b5733b69e488d668f
dave <dave@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-04-29 13:02:14 +0000

    Created one RSpec test for R1 and started outlining tests for R2
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@647 b21a84b9-e453-0410-92a3-c4f515f7ae45

---
+++ nedss-poc/rails/nedss/test/spec/CreatingQuestions.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe "Administrator functionality to create questions" do
+  
+  it "An adminstrator should be able to create a questions with a free-text answer"
+  end
+  
+  it "An adminstrator should be able to create a question with answers limited to a finites set of single-selectable values to be chosen from a drop-down list"
+  end
+  
+  it "An adminstrator should be able to create a question with answers limited to a finite set of muli-selectable values to be chosen using checkboxes"
+  end
+  
+  it "An administrator should be able to create a question where the answer is limited to a valid date"
+  end
+
+  it "An adminstrator should be able to create a question where the answer is limited to a valid phone number"
+  end
+  
+end
---
+++ nedss-poc/rails/nedss/test/spec/CreatingSections.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe "Administrator functionality for creating sections from selected questions" do
+
+it "An administrator can create a section"
+end
+
+it "An administrator can identify a set of questions to include in a section"
+end
+
+it "An administrator can specify the order of those questions"
+end
+
