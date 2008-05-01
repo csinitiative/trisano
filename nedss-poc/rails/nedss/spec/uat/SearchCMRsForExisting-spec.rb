f0e414d6375674b9f3a11174a3bb4cdc6d094c96
dave <dave@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-05-01 13:17:42 +0000

    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@667 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/spec/uat/SearchCMRsForExisting-spec.rb
+++ nedss-poc/rails/nedss/spec/uat/SearchCMRsForExisting-spec.rb
@@ -17,7 +17,7 @@ describe 'User functionality for searching for existing users' do
     @browser.click('link=People Search')
     @browser.wait_for_page_to_load('30000') 
     @browser.type('name', 'Smoker')
-    @browser.click('event_submit')
+    @browser.click('//input[@type=\'submit\']')
     @browser.wait_for_page_to_load('30000') 
     @browser.is_text_present('Smoker, Steve').should be_true
   end
@@ -26,35 +26,35 @@ describe 'User functionality for searching for existing users' do
     @browser.click('link=CMR Search')
     @browser.wait_for_page_to_load('30000')
     @browser.type('name', 'Stephen Smoker') 
-    @browser.click('event_submit')
+    @browser.click('//input[@type=\'submit\']')
     @browser.wait_for_page_to_load('30000')
     @browser.is_text_present('Smoker, Steve').should be_true
   end
   
   it 'should find a person named Steve Smoker when searching by Stephen Smooker' do
     @browser.type('name', 'Stephen Smooker')
-    @browser.click('event_submit')
+    @browser.click('//input[@type=\'submit\']')
     @browser.wait_for_page_to_load('30000')
     @browser.is_text_present('Smoker, Steve').should be_true
   end
   
   it 'should not find anyone when searching by Stephen Smokesalot' do
     @browser.type('name', 'Stephen Smokesalot')
-    @browser.click('event_submit')
+    @browser.click('//input[@type=\'submit\']')
     @browser.wait_for_page_to_load('30000')
     @browser.is_text_present('Your search returned no results.').should be_true
   end
     
   it 'should not find anyone when searching by Stephen' do
     @browser.type('name', 'Stephen')
-    @browser.click('event_submit')
+    @browser.click('//input[@type=\'submit\']')
     @browser.wait_for_page_to_load('30000')
     @browser.is_text_present('Your search returned no results.').should be_true
   end
   
   it 'should find a person named Steve Smoker when searching by Steve' do
     @browser.type('name', 'Steve')
-    @browser.click('event_submit')
+    @browser.click('//input[@type=\'submit\']')
     @browser.wait_for_page_to_load('30000')
     @browser.is_text_present('Smoker, Steve').should be_true
   end
@@ -62,7 +62,7 @@ describe 'User functionality for searching for existing users' do
   it 'should not find anyone when searching by first name smo' do
     @browser.type('name', '')
     @browser.type 'sw_first_name', 'smo'
-    @browser.click('event_submit')
+    @browser.click('//input[@type=\'submit\']')
     @browser.wait_for_page_to_load('30000')
     @browser.is_text_present('Your search returned no results.').should be_true
   end
@@ -70,7 +70,7 @@ describe 'User functionality for searching for existing users' do
   it 'should find a person named Steve Smoker when searching by last name smo' do
     @browser.type('sw_first_name', '')
     @browser.type('sw_last_name', 'smo')
-    @browser.click('event_submit')
+    @browser.click('//input[@type=\'submit\']')
     @browser.wait_for_page_to_load('30000')
     @browser.is_text_present('Smoker, Steve').should be_true
   end
@@ -82,7 +82,7 @@ describe 'User functionality for searching for existing users' do
 
   it 'should find Steve Smoker when searching by Bear River jurisdiction' do  
     @browser.select('jurisdiction_id', 'label=Bear River Health Department')
-    @browser.click('event_submit')
+    @browser.click('//input[@type=\'submit\']')
     @browser.wait_for_page_to_load('30000')
     @browser.is_text_present('Bear River Health Department').should be_true
   end
--- nedss-poc/rails/nedss/spec/uat/test.rb
+++ nedss-poc/rails/nedss/spec/uat/test.rb
@@ -6,10 +6,12 @@ describe 'User functionality for searching for existing users' do
     # The @browser is initialised in spec_helper.rb
     @browser.open('http://utah:arches@ut-nedss-dev.csinitiative.com')
   end
-  it "should find a person named Steve Smoker when viewing all CMRs" do
-    @browser.open('/nedss/')
-    @browser.click('link=View CMRs')
-    @browser.wait_for_page_to_load('30000')
+  it 'should find a person named Steve Smoker when searching by Smoker' do
+    @browser.click('link=People Search')
+    @browser.wait_for_page_to_load('30000') 
+    @browser.type('name', 'Smoker')
+    @browser.click('//input[@type=\'submit\']')
+    @browser.wait_for_page_to_load('30000') 
     @browser.is_text_present('Smoker, Steve').should be_true
   end
   
