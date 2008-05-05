b45ca739b0074b76cc75259d7a65ee6ff4005424
claym <claym@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-05-05 19:58:11 +0000

    Initial commit of init.rb and default behavior for update_attributes
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@681 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/vendor/plugins/acts_as_auditable/lib/acts_as_auditable.rb
+++ nedss-poc/rails/nedss/vendor/plugins/acts_as_auditable/lib/acts_as_auditable.rb
@@ -1,1 +1,10 @@
 # ActsAsAuditable
+
+module ActsAsAuditable
+    class ActiveRecord
+        def update_attributes(attributes)
+            self.attributes = attributes
+	    save
+	end
+    end
+end
