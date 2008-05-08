55d04bcd5db0006a5470532bd3eb61f4aa006521
ed <ed@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-05-08 21:05:13 +0000

    Adding admin-side UAT for publishing.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@708 b21a84b9-e453-0410-92a3-c4f515f7ae45

---
+++ nedss-poc/rails/nedss/spec/uat/FormBuilderAdminPublish.rb
@@ -0,0 +1,57 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe 'Form Builder Admin Publish' do
+
+  it 'should create a new form and allow navigation to builder' do
+    @browser.open "/nedss/cmrs"
+    @browser.click "link=Forms"
+    @browser.wait_for_page_to_load "30000"
+    @browser.click "link=New form"
+    @browser.wait_for_page_to_load "30000"
+    @browser.type "form_name", "African Tick Bite Publish Test"
+    @browser.click "form_submit"
+    @browser.wait_for_page_to_load "30000"
+    @browser.is_text_present("Not Published").should be_true
+  end
+  
+  it 'should add two questions' do
+    @browser.click "link=Form Builder"
+    @browser.wait_for_page_to_load "30000"
+    @browser.click "link=Add a question"
+    wait_for_element_present("new-question-form")
+    @browser.type "question_question_text", "Did you go into the tall grass?"
+    @browser.select "question_data_type", "label=Drop-down select list"
+    @browser.click "question_submit"    
+    wait_for_element_not_present("new-question-form")
+    @browser.is_text_present("Did you go into the tall grass?").should be_true
+    @browser.click "link=Add a question"
+    wait_for_element_present("new-question-form")
+    @browser.type "question_question_text", "Describe the tick."
+    @browser.select "question_data_type", "label=Multi-line text"
+    @browser.click "question_submit"    
+    wait_for_element_not_present("new-question-form")
+    @browser.is_text_present("Describe the tick.").should be_true
+  end
+  
+  it 'should add one value set' do
+    @browser.click "link=Add value set"
+    wait_for_element_present("new-value-set-form")
+    @browser.type "value_set_element_name", "Yes/No/Maybe"
+    @browser.click "link=Add a value"
+    @browser.click "link=Add a value"
+    @browser.click "link=Add a value"
+    wait_for_element_present("value_set_element_value_attributes__name")
+    @browser.type "value_set_element_value_attributes__name", "Yes"
+    @browser.type "document.forms[0].elements['value_set_element[value_attributes][][name]'][1]", "No"
+    @browser.type "document.forms[0].elements['value_set_element[value_attributes][][name]'][2]", "Maybe"
+    @browser.click "value_set_element_submit"
+    wait_for_element_not_present("new-value-set-form")
+    @browser.is_text_present("Yes/No/Maybe").should be_true
+  end
+  
+  it 'should publish' do
+    @browser.click "//input[@value='Publish']"
+    @browser.wait_for_page_to_load "30000"
+    @browser.is_text_present("Form was successfully published").should be_true
+  end
+end
