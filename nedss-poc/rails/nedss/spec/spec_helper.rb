e63689c35029eb3f72b245a7766647f44214473b
pete <pete@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-06-12 18:39:57 +0000

    Still returning MVC testing to normal.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@858 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/spec/spec_helper.rb
+++ nedss-poc/rails/nedss/spec/spec_helper.rb
@@ -24,6 +24,7 @@ Spec::Runner.configure do |config|
   # names with your fixtures.
   #
   # config.global_fixtures = :table_a, :table_b
+  config.global_fixtures = :codes
   #
   # If you declare global fixtures, be aware that they will be declared
   # for all of your examples, even those that don't use them.
