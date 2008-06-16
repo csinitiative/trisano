2c5a4002ebbd6ac62e9bb00bdc249074aa169052
ed <ed@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-06-16 21:23:49 +0000

    Adding UAT for unds560.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@865 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/spec/uat/form_builder_admin_core_follow_up_selspec.rb
+++ nedss-poc/rails/nedss/spec/uat/form_builder_admin_core_follow_up_selspec.rb
@@ -1,21 +1,68 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-$dont_kill_browser = true
+# $dont_kill_browser = true
 
 describe 'Form Builder Admin Core Follow-Up Functionality' do
   
   before(:all) do
-    @form_name = NedssHelper.get_unique_name(2) + " follow up uat"
-
+    @form_name = NedssHelper.get_unique_name(2) + " fu-uat"
+    @cmr_last_name = NedssHelper.get_unique_name(2) + " fu-uat"
+    @follow_up_question_text = NedssHelper.get_unique_name(2)  + " question fu-uat"
+    @follow_up_answer =  NedssHelper.get_unique_name(2)  + " answer fu-uat"    
   end
   
   after(:all) do
     @form_name = nil
-
+    @cmr_last_name = nil
+    @follow_up_question_text = nil
+    @follow_up_answer =  nil
   end
   
   it 'should handle core follow-ups.' do
+    NedssHelper.create_new_form_and_go_to_builder(@browser, @form_name, "African Tick Bite Fever", "All Jurisdictions")
+    NedssHelper.add_core_follow_up_to_view(@browser, "Default View", "2", "Patient birth gender")
+    NedssHelper.add_question_to_follow_up(@browser, "Core follow up for: '2'", @follow_up_question_text, "Single line text")
+    NedssHelper.publish_form(@browser)
+    NedssHelper.create_basic_investigatable_cmr(@browser, @cmr_last_name, "African Tick Bite Fever", "Bear River Health Department")
+    
+    @browser.click "edit_cmr_link"
+    @browser.wait_for_page_to_load($load_time)
+    @browser.is_text_present(@follow_up_question_text).should be_false
+    
+    # Enter the answer that meets the follow-up condition
+    @browser.select("event_active_patient__active_primary_entity__person_birth_gender_id", "label=Female")
+    NedssHelper.click_core_tab(@browser, "Investigation") # This click triggers the onChange that triggers the condition processing
+    sleep(2) # Replace this with something better -- need to make sure the round trip to process condition has happened
+    @browser.is_text_present(@follow_up_question_text).should be_true
+    
+    # Enter an answer that does not meet the follow-up condition
+    @browser.select("event_active_patient__active_primary_entity__person_birth_gender_id", "label=Male")
+    NedssHelper.click_core_tab(@browser, "Investigation") # This click triggers the onChange that triggers the condition processing
+    sleep(2) # Replace this with something better -- need to make sure the round trip to process condition has happened
+    @browser.is_text_present(@follow_up_question_text).should be_false
 
+    # Back to a match, enter follow up answer and submit
+    @browser.select("event_active_patient__active_primary_entity__person_birth_gender_id", "label=Female")
+    NedssHelper.click_core_tab(@browser, "Investigation") # This click triggers the onChange that triggers the condition processing
+    sleep(2) # Replace this with something better -- need to make sure the round trip to process condition has happened
+    NedssHelper.answer_investigator_question(@browser, @follow_up_question_text, @follow_up_answer)
+
+    @browser.click "event_submit"
+    @browser.wait_for_page_to_load($load_time)
+    @browser.is_text_present(@follow_up_answer).should be_true
+    
+    @browser.click "edit_cmr_link"
+    @browser.wait_for_page_to_load($load_time)
+    
+    # Enter an answer that does not meet the follow-up condition
+    @browser.select("event_active_patient__active_primary_entity__person_birth_gender_id", "label=Male")
+    NedssHelper.click_core_tab(@browser, "Investigation") # This click triggers the onChange that triggers the condition processing
+    sleep(2) # Replace this with something better -- need to make sure the round trip to process condition has happened
+    
+    @browser.click "event_submit"
+    @browser.wait_for_page_to_load($load_time)
+    @browser.is_text_present(@follow_up_answer).should be_false
+    
   end
     
 end
