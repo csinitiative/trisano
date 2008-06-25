01a98c4c244de7bcee9fefd6f5ea30d5c6299fa4
claym <claym@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-06-25 20:22:32 +0000

    Added intial code list functionality
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@904 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/app/controllers/external_codes_controller.rb
+++ nedss-poc/rails/nedss/app/controllers/external_codes_controller.rb
@@ -1,2 +1,12 @@
 class ExternalCodesController < ApplicationController
+    before_filter :can_update?, :only => [:edit, :update, :destroy]
+    before_filter :can_view?, :only => [:show]
+
+    def index
+        @external_codes = ExternalCode.find(:all)
+	respond_to do |format|
+            format.html
+	    format.xml
+	end
+    end
 end
