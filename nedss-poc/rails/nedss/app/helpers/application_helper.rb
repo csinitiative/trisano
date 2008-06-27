bd5b96ed39b4fb84bc407cf4f3432b0046af7257
pete <pete@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-06-27 20:25:38 +0000

    And a little more cleanup
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@946 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/app/helpers/application_helper.rb
+++ nedss-poc/rails/nedss/app/helpers/application_helper.rb
@@ -82,27 +82,4 @@ module ApplicationHelper
     return replace_element, replace_partial
   end
 
-  module ActiveRecord::Validations::ClassMethods
-    def _validates_associated(*associations)
-      associations.each do |association|
-        class_eval do
-          validates_each(associations) do |record, associate_name, value|
-            p record, associate_name, value
-            p "---------------------------"
-            associates = record.send(associate_name)
-            associates = [associates] unless associates.respond_to?('each')
-            associates.each do |associate|
-              if associate && !associate.valid?
-                associate.errors.each do |key, value|
-                  record.errors.add(key, value)
-                end
-              end
-            end
-            record.errors.delete associate_name
-          end
-        end
-      end
-    end
-  end
-
 end
