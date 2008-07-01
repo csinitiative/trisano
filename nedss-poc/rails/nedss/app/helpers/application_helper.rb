42f8f63cd4a8f531655545d65fa8c44fb284683f
ed <ed@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-07-01 22:24:29 +0000

    Fixing tab enabling/disabling defect (no defect # to reference). Also gets rid of some duplication and makes the tab ids consistent.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@963 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/app/helpers/application_helper.rb
+++ nedss-poc/rails/nedss/app/helpers/application_helper.rb
@@ -89,5 +89,13 @@ module ApplicationHelper
       "#{User.current_user.first_name} #{User.current_user.last_name}"
     end
   end
+  
+  def tab_toggler(include_investigation_tab)
+    result = ""
+    investigation_tab = (include_investigation_tab == true) ?  ", 'investigation_tab'" : ""
+    result += "<span id='disable_tabs' onClick=\"myTabs.removeClass('yui-navset'); myTabs.removeClass('yui-content'); ['demographic_tab','clinical_tab','lab_info_tab', 'contacts_tab', 'epi_tab','reporting_tab','administrative_tab'#{investigation_tab}].each(Element.show); Element.hide('disable_tabs'); Element.hide('tabs'); Element.show('enable_tabs');return false;\">[Disable Tabs]</span>"
+    result += "<span id='enable_tabs' onClick=\"myTabs.addClass('yui-navset'); myTabs.addClass('yui-content'); ['demographic_tab','clinical_tab','lab_info_tab', 'contacts_tab',  'epi_tab', 'reporting_tab', 'administrative_tab', 'enable_tabs'#{investigation_tab}].each(Element.hide); Element.show('disable_tabs'); Element.show('tabs'); myTabs.set('activeIndex',0); return false;\" style='display: none;'>[Enable Tabs]</span>"
+    result
+  end
 
 end
