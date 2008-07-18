92471bcc7a316c0ad89427d77c27493a8f4377e6
dave <dave@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-07-18 03:38:18 +0000

    Re-factoring helper method for creating cmr's
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@1051 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-poc/rails/nedss/spec/uat/examples/cmr_helper_example_selspec.rb
+++ nedss-poc/rails/nedss/spec/uat/examples/cmr_helper_example_selspec.rb
@@ -1,6 +1,5 @@
-require File.dirname(__FILE__) + '/spec_helper'
-
-# $dont_kill_browser = true
+require File.dirname(__FILE__) + './spec_helper' 
+$dont_kill_browser = true
 
 describe "cmr helper example" do 
   before(:each) do
@@ -12,127 +11,10 @@ describe "cmr helper example" do
   end
   
   it "should create a cmr from a hash of field names and values" do 
-    @cmr_fields = {
-      # Patient fields
-      "event_active_patient__active_primary_entity__person_last_name" => get_unique_name(1),
-      "event_active_patient__active_primary_entity__person_first_name" => get_unique_name(1),
-      "event_active_patient__active_primary_entity__person_middle_name" => get_unique_name(1),
-      "event_active_patient__active_primary_entity__address_street_number" => "123",
-      "event_active_patient__active_primary_entity__address_street_name" => get_unique_name(1),
-      "event_active_patient__active_primary_entity__address_unit_number" => "2",
-      "event_active_patient__active_primary_entity__address_city" => get_unique_name(1),
-      "event_active_patient__active_primary_entity__address_postal_code" => "84601",
-      "event_active_patient__active_primary_entity__person_birth_date" => "1/1/1974",
-      "event_active_patient__active_primary_entity__person_approximate_age_no_birthday" => "22",
-      "event_active_patient__active_primary_entity__person_date_of_death" => "1/1/1974",
-      "event_active_patient__active_primary_entity__telephone_area_code" => "801",
-      "event_active_patient__active_primary_entity__telephone_phone_number" => "555-7894",
-      "event_active_patient__active_primary_entity__telephone_extension" => "147",
-      "event_active_patient__active_primary_entity__person_birth_gender_id" => "Female",
-      "event_active_patient__active_primary_entity__person_ethnicity_id" => "Not Hispanic or Latino",
-      "event_active_patient__active_primary_entity__person_primary_language_id" => "Hmong",
-      "event_active_patient__active_primary_entity_race_ids" => "Asian",
-      "event_active_patient__active_primary_entity__address_county_id" => "Beaver",
-      "event_active_patient__active_primary_entity__address_state_id" => "Utah",
-      #Disease fields
-      "event_disease_disease_onset_date" => "1/1/1974",
-      "event_disease_date_diagnosed" => "1/1/1974",
-      "event_disease_disease_id" => "Amebiasis",
-      #Status fields
-      "event_disease_died_id" => "Yes",
-      "event_imported_from_id" => "Utah",
-      #Hospital fields
-      "event_hospitalized_health_facility__hospitals_participation_admission_date" => "1/1/1974",
-      "event_hospitalized_health_facility__hospitals_participation_discharge_date" => "1/1/1974",
-      "event_disease_hospitalized_id" => "Yes",
-      "event_hospitalized_health_facility_secondary_entity_id" => "Alta View Hospital",
-      #Diagnosis field
-      "event_diagnosing_health_facility_secondary_entity_id" => "Alta View Hospital",
-      #Treatment fields
-      "event_active_patient__participations_treatment_treatment_given_yn_id" => "Yes",
-      "event_active_patient__participations_treatment_treatment" => get_unique_name(1),
-      #Clinician fields
-      "event_clinician__active_secondary_entity__person_last_name" => get_unique_name(1),
-      "event_clinician__active_secondary_entity__person_first_name" => get_unique_name(1),
-      "event_clinician__active_secondary_entity__person_middle_name" => get_unique_name(1),
-      "event_clinician__active_secondary_entity__address_street_number" => "456",
-      "event_clinician__active_secondary_entity__address_street_name" => get_unique_name(1),
-      "event_clinician__active_secondary_entity__address_unit_number" => "5141",
-      "event_clinician__active_secondary_entity__address_city" => get_unique_name(1),
-      "event_clinician__active_secondary_entity__address_postal_code" => "84602",
-      "event_clinician__active_secondary_entity__person_birth_date" => "1/1/1974",
-      "event_clinician__active_secondary_entity__person_approximate_age_no_birthday" => "55",
-      "event_clinician__active_secondary_entity__person_date_of_death" => "1/1/1974",
-      "event_clinician__active_secondary_entity__telephone_area_code" => "501",
-      "event_clinician__active_secondary_entity__telephone_phone_number" => "555-1645",
-      "event_clinician__active_secondary_entity__telephone_extension" => "1645",
-      "event_clinician__active_secondary_entity__person_birth_gender_id" => "Female",
-      "event_clinician__active_secondary_entity__person_ethnicity_id" => "Hispanic or Latino",
-      "event_clinician__active_secondary_entity_race_ids" => "American Indian",
-      "event_clinician__active_secondary_entity__person_primary_language_id" => "Japanese",
-      #lab result fields -- Need to be updated
-#      "event_lab_result_specimen_source_id" => "Blood",
-#      "event_lab_result_tested_at_uphl_yn_id" => "Yes",
-#      "event_lab_result_lab_result_text" => get_unique_name(1),
-#      "event_lab_result_collection_date" => "1/1/1974",
-#      "event_lab_result_lab_test_date" => "1/1/1974",
-      #contact fields
-      "event_contact__active_secondary_entity__address_state_id" => "Alaska",
-      "event_contact__active_secondary_entity__address_county_id" => "Davis",
-      "event_contact__active_secondary_entity__person_birth_gender_id" => "Female",
-      "event_contact__active_secondary_entity__person_ethnicity_id" => "Not Hispanic or Latino",
-      "event_contact__active_secondary_entity_race_ids" => "American Indian",
-      "event_contact__active_secondary_entity__person_primary_language_id" => "Italian",
-      "event_contact__active_secondary_entity__person_last_name" => get_unique_name(1),
-      "event_contact__active_secondary_entity__person_first_name" => get_unique_name(1),
-      "event_contact__active_secondary_entity__person_middle_name" => get_unique_name(1),
-      "event_contact__active_secondary_entity__address_street_number" => "7845",
-      "event_contact__active_secondary_entity__address_street_name" => get_unique_name(1),
-      "event_contact__active_secondary_entity__address_unit_number" => "7788",
-      "event_contact__active_secondary_entity__address_city" => get_unique_name(1),
-      "event_contact__active_secondary_entity__address_postal_code" => "87484",
-      "event_contact__active_secondary_entity__person_birth_date" => "1/1/1974",
-      "event_contact__active_secondary_entity__person_approximate_age_no_birthday" => "64",
-      "event_contact__active_secondary_entity__person_date_of_death" => "1/1/1974",
-      "event_contact__active_secondary_entity__telephone_area_code" => "840",
-      "event_contact__active_secondary_entity__telephone_phone_number" => "555-7457",
-      "event_contact__active_secondary_entity__telephone_extension" => "4557",
-      #epidemiological fields
-      "event_active_patient__participations_risk_factor_food_handler_id" => "Yes",
-      "event_active_patient__participations_risk_factor_healthcare_worker_id" => "Yes",
-      "event_active_patient__participations_risk_factor_group_living_id" => "Yes",
-      "event_active_patient__participations_risk_factor_day_care_association_id" => "Yes",
-      "event_active_patient__participations_risk_factor_pregnant_id" => "Yes",
-      "event_active_patient__participations_risk_factor_pregnancy_due_date" => "1/1/1974",
-      "event_active_patient__participations_risk_factor_risk_factors" => get_unique_name(3),
-      #Not sure why this doesn't work. For some reason rspec thinks this is a select, but it's a type...
-      #"event_active_patient__participations_risk_factor_risk_factors_notes" => NedssHelper.get_unique_name(30),              
-      #reporting info fields
-      "event_active_reporter__active_secondary_entity__person_first_name" => get_unique_name(1),
-      "event_active_reporter__active_secondary_entity__person_last_name" => get_unique_name(1),
-      "event_active_reporter__active_secondary_entity__telephone_area_code" => "901",
-      "event_active_reporter__active_secondary_entity__telephone_phone_number" => "555-1452",
-      "event_active_reporter__active_secondary_entity__telephone_extension" => "4777",
-      "event_results_reported_to_clinician_date" => "1/1/1974",
-      "model_auto_completer_tf" => get_unique_name(2), #This is the reporting agency field...
-      #Administrative fields
-      "event_event_case_status_id" => "Confirmed",
-      "event_outbreak_associated_id" => "Yes",
-      "event_active_jurisdiction_secondary_entity_id" => "Out of State",
-      "event_event_status_id" => "Investigation Complete",
-      "event_investigation_LHD_status_id" => "Open",
-      "event_event_onset_date" => "1/1/1974",
-      "event_outbreak_name" => get_unique_name(1),
-      "event_investigation_started_date" => "1/1/1974",
-      "event_investigation_completed_LHD_date" => "1/1/1974",
-      "event_event_name" => get_unique_name(1),
-      "event_first_reported_PH_date" => "1/1/1974",
-      "event_review_completed_UDOH_date" => "1/1/1974"             
-    }
     @browser.open("/nedss/forms")
-    click_nav_new_cmr(@browser)
-    set_fields(@browser, @cmr_fields)
-    save_cmr(@browser)
+    @browser.wait_for_page_to_load("30000")
+    cmr_hash = NedssHelper.get_full_cmr_hash()
+    NedssHelper.create_cmr_from_hash(@browser, cmr_hash)
   end
 end
 
--- nedss-poc/rails/nedss/spec/uat/nedss_helper.rb
+++ nedss-poc/rails/nedss/spec/uat/nedss_helper.rb
@@ -42,13 +42,45 @@ module NedssHelper
   #  for you. 
   def set_fields(browser, value_hash)
     fields = browser.get_all_fields
+    puts fields
     value_hash.each_pair do |key, value|
       if fields.index(key) != nil
         browser.type(key, value) 
       else
-        browser.select(key,"label=" + value)
+        begin
+          browser.select(key,"label=" + value)
+        rescue StandardError => err
+          puts err
+          if err == "Specified element is not a Select (has no options)"
+            #This is usually because the element is the name of a auto-complete field
+            browser.type(key, value)
+          else
+            puts("WARNING: Field " + key + " not found. Value not set.")
+          end
+        end
       end
     end
+  end
+  
+  #TODO Check for link first and navigate to home page if not found, then click new
+  def click_new_cmr(browser)
+    browser.click("link=NEW CMR")
+  end
+  
+  # Use get_full_cmr to create a cmr with every field filled in (excludes repeating elements)
+  # It uses random words for text fields
+  def create_cmr_from_hash(browser, cmr_hash)
+    click_new_cmr(browser)
+    browser.wait_for_page_to_load($load_time)
+    set_fields(browser,cmr_hash)
+    browser.click('event_submit')
+    browser.wait_for_page_to_load($load_time)
+  end
+  
+  # Use get_full_cmr to create a cmr with only the last name filled in
+  # It uses random words for the last name field
+  def get_nil_cmr()
+    
   end
   
   #Use click_core_tab to change tabs in CMR views
@@ -304,6 +336,128 @@ module NedssHelper
     html_source[record_number_start_position..record_number_end_position]
   end
 
+  def get_full_cmr_hash()
+    @cmr_fields = {
+              # Patient fields
+              "event_active_patient__active_primary_entity__person_last_name" => NedssHelper.get_unique_name(1),
+              "event_active_patient__active_primary_entity__person_first_name" => NedssHelper.get_unique_name(1),
+              "event_active_patient__active_primary_entity__person_middle_name" => NedssHelper.get_unique_name(1),
+              "event_active_patient__active_primary_entity__address_street_number" => "123",
+              "event_active_patient__active_primary_entity__address_street_name" => NedssHelper.get_unique_name(1),
+              "event_active_patient__active_primary_entity__address_unit_number" => "2",
+              "event_active_patient__active_primary_entity__address_city" => NedssHelper.get_unique_name(1),
+              "event_active_patient__active_primary_entity__address_postal_code" => "84601",
+              "event_active_patient__active_primary_entity__person_birth_date" => "1/1/1974",
+              "event_active_patient__active_primary_entity__person_approximate_age_no_birthday" => "22",
+              "event_active_patient__active_primary_entity__person_date_of_death" => "1/1/1974",
+              "event_active_patient__active_primary_entity__telephone_area_code" => "801",
+              "event_active_patient__active_primary_entity__telephone_phone_number" => "555-7894",
+              "event_active_patient__active_primary_entity__telephone_extension" => "147",
+              "event_active_patient__active_primary_entity__person_birth_gender_id" => "Female",
+              "event_active_patient__active_primary_entity__person_ethnicity_id" => "Not Hispanic or Latino",
+              "event_active_patient__active_primary_entity__person_primary_language_id" => "Hmong",
+              "event_active_patient__active_primary_entity_race_ids" => "Asian",
+              "event_active_patient__active_primary_entity__address_county_id" => "Beaver",
+              "event_active_patient__active_primary_entity__address_state_id" => "Utah",
+             #Disease fields
+              "event_disease_disease_onset_date" => "1/1/1974",
+              "event_disease_date_diagnosed" => "1/1/1974",
+              "event_disease_disease_id" => "Amebiasis",
+              #Status fields
+              "event_disease_died_id" => "Yes",
+              "event_imported_from_id" => "Utah",
+              #Hospital fields
+              "event_new_hospital_attributes__admission_date" => "1/1/1974",
+              "event_new_hospital_attributes__discharge_date" => "1/1/1974",
+              "event_new_hospital_attributes__secondary_entity_id" => "Ashley Regional Medical Center",
+              "event_disease_hospitalized_id" => "Yes",
+              #Diagnosis field
+              "event_new_diagnostic_attributes__secondary_entity_id" => "Alta View Hospital",
+              #Treatment fields
+              "event_active_patient__participations_treatment_treatment_given_yn_id" => "Yes",
+              "event_active_patient__participations_treatment_treatment" => NedssHelper.get_unique_name(1),
+              #Clinician fields
+              "event_clinician__active_secondary_entity__person_last_name" => NedssHelper.get_unique_name(1),
+              "event_clinician__active_secondary_entity__person_first_name" => NedssHelper.get_unique_name(1),
+              "event_clinician__active_secondary_entity__person_middle_name" => NedssHelper.get_unique_name(1),
+              "event_clinician__active_secondary_entity__address_street_number" => "456",
+              "event_clinician__active_secondary_entity__address_street_name" => NedssHelper.get_unique_name(1),
+              "event_clinician__active_secondary_entity__address_unit_number" => "5141",
+              "event_clinician__active_secondary_entity__address_city" => NedssHelper.get_unique_name(1),
+              "event_clinician__active_secondary_entity__address_postal_code" => "84602",
+              "event_clinician__active_secondary_entity__person_birth_date" => "1/1/1974",
+              "event_clinician__active_secondary_entity__person_approximate_age_no_birthday" => "55",
+              "event_clinician__active_secondary_entity__person_date_of_death" => "1/1/1974",
+              "event_clinician__active_secondary_entity__telephone_area_code" => "501",
+              "event_clinician__active_secondary_entity__telephone_phone_number" => "555-1645",
+              "event_clinician__active_secondary_entity__telephone_extension" => "1645",
+              "event_clinician__active_secondary_entity__person_birth_gender_id" => "Female",
+              "event_clinician__active_secondary_entity__person_ethnicity_id" => "Hispanic or Latino",
+              "event_clinician__active_secondary_entity_race_ids" => "American Indian",
+              "event_clinician__active_secondary_entity__person_primary_language_id" => "Japanese",
+              #lab result fields
+              "event[new_lab_attributes][][name]" => NedssHelper.get_unique_name(2),
+              "event_new_lab_attributes__specimen_source_id" => "Blood",
+              "event_new_lab_attributes__tested_at_uphl_yn_id" => "Yes",
+              "event_new_lab_attributes__lab_result_text" => NedssHelper.get_unique_name(1),
+              "event_new_lab_attributes__collection_date" => "1/1/1974",
+              "event_new_lab_attributes__lab_test_date" => "1/1/1974",
+              #contact fields
+              "event_contact__active_secondary_entity__address_state_id" => "Alaska",
+              "event_contact__active_secondary_entity__address_county_id" => "Davis",
+              "event_contact__active_secondary_entity__person_birth_gender_id" => "Female",
+              "event_contact__active_secondary_entity__person_ethnicity_id" => "Not Hispanic or Latino",
+              "event_contact__active_secondary_entity_race_ids" => "American Indian",
+              "event_contact__active_secondary_entity__person_primary_language_id" => "Italian",
+              "event_contact__active_secondary_entity__person_last_name" => NedssHelper.get_unique_name(1),
+              "event_contact__active_secondary_entity__person_first_name" => NedssHelper.get_unique_name(1),
+              "event_contact__active_secondary_entity__person_middle_name" => NedssHelper.get_unique_name(1),
+              "event_contact__active_secondary_entity__address_street_number" => "7845",
+              "event_contact__active_secondary_entity__address_street_name" => NedssHelper.get_unique_name(1),
+              "event_contact__active_secondary_entity__address_unit_number" => "7788",
+              "event_contact__active_secondary_entity__address_city" => NedssHelper.get_unique_name(1),
+              "event_contact__active_secondary_entity__address_postal_code" => "87484",
+              "event_contact__active_secondary_entity__person_birth_date" => "1/1/1974",
+              "event_contact__active_secondary_entity__person_approximate_age_no_birthday" => "64",
+              "event_contact__active_secondary_entity__person_date_of_death" => "1/1/1974",
+              "event_contact__active_secondary_entity__telephone_area_code" => "840",
+              "event_contact__active_secondary_entity__telephone_phone_number" => "555-7457",
+              "event_contact__active_secondary_entity__telephone_extension" => "4557"}#,
+=begin               #epidemiological fields
+              "event_active_patient__participations_risk_factor_food_handler_id" => "Yes",
+              "event_active_patient__participations_risk_factor_healthcare_worker_id" => "Yes",
+              "event_active_patient__participations_risk_factor_group_living_id" => "Yes",
+              "event_active_patient__participations_risk_factor_day_care_association_id" => "Yes",
+              "event_active_patient__participations_risk_factor_pregnant_id" => "Yes",
+              "event_active_patient__participations_risk_factor_pregnancy_due_date" => "1/1/1974",
+              "event_active_patient__participations_risk_factor_risk_factors" => NedssHelper.get_unique_name(3),
+              #Not sure why this doesn't work. For some reason rspec thinks this is a select, but it's a type...
+              #"event_active_patient__participations_risk_factor_risk_factors_notes" => NedssHelper.get_unique_name(30),              
+              #reporting info fields
+              "event_active_reporter__active_secondary_entity__person_first_name" => NedssHelper.get_unique_name(1),
+              "event_active_reporter__active_secondary_entity__person_last_name" => NedssHelper.get_unique_name(1),
+              "event_active_reporter__active_secondary_entity__telephone_area_code" => "901",
+              "event_active_reporter__active_secondary_entity__telephone_phone_number" => "555-1452",
+              "event_active_reporter__active_secondary_entity__telephone_extension" => "4777",
+              "event_results_reported_to_clinician_date" => "1/1/1974",
+              "model_auto_completer_tf" => NedssHelper.get_unique_name(2), #This is the reporting agency field...
+              #Administrative fields
+              "event_event_case_status_id" => "Confirmed",
+              "event_outbreak_associated_id" => "Yes",
+              #"event_active_jurisdiction_secondary_entity_id" => "Out of State",
+              "event_event_status_id" => "Investigation Complete",
+              "event_investigation_LHD_status_id" => "Open",
+              "event_event_onset_date" => "1/1/1974",
+              "event_outbreak_name" => NedssHelper.get_unique_name(1),
+              "event_investigation_started_date" => "1/1/1974",
+              "event_investigation_completed_LHD_date" => "1/1/1974",
+              "event_event_name" => NedssHelper.get_unique_name(1),
+              "event_first_reported_PH_date" => "1/1/1974",
+              "event_review_completed_UDOH_date" => "1/1/1974"             
+=end             #}
+    return(@cmr_fields)
+  end
+  
   private
   
   def assert_contains(browser, container_element, element)
