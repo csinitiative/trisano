8f5c8c430a968ba5f8e426bfd8da37a621948825
mike <mike@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-08-13 23:56:32 +0000

    removing some old directories that are no longer in use from the root of the repo
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@1204 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- nedss-ui/ui-prototyping/public/javascripts/untitled.js
+++
@@ -1,6 +0,0 @@
-YAHOO.util.Event.onDOMReady(function() {
-var tabs = new YAHOO.yodeler.widget.ScrollTabView('cmr_tabs', { width: 723, height: 300, direction: /'horizontal' });
-tabs.set('activeTab', tabs.getTab(0));
-tabs.addTab(new YAHOO.widget.Tab({label: 'Dynamically Added Tab', content: 'The content moves from left to / right in this example.'}));
- });
-
---
+++ users/README.txt
@@ -0,0 +1,1 @@
+This is an area of the repo for developers to work on prototypes etc.
