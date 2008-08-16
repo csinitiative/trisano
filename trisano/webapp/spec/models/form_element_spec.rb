6557418b7e577f75b266c1985ce330ab4c4a1523
ed <ed@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-08-16 16:37:29 +0000

    Restoring specs inadvertently replaced by a botched copy/paste in previous commit.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@1232 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- trisano/webapp/spec/models/form_element_spec.rb
+++ trisano/webapp/spec/models/form_element_spec.rb
@@ -81,6 +81,62 @@ describe "Quesiton FormElement" do
   end
 end
 
+describe "Quesiton FormElement when added to library" do
+  
+  before(:each) do
+    @form_element = QuestionElement.create(:tree_id => 1, :form_id => 1)
+    @question = Question.create({:question_text => "Que?", :data_type => "single_line_text", :short_name => "que_q" })
+    @form_element.question = @question
+    
+  end
+  
+  it "the copy should have a correct ids and type" do
+    @library_question = @form_element.add_to_library(nil)
+    @library_question.id.should_not be_nil
+    @library_question.form_id.should be_nil
+    @library_question.template_id.should be_nil
+    @library_question.parent_id.should be_nil
+    @library_question.type.should eql("QuestionElement")
+    @library_question.tree_id.should_not be_nil
+    @library_question.tree_id.should_not eql(@form_element.tree_id)
+  end
+    
+  it "the copy should be a template" do
+    @library_question = @form_element.add_to_library(nil)
+    @library_question.is_template.should be_true
+  end
+    
+  it "the question copy should be a clone of the question it was created from" do
+    @library_question = @form_element.add_to_library(nil)
+    @library_question.question.should_not be_nil
+    @library_question.question.question_text.should eql(@question.question_text)
+    @library_question.question.data_type.should eql(@question.data_type)
+    @library_question.question.short_name.should eql(@question.short_name)
+  end
+    
+  it "the copy should have follow up questions" do
+    follow_up_container = FollowUpElement.create({:tree_id => 1, :form_id => 1,:name => "Follow up", :condition => "Yes"})
+    follow_up_question_element = QuestionElement.create(:tree_id => 1, :form_id => 1)
+    follow_up_question = Question.create({:question_text => "Did you do it?", :data_type => "single_line_text"})
+    follow_up_question_element.question = follow_up_question
+    follow_up_container.add_child(follow_up_question_element)
+    @form_element.add_child(follow_up_container)
+    
+    @library_question = @form_element.add_to_library(nil)
+    follow_up_copy = @library_question.children[0]
+    follow_up_copy.name.should eql(follow_up_container.name)
+    follow_up_copy.condition.should eql(follow_up_container.condition)
+    
+    follow_up_copy_quesiton_element = follow_up_copy.children[0]
+    follow_up_copy_quesiton_element.should_not be_nil
+    follow_up_copy_quesiton_element.question.should_not be_nil
+    follow_up_copy_quesiton_element.question.question_text.should eql(follow_up_question.question_text)
+    follow_up_copy_quesiton_element.question.data_type.should eql(follow_up_question.data_type)
+    
+  end
+   
+end
+
 describe "Quesiton FormElement when filtering the library" do
   
   before(:each) do
@@ -127,7 +183,7 @@ describe "Quesiton FormElement when filtering the library" do
     @filtered_elements[0].is_a?(QuestionElement).should be_true
   end
   
-    it "should return no question elements if a non-matching filter and a from_library direction are provided" do
+  it "should return no question elements if a non-matching filter and a from_library direction are provided" do
     @filtered_elements = FormElement.filter_library(:from_library, "ZZZ")
     @filtered_elements.size.should eql(0)
   end
