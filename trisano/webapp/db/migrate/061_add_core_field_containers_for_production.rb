039e5ec26885ce1baa885e526f789b507732d0ab
ed <ed@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-08-21 19:25:53 +0000

    Adding production migrations for form builder. Addresses tri583: R2 Forms not rendering in 3.2. Also upgrades existing core paths to use a base of morbidity_event instead of event.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@1287 b21a84b9-e453-0410-92a3-c4f515f7ae45

---
+++ trisano/webapp/db/migrate/061_add_core_field_containers_for_production.rb
@@ -0,0 +1,44 @@
+# Copyright (C) 2007, 2008, The Collaborative Software Foundation
+#
+# This file is part of TriSano.
+#
+# TriSano is free software: you can redistribute it and/or modify it under the 
+# terms of the GNU Affero General Public License as published by the 
+# Free Software Foundation, either version 3 of the License, 
+# or (at your option) any later version.
+#
+# TriSano is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License 
+# along with TriSano. If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
+
+class AddCoreFieldContainersForProduction < ActiveRecord::Migration
+  def self.up
+    
+    if RAILS_ENV == "production"
+      transaction do
+        say "Upgrading forms: Adding core field element containers to forms that need them."
+        
+        Form.find(:all).each do |form|
+          form_base_element = form.form_base_element  
+          if (form_base_element.children_count == 2)
+            say "Upgrading form #{form.id} -- #{form.name}"
+            form_base_element.add_child(CoreFieldElementContainer.create({
+                  :form_id => form_base_element.form_id, 
+                  :tree_id => form_base_element.tree_id 
+                }
+              )
+            )
+          end
+        end
+      end
+    end
+    
+  end
+
+  def self.down
+  end
+end
