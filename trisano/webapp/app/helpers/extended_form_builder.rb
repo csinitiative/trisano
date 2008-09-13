8e6f82c729dfa5ece420fe19b164483d38f20961
ed <ed@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-09-13 16:06:17 +0000

    tri666 -- Fix for core follow ups on contact forms. There's a UAT gap here to fill post 3.4 release.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@1365 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- trisano/webapp/app/helpers/extended_form_builder.rb
+++ trisano/webapp/app/helpers/extended_form_builder.rb
@@ -179,10 +179,11 @@ class ExtendedFormBuilder < ActionView::Helpers::FormBuilder
   end
 
   def core_follow_up_event(attribute, event, can_investigate)
+    return if  (event.nil? || event.form_references.nil?)
     result = ""
     
-    unless (@object.nil? || event.nil?)
-      if (can_investigate && !event.form_references.nil?)        
+    unless (@object.nil?)
+      if (event.attributes["type"] == "ContactEvent" || can_investigate)
         event.form_references.each do |form_reference|
           if (form_reference.form.form_element_cache.all_follow_ups_by_core_path("#{@object_name}[#{attribute}]").size > 0)
             result = "sendCoreConditionRequest(this, '#{event.id}', '#{@object_name}[#{attribute}]');"
