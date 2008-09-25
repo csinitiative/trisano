9b5be84ca245a954a27430694e243dc0762e099e
mike <mike@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-09-25 21:11:38 +0000

    added comments to distro shell scripts & added AGPL license
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@1431 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- trisano/distro/deploy_app.sh
+++ trisano/distro/deploy_app.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Copyright (C) 2007, 2008, The Collaborative Software Foundation
+#
+# This file is part of TriSano.
+#
+# TriSano is free software: you can redistribute it and/or modify it under the 
+# terms of the GNU Affero General Public License as published by the 
+# Free Software Foundation, either version 3 of the License, 
+# or (at your option) any later version.
+#
+# TriSano is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License 
+# along with TriSano. If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
+
+# Installs application on the same machine that script is run on. Assumes Tomcat is installed.
+# You must run ./package_app.sh prior to running this script.
 # Set TOMCAT_HOME environment variable to override default of/opt/tomcat/apache-tomcat-6.0.14
 
 . setenv.sh
