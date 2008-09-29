4fee021a6c43873c4b701268a86e6a346ff56f47
ryan <ryan@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-09-29 16:07:35 +0000

    Fix for tabs not toggling correctly.
     * updated tab_toggler to take an optional list of tab names.
    
     * updated contact and place views to explicitly pass in their list of
       tab names.
    
     * tri701
    
    
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@1442 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- trisano/webapp/app/helpers/application_helper.rb
+++ trisano/webapp/app/helpers/application_helper.rb
@@ -114,11 +114,12 @@ module ApplicationHelper
     end
   end
   
-  def tab_toggler(include_investigation_tab)
+  def tab_toggler(include_investigation_tab, tabs=%w(demographic_tab clinical_tab lab_info_tab contacts_tab epi_tab reporting_tab administrative_tab))
     result = ""
-    investigation_tab = (include_investigation_tab == true) ?  ", 'investigation_tab'" : ""
-    result += "<span id='disable_tabs' onClick=\"myTabs.removeClass('yui-navset'); myTabs.removeClass('yui-content'); ['demographic_tab','clinical_tab','lab_info_tab', 'contacts_tab', 'epi_tab', 'place_tab','reporting_tab','administrative_tab'#{investigation_tab}].each(Element.show); Element.hide('disable_tabs'); Element.hide('tabs'); Element.show('enable_tabs');return false;\">[Disable Tabs]</span>"
-    result += "<span id='enable_tabs' onClick=\"myTabs.addClass('yui-navset'); myTabs.addClass('yui-content'); ['demographic_tab','clinical_tab','lab_info_tab', 'contacts_tab',  'epi_tab', 'place_tab', 'reporting_tab', 'administrative_tab', 'enable_tabs'#{investigation_tab}].each(Element.hide); Element.show('disable_tabs'); Element.show('tabs'); myTabs.set('activeIndex',0); return false;\" style='display: none;'>[Enable Tabs]</span>"
+    tabs << 'investigation_tab' if include_investigation_tab
+    tabs_string = tabs.map{|value| "'#{value}'"}.join(',')
+    result += "<span id='disable_tabs' onClick=\"myTabs.removeClass('yui-navset'); myTabs.removeClass('yui-content'); [#{tabs_string}].each(Element.show); Element.hide('disable_tabs'); Element.hide('tabs'); Element.show('enable_tabs');return false;\">[Disable Tabs]</span>"
+    result += "<span id='enable_tabs' onClick=\"myTabs.addClass('yui-navset'); myTabs.addClass('yui-content'); [#{tabs_string},'enable_tabs'].each(Element.hide); Element.show('disable_tabs'); Element.show('tabs'); myTabs.set('activeIndex',0); return false;\" style='display: none;'>[Enable Tabs]</span>"
     result
   end
 
