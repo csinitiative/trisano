c9b08a909be279782d392baff39d6c29ba55f505
pete <pete@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-10-10 15:25:56 +0000

    Gets cmr_multiple_clinicians UAT working
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@1495 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- trisano/webapp/spec/uat/cmr_multiple_clinicians_selspec.rb
+++ trisano/webapp/spec/uat/cmr_multiple_clinicians_selspec.rb
@@ -27,7 +27,8 @@ describe 'Adding multiple clinicians to a CMR' do
   before(:all) do
     @browser.open "/trisano/cmrs"
     @browser.wait_for_page_to_load($load_time)
-    @original_last_name = get_unique_name(2) + " mc"
+    @original_last_name_1 = get_unique_name(2) + " mc"
+    @original_last_name_2 = get_unique_name(2) + " mc"
     @edited_last_name = get_unique_name(2) + " mc"
     @new_last_name = get_unique_name(2) + " mc"
   end
@@ -38,53 +39,49 @@ describe 'Adding multiple clinicians to a CMR' do
     @new_last_name = nil
   end
   
-  it "should allow a single clinician to be saved with a new CMR" do
+  it "should allow multiple clinicians to be saved with a new CMR" do
     click_nav_new_cmr(@browser).should be_true
-    @browser.type "morbidity_event_active_patient__active_primary_entity__person_last_name", "multi-clinician"
-    @browser.type "morbidity_event_active_patient__active_primary_entity__person_first_name", "test"
-    @browser.type "morbidity_event_clinician__active_secondary_entity__person_last_name", @original_last_name
-    @browser.type "morbidity_event_clinician__active_secondary_entity__person_first_name", "multi-clinician"
+    @browser.type "morbidity_event_active_patient__person_last_name", "multi-clinician"
+    @browser.type "morbidity_event_active_patient__person_first_name", "test"
+
+    click_core_tab(@browser, "Clinical")
+    @browser.click "link=Add a clinician"
+    sleep(1)
+
+    @browser.type "//div[@class='clinician'][1]//input[contains(@id, 'last_name')]", @original_last_name_1
+    @browser.type "//div[@class='clinician'][1]//input[contains(@id, 'first_name')]", "John"
+    @browser.type "//div[@class='clinician'][1]//input[contains(@id, 'phone_number')]", "5551212"
+
+    @browser.type "//div[@class='clinician'][2]//input[contains(@id, 'last_name')]", @original_last_name_2
+    @browser.type "//div[@class='clinician'][2]//input[contains(@id, 'first_name')]", "Joe"
+    @browser.type "//div[@class='clinician'][2]//input[contains(@id, 'phone_number')]", "5552323"
+
     save_cmr(@browser).should be_true
+
     @browser.is_text_present('CMR was successfully created.').should be_true
-    @browser.is_text_present(@original_last_name).should be_true
+    @browser.is_text_present(@original_last_name_1).should be_true
+    @browser.is_text_present("555-1212").should be_true
+    @browser.is_text_present(@original_last_name_2).should be_true
+    @browser.is_text_present("555-2323").should be_true
   end
 
-  it "should allow editing a clinician from the CMR's show mode" do
-    @browser.click "link=Edit clinician"
-    sleep(3)
-    # @browser.wait_for_element_present("person_form")
-    @browser.type "entity_person_last_name", @edited_last_name
-    @browser.click "person-save-button"
-    sleep(3)
-    # @browser.wait_for_element_not_present("person_form")
-    @browser.is_text_present(@edited_last_name).should be_true
-    @browser.is_text_present(@original_last_name).should be_false
-  end
-  
-  it "should allow editing a clinician from the CMR's edit mode, changing last name back to the original version" do
-    edit_cmr(@browser).should be_true
-    @browser.click "link=Edit clinician"
-    sleep(3)
-    # @browser.wait_for_element_present("person_form")
-    @browser.type "entity_person_last_name", @original_last_name
-    @browser.click "person-save-button"
-    sleep(3)
-    # @browser.wait_for_element_not_present("person_form")
-    @browser.is_text_present(@edited_last_name).should be_false
-    @browser.is_text_present(@original_last_name).should be_true
+  it "should allow removing a clinician" do
+    edit_cmr(@browser)
+    click_core_tab(@browser, "Clinical")
+    @browser.click "remove_clinician_link"
+    save_cmr(@browser).should be_true
+    @browser.is_text_present(@original_last_name_1).should_not be_true
   end
 
-  it "should allow adding a clinician from the CMR's edit mode" do
-    @browser.click "link=Clinical"
-    @browser.click "link=New Clinician"
-    sleep(3)
-    # @browser.wait_for_element_present("person_form")
-    @browser.type "entity_person_last_name", @new_last_name
-    @browser.click "person-save-button"
-    sleep(3)
-    # @browser.wait_for_element_not_present("person_form")
-    @browser.is_text_present(@new_last_name).should be_true
-    @browser.is_text_present(@original_last_name).should be_true
+  it "should allow editing a clinician" do
+    edit_cmr(@browser)
+    click_core_tab(@browser, "Clinical")
+    @browser.type "//div[@class='clinician'][1]//input[contains(@id, 'first_name')]", "William"
+    @browser.type "//div[@class='clinician'][1]//input[contains(@id, 'area_code')]", "777"
+    @browser.type "//div[@class='clinician'][1]//input[contains(@id, 'phone_number')]", "6666666"   
+    save_cmr(@browser).should be_true
+    @browser.is_text_present('William').should be_true
+    @browser.is_text_present('(777) 666-6666').should be_true
   end
 
 end
