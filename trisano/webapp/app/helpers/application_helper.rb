3f25bbb4e1e73b5aae71817dc81a85b06869a0e3
pete <pete@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-10-20 22:12:52 +0000

    Make it so that 'save & cont.' in new mode triggers the investigation forms.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@1555 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- trisano/webapp/app/helpers/application_helper.rb
+++ trisano/webapp/app/helpers/application_helper.rb
@@ -114,14 +114,6 @@ module ApplicationHelper
     end
   end
   
-  def tab_toggler(tabs=%w(demographic_tab clinical_tab lab_info_tab contacts_tab epi_tab reporting_tab investigation_tab notes_tab administrative_tab))
-    result = ""
-    tabs_string = tabs.map{|value| "'#{value}'"}.join(',')
-    result += "<span id='disable_tabs' onClick=\"myTabs.removeClass('yui-navset'); myTabs.removeClass('yui-content'); [#{tabs_string}].each(Element.show); Element.hide('disable_tabs'); Element.hide('tabs'); Element.show('enable_tabs');return false;\">[Disable Tabs]</span>"
-    result += "<span id='enable_tabs' onClick=\"myTabs.addClass('yui-navset'); myTabs.addClass('yui-content'); [#{tabs_string},'enable_tabs'].each(Element.hide); Element.show('disable_tabs'); Element.show('tabs'); myTabs.set('activeIndex',0); return false;\" style='display: none;'>[Enable Tabs]</span>"
-    result
-  end
-
   # http://www.pathf.com/blogs/2008/07/pretty-blocks-in-rails-views/
   def tabbed_content(tabs, focus_tab, &block)
     raise ArgumentError, "Missing block" unless block_given?
