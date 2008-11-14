1b0d4844493d09ac09111a11338c8679a844a458
mike <mike@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-11-14 22:42:42 +0000

    initial working update to httperf test
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@1726 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- trisano/webapp/script/performance/httperf/trisano-httperf-script.sh
+++ trisano/webapp/script/performance/httperf/trisano-httperf-script.sh
@@ -2,6 +2,6 @@
 
 #httperf --server=ut-nedss-dev.csinitiative.com --port=5050 --wsesslog=100,0,trisano-perf-script-data --print-reply
 
-httperf --server=ut-nedss-dev.csinitiative.com --port=5050 --wsesslog=25,0,trisano-perf-script-data 
+httperf --server=test.csi.osuosl.org --port=80 --wsesslog=25,0,trisano-new-cmr-data
 
 #httperf --server=localhost --port=8080 --wsesslog=10,0,trisano-perf-script-data
