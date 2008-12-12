528d436c3ffd31bd929ecd2f0eaa602df4261e6e
mike <mike@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-12-12 19:36:09 +0000

    tri1063 fixes trigger install
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@1949 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- trisano/distro/database/create_people_fts_trigger.sql
+++ trisano/distro/database/create_people_fts_trigger.sql
@@ -1,14 +1,20 @@
-LANGUAGE plpgsql
-AS $$
-CREATE FUNCTION people_trigger() RETURNS trigger AS $$
-begin
-  new.vector :=
-    setweight(to_tsvector('pg_catalog.english', coalesce(new.first_name,'')), 'B') ||
-    setweight(to_tsvector('pg_catalog.english', coalesce(new.last_name,'')), 'B') ||
-    setweight(to_tsvector('pg_catalog.english', coalesce(new.first_name_soundex,'')), 'A') ||
-    setweight(to_tsvector('pg_catalog.english', coalesce(new.last_name_soundex,'')), 'A');
-  return new;
-end
+--LANGUAGE plpgsql
+--AS $$
 
-CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE ON people
-  FOR EACH ROW EXECUTE PROCEDURE people_trigger();
+CREATE FUNCTION people_trigger() RETURNS trigger
+    AS $$
+                      begin
+                        new.vector :=
+                          setweight(to_tsvector('pg_catalog.english', coalesce(new.first_name,'')), 'B') ||
+                          setweight(to_tsvector('pg_catalog.english', coalesce(new.last_name,'')), 'B') ||
+                          setweight(to_tsvector('pg_catalog.english', coalesce(new.first_name_soundex,'')), 'A') ||
+                          setweight(to_tsvector('pg_catalog.english', coalesce(new.last_name_soundex,'')), 'A');
+                        return new;
+                      end
+                    $$
+    LANGUAGE plpgsql;
+
+CREATE TRIGGER tsvectorupdate
+    BEFORE INSERT OR UPDATE ON people
+    FOR EACH ROW
+    EXECUTE PROCEDURE people_trigger();
