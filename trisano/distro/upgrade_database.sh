8d80909ab364800622ac2c62c9064acec4a803d1
mike <mike@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-12-13 00:18:07 +0000

    tri1063 no longer doing permissions this way - built into upgrade task - now granting access to currently configured user
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@1954 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- trisano/distro/upgrade_database.sh
+++ trisano/distro/upgrade_database.sh
@@ -23,4 +23,3 @@
 . setenv.sh
 
 jruby -S rake -f ../webapp/Rakefile trisano:distro:upgrade_db > upgrade_db_output.txt
-jruby -S rake -f ../webapp/Rakefile trisano:distro:set_new_db_permissions > set_new_db_permissions_output.txt
