235e7338f6104d016ccae3f19a8c15dad4a93a10
pete <pete@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-12-18 22:37:37 +0000

    Implements tri1131: Update hospital list.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@2000 b21a84b9-e453-0410-92a3-c4f515f7ae45

---
+++ trisano/webapp/db/migrate/121_update_hospital_list.rb
@@ -0,0 +1,48 @@
+# Copyright (C) 2007, 2008, The Collaborative Software Foundation
+#
+# This file is part of TriSano.
+#
+# TriSano is free software: you can redistribute it and/or modify it under the 
+# terms of the GNU Affero General Public License as published by the 
+# Free Software Foundation, either version 3 of the License, 
+# or (at your option) any later version.
+#
+# TriSano is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License 
+# along with TriSano. If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
+
+class UpdateHospitalList < ActiveRecord::Migration
+
+  def self.up
+    if RAILS_ENV == "production"
+      hospital_type_id = Code.find_by_code_name_and_the_code("placetype", "H").id
+
+      # First we have to delete all existing hospitals.  Might trigger a FK constraint, but a purge of existing events is assumed
+      existing_hospitals = Entity.find(:all, 
+                                       :include => :places, 
+                                       :conditions => ["entities.entity_type = 'place' and places.place_type_id = ?", hospital_type_id])
+
+      existing_hospitals.each do |hospital|
+        hospital.place.destroy
+        hospital.destroy
+      end
+
+      # Now add all hospitals from the updated list
+      new_hospitals = YAML::load_file "#{RAILS_ROOT}/db/defaults/hospitals.yml"
+
+      new_hospitals.each do |hospital|
+        e = Entity.new
+        e.entity_type = 'place'
+        e.places.build(:name => hospital, :place_type_id => hospital_type_id)
+        e.save
+      end
+    end
+  end
+
+  def self.down
+  end
+end
