87f924e8cc6e13a7e5d76220efeb11c9bf168032
mike <mike@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2008-12-30 04:15:04 +0000

    tri1184 made field larger and added validation
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@2068 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- trisano/webapp/app/models/core_field.rb
+++ trisano/webapp/app/models/core_field.rb
@@ -16,7 +16,8 @@
 # along with TriSano. If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
 
 class CoreField < ActiveRecord::Base
-  
+ 
+  validates_length_of :help_text, :maximum => 1000, :allow_blank => true 
   after_save :flush_caches
   
   class << self
