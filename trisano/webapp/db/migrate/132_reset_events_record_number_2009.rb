87304affde84f4e90b761edf6118bd0212678dae
mike <mike@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2009-01-03 19:23:04 +0000

    tri1234 resetting event # sequence to be based on 2009
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@2095 b21a84b9-e453-0410-92a3-c4f515f7ae45

---
+++ trisano/webapp/db/migrate/132_reset_events_record_number_2009.rb
@@ -0,0 +1,32 @@
+# Copyright (C) 2007, 2008, The Collaborative Software Foundation
+#
+# This file is part of TriSano.
+#
+# TriSano is free software: you can redistribute it and/or modify it under the 
+# terms of the GNU Affero General Public License as published by the 
+# Free Software Foundation, either version 3 of the License, 
+# or (at your option) any later version.
+#
+# TriSano is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License 
+# along with TriSano. If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
+require 'migration_helpers'
+
+class ResetEventsRecordNumber2009 < ActiveRecord::Migration
+  extend MigrationHelpers
+
+  def self.up
+    transaction do
+      execute('DROP SEQUENCE events_record_number_seq')
+      execute('CREATE SEQUENCE events_record_number_seq START WITH 2009000001 INCREMENT BY 1 MAXVALUE 2009999999 MINVALUE 2009000001 CACHE 1')
+    end
+  end
+
+  def self.down
+  end
+
+end
