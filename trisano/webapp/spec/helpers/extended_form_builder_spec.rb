19591ba149731af4bff6cbf8d85ab9633fae4067
ryan <ryan@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2009-01-15 17:17:41 +0000

    Paying back some spec debt (tri1340)
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@2199 b21a84b9-e453-0410-92a3-c4f515f7ae45

---
+++ trisano/webapp/spec/helpers/extended_form_builder_spec.rb
@@ -0,0 +1,84 @@
+# Copyright (C) 2007, 2008, The Collaborative Software Foundation
+#
+# This file is part of TriSano.
+#
+# TriSano is free software: you can redistribute it and/or modify it under the 
+# terms of the GNU Affero General Public License as published by the 
+# Free Software Foundation, either version 3 of the License, 
+# or (at your option) any later version.
+#
+# TriSano is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License 
+# along with TriSano. If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
+
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe ExtendedFormBuilder, 'radio button export js' do  
+
+  describe 'during an html request' do
+
+    before(:each) do
+      @form_builder = ExtendedFormBuilder.new(nil, nil, nil, nil, nil)
+      @radio_buttons = [{:id => 1, :export_conversion_value_id => 200},
+                        {:id => 2, :export_conversion_value_id => 201}]
+      @result = @form_builder.send(:rb_export_js, @radio_buttons, 'test_id')
+    end
+
+    it 'should delay execution until the dom loads' do
+      @result.should =~ /Event.observe\(window, 'load', function\(\) \{.*\}/m      
+    end
+
+    it 'should wrap script in a script tag' do
+      @result.should =~ /<script type="text\/javascript">.*<\/script>/m
+    end
+
+    it 'should produce an observer for each radio button' do
+      @result.should =~ /\$\('1'\).observe\('click', function\(\) \{/
+      @result.should =~ /\$\('2'\).observe\('click', function\(\) \{/
+    end
+
+    it 'each observer should write the conversion value to the "id" field' do
+      @result.should =~ /\$\('test_id'\).writeAttribute\('value', '200'\)/
+      @result.should =~ /\$\('test_id'\).writeAttribute\('value', '201'\)/
+    end
+
+  end
+end
+
+describe ExtendedFormBuilder, 'drop down export js' do
+  
+  describe 'during an html request' do
+    
+    before(:each) do
+      @form_builder = ExtendedFormBuilder.new(nil, nil, nil, nil, nil)
+      @select_options = [{:value => 1, :export_conversion_value_id => 200},
+                         {:value => 2, :export_conversion_value_id => 201}]
+
+      @result = @form_builder.send(:dd_export_js, @select_options, 'hidden_id', 'select_id')
+    end
+
+    it 'should delay execution until the dom loads' do
+      @result.should =~ /Event.observe\(window, 'load', function\(\) \{.*\}/m      
+    end
+
+    it 'should wrap script in a script tag' do
+      @result.should =~ /<script type="text\/javascript">.*<\/script>/m
+    end
+
+    it 'should observe the select field' do
+      @result.should =~ /\$\('select_id'\).observe\('change', function\(\) \{/
+    end
+
+    it 'should write to option export values to the hidden field' do
+      @result.should =~ /if \(this.value == '1'\) \{ \$\('hidden_id'\).writeAttribute\('value', '200'\) \}/
+      @result.should =~ /if \(this.value == '2'\) \{ \$\('hidden_id'\).writeAttribute\('value', '201'\) \}/
+    end
+  end
+    
+end
+
+  
