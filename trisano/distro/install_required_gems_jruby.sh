8ff912b026ee661c28df7c4b36671822fe8a89d2
ryan <ryan@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2009-02-27 06:07:35 +0000

    added rack gem
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@2396 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- trisano/distro/install_required_gems_jruby.sh
+++ trisano/distro/install_required_gems_jruby.sh
@@ -53,3 +53,5 @@ jruby -S gem install --no-ri --no-rdoc --ignore-dependencies $GEM_DIR/rspec-1.1.
 jruby -S gem install --no-ri --no-rdoc --ignore-dependencies $GEM_DIR/rspec-rails-1.1.12.gem
 jruby -S gem install --no-ri --no-rdoc --ignore-dependencies $GEM_DIR/haml-2.0.7.gem
 jruby -S gem install --no-ri --no-rdoc --ignore-dependencies $GEM_DIR/cucumber-0.1.16.gem
+jruby -S gem install --no-ri --no-rdoc --ignore-dependencies $GEM_DIR/rack-0.9.1.gem
+
