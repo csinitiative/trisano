2d96ba35b7b4f90905e238e8196f5618dd4c30aa
ryan <ryan@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2009-03-05 05:56:26 +0000

    fixed these basic uats
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@2431 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- trisano/webapp/spec/uat/simple_cmr_create_selspec-14.rb
+++ trisano/webapp/spec/uat/simple_cmr_create_selspec-14.rb
@@ -21,9 +21,7 @@ describe "Creating a CMR with minimal data" do
   
   it "should create a CMR when the user provides only the person's last name" do
     @browser.open('/trisano/')
-    click_nav_new_cmr(@browser).should be_true
-    @browser.type('morbidity_event_active_patient__person_last_name','Joker')
-    save_cmr(@browser).should be_true
+    create_simplest_cmr(@browser, 'Joker').should be_true
     @browser.is_text_present("CMR was successfully created.").should be_true
   end
 end
