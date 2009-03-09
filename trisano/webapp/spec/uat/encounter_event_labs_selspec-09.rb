d328438137b53124d157822541293db8c2bdd5ee
ed <ed@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2009-03-09 22:30:03 +0000

    UAT for tri1407.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@2452 b21a84b9-e453-0410-92a3-c4f515f7ae45

---
+++ trisano/webapp/spec/uat/encounter_event_labs_selspec-09.rb
@@ -0,0 +1,78 @@
+# Copyright (C) 2007, 2008, 2009 The Collaborative Software Foundation
+#
+# This file is part of TriSano.
+#
+# TriSano is free software: you can redistribute it and/or modify it under the 
+# terms of the GNU Affero General Public License as published by the 
+# Free Software Foundation, either version 3 of the License, 
+# or (at your option) any later version.
+#
+# TriSano is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License 
+# along with TriSano. If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
+
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe 'Encounter event labs' do
+  
+  # $dont_kill_browser = true
+  
+  before(:all) do
+    @cmr_last_name = get_random_word << " en-l-uat"
+    @disease = get_random_disease
+    @encounter_date = "March 10, 2009"
+    @encounter_description = get_unique_name(3) << " en-l-uat"
+    @lab_name = get_unique_name(1) << " en-l-uat"
+    @lab_test_type_1 = get_unique_name(1) << " en-l-uat"
+    @lab_result_1 = get_unique_name(1) << " en-l-uat"
+  end
+  
+  after(:all) do
+    @cmr_last_name = nil
+    @disease = nil
+    @encounter_date = nil
+    @encounter_description = nil
+    @lab_name = nil
+    @lab_test_type_1 = nil
+    @lab_result_1 = nil
+  end
+
+  it 'should create a basic investigatable CMR' do
+    @browser.open "/trisano"
+    create_basic_investigatable_cmr(@browser, @cmr_last_name, @disease).should be_true
+  end
+
+  it 'should add an encounter' do
+    edit_cmr(@browser)
+    @browser.type('css=input[id$=participations_encounter_attributes_encounter_date]', @encounter_date)
+    @browser.type('css=textarea[id$=participations_encounter_attributes_description]', @encounter_description)
+    save_cmr(@browser)
+    @browser.is_text_present("2009-03-10").should be_true
+    @browser.is_text_present(@encounter_description).should be_true
+  end
+
+  it 'should add a lab to the encounter' do
+    @browser.click("link=Edit encounter event")
+    @browser.wait_for_page_to_load($load_time)
+    @browser.type('css=input[id^=lab_name_]', @lab_name)
+    @browser.type('css=input[id^=test_type_]', @lab_test_type_1)
+    @browser.type('css=input[id^=lab_result_]', @lab_result_1)
+    save_and_exit(@browser)
+    @browser.is_text_present(@lab_name).should be_true
+    @browser.is_text_present(@lab_test_type_1).should be_true
+    @browser.is_text_present(@lab_result_1).should be_true
+  end
+  
+  it 'should show the encounter event labs on the morbidity event' do
+    @browser.click("link=#{@cmr_last_name}")
+    @browser.wait_for_page_to_load($load_time)
+    @browser.is_text_present(@lab_name).should be_true
+    @browser.is_text_present(@lab_test_type_1).should be_true
+    @browser.is_text_present(@lab_result_1).should be_true
+  end
+
+end
