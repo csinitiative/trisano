8c3f221e38bbeedc4666c4fdc0083c834b663174
ed <ed@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2009-03-29 19:22:38 +0000

    Updating UATs for 1.1.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@2546 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- trisano/webapp/spec/uat/fb_admin_delete_follow_ups_selspec-02.rb
+++ trisano/webapp/spec/uat/fb_admin_delete_follow_ups_selspec-02.rb
@@ -55,7 +55,7 @@ describe 'Form Builder Admin Delete Follow-Up Functionality' do
     edit_cmr(@browser)
 
     # Enter the answer that meets the core follow-up condition
-    @browser.select("morbidity_event_active_patient__person_birth_gender_id", "label=Female")
+    add_demographic_info(@browser, { :birth_gender => "Female" })
     click_core_tab(@browser, "Investigation") # This click triggers the onChange that triggers the condition processing
     sleep(2) # Replace this with something better -- need to make sure the round trip to process condition has happened
     @browser.is_text_present(@core_follow_up_question_text).should be_false
