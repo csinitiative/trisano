ee62ae4219943b08c588a18c91652a557ae1bc34
pete <pete@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2009-03-31 01:06:05 +0000

    Updating migrations and schema to work for production upgrades AND locally.
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@2573 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- trisano/webapp/db/schema.rb
+++ trisano/webapp/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 146) do
+ActiveRecord::Schema.define(:version => 156) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "location_id"
