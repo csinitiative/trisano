d1f416a9e0dd5a3daa31060c791004d19920a528
ryan <ryan@b21a84b9-e453-0410-92a3-c4f515f7ae45>
2009-04-01 06:04:28 +0000

    Routing from contact show more and fixes to the production migration
     * tri1441
    
    git-svn-id: https://trisano.csinitiative.net/svn/trisano/top-level/trunk@2582 b21a84b9-e453-0410-92a3-c4f515f7ae45

--- trisano/webapp/db/migrate/20090327154026_add_workflow_state.rb
+++ trisano/webapp/db/migrate/20090327154026_add_workflow_state.rb
@@ -1,32 +1,90 @@
 class AddWorkflowState < ActiveRecord::Migration
+  extend MigrationHelpers
   def self.up
-    Event.transaction do
+    ActiveRecord::Base.transaction do
       add_column :events, :workflow_state, :string
 
-      if RAILS_ENV == 'production'
-        MorbidityEvent.update_all("workflow_state = 'new'", "event_status = 'NEW'")
-        MorbidityEvent.update_all("workflow_state = 'assigned_to_lhd'", "event_status = 'ASGD-LHD'")
-        MorbidityEvent.update_all("workflow_state = 'accepted_by_lhd'", "event_status = 'ACPTD-LHD'")
-        MorbidityEvent.update_all("workflow_state = 'rejected_by_lhd'", "event_status = 'RJCTD-LHD'")
-        MorbidityEvent.update_all("workflow_state = 'assigned_to_investigator'", "event_status = 'ASGD-INV'")
-        MorbidityEvent.update_all("workflow_state = 'under_investigation'", "event_status = 'UI'")
-        MorbidityEvent.update_all("workflow_state = 'rejected_by_investigator'", "event_status = 'RJCTD-INV'")
-        MorbidityEvent.update_all("workflow_state = 'investigation_complete'", "event_status = 'IC'")
-        MorbidityEvent.update_all("workflow_state = 'approved_by_lhd'", "event_status = 'APP-LHD'")
-        MorbidityEvent.update_all("workflow_state = 'reopened_by_manager'", "event_status = 'RO-MGR'")
-        MorbidityEvent.update_all("workflow_state = 'closed'", "event_status = 'CLOSED'")
-        MorbidityEvent.update_all("workflow_state = 'reopened_by_state'", "event_status = 'RO-STATE'")
+      if RAILS_ENV == 'production'        
+        execute <<-SQL
+          update events set workflow_state = 'new' 
+          where event_status = 'NEW' and type = 'MorbidityEvent'
+        SQL
 
-        ContactEvent.update_all("workflow_state = 'new'")
+        execute <<-SQL
+          update events set workflow_state = 'assigned_to_lhd'
+          where event_status = 'ASGD-LHD' and type = 'MorbidityEvent'
+        SQL
         
-        CoreField.update_all "key = 'morbidity_event[workflow_state]'", 
-                             "key = 'morbidity_event[event_status]'"
+        execute <<-SQL
+          update events set workflow_state = 'accepted_by_lhd'
+          where event_status = 'ACPTD-LHD' and type = 'MorbidityEvent'
+        SQL
 
-        CsvField.update_all "long_name = 'patient_workflow_state', use_description = 'workflow_state'",
-                            "long_name = 'patient_event_status'"
+        execute <<-SQL
+          update events set workflow_state = 'rejected_by_lhd' 
+          where event_status = 'RJCTD-LHD' and type = 'MorbidityEvent'
+        SQL
 
-        ExportColumn.update_all "column_name = 'workflow_state'",
-                                "column_name = 'event_status'"
+        execute <<-SQL
+          update events set workflow_state = 'assigned_to_investigator'
+          where event_status = 'ASGD-INV' and type = 'MorbidityEvent'
+        SQL
+        
+        execute <<-SQL
+          update events set workflow_state = 'under_investigation'
+          where event_status = 'UI' and type = 'MorbidityEvent'
+        SQL
+
+        execute <<-SQL
+          update events set workflow_state = 'rejected_by_investigator'
+          where event_status = 'RJCTD-INV' and type = 'MorbidityEvent'
+        SQL
+
+        execute <<-SQL
+          update events set workflow_state = 'investigation_complete'
+          where event_status = 'IC' and type = 'MorbidityEvent'
+        SQL
+
+        execute <<-SQL
+          update events set workflow_state = 'approved_by_lhd'
+          where event_status = 'APP-LHD' and type = 'MorbidityEvent'
+        SQL
+
+        execute <<-SQL
+          update events set workflow_state = 'reopened_by_manager'
+          where event_status = 'RO-MGR' and type = 'MorbidityEvent'
+        SQL
+
+        execute <<-SQL
+          update events set workflow_state = 'closed'
+          where event_status = 'CLOSED' and type = 'MorbidityEvent'
+        SQL
+
+        execute <<-SQL
+          update events set workflow_state = 'reopened_by_state'
+          where event_status = 'RO-STATE' and type = 'MorbidityEvent'
+        SQL
+
+        execute <<-SQL
+          update events set workflow_state = 'new'
+          where type = 'ContactEvent'
+        SQL
+        
+        execute <<-SQL
+          update core_fields set key = 'morbidity_event[workflow_state]'
+          where key = 'morbidity_event[event_status]'
+        SQL
+
+        execute <<-SQL
+          update csv_fields set long_name = 'patient_workflow_state', 
+                                use_description = 'workflow_state'
+          where long_name = 'patient_event_status'
+        SQL
+
+        execute <<-SQL
+          update export_columns set column_name = 'workflow_state'
+          where column_name = 'event_status'
+        SQL
       end
       
       remove_column :events, :event_status
