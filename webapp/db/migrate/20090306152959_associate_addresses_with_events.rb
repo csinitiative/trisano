d908df698a05b45941e128c1556fb25419e915e7
Pete Lacey <placey@csinitiative.com>
2009-04-24 11:04:56 -0400

    Attempting to fix timeout on build server by indexing addresses|event_id

--- webapp/db/migrate/20090306152959_associate_addresses_with_events.rb
+++ webapp/db/migrate/20090306152959_associate_addresses_with_events.rb
@@ -4,6 +4,7 @@ class AssociateAddressesWithEvents < ActiveRecord::Migration
   def self.up
     add_column :addresses, :event_id, :integer
     add_foreign_key :addresses, :event_id, :events
+    add_index :addresses, :event_id
 
     if RAILS_ENV == 'production'
       eval(File.read("#{RAILS_ROOT}/script/associate_addresses_to_events.rb"))
