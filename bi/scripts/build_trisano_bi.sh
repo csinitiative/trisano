6b2a8b49b0ee106f15804b9ee4aecaa54e3bdbef
Pete Lacey <placey@csinitiative.com>
2009-04-27 14:38:20 -0400

    Changes BI Server to use  Postgres by default
     * Removes Hypersonic SQL as default data store
     * Makes admin console use Postgres too
     * Updates BI Server pacakging script
     * Partial commit towards tri1656: Packaging BI Server for Trisano

--- bi/scripts/build_trisano_bi.sh
+++ bi/scripts/build_trisano_bi.sh
@@ -44,11 +44,40 @@ fi
 
 # Step 1: Copy XML config files
 echo "Configuring BI Server to use SiteMinder"
+
+# Backup originals
 cp $BI_SERVER_HOME/pentaho-solutions/system/applicationContext-acegi-security.xml $BI_SERVER_HOME/pentaho-solutions/system/applicationContext-acegi-security.xml.org
 cp $BI_SERVER_HOME/pentaho-solutions/system/pentaho-spring-beans.xml $BI_SERVER_HOME/pentaho-solutions/system/pentaho-spring-beans.xml.org
 cp $BI_SERVER_HOME/pentaho-solutions/system/pentaho.xml $BI_SERVER_HOME/pentaho-solutions/system/pentaho.xml.org
-cp $TRISANO_SOURCE_HOME/bi/server_config_files/* $BI_SERVER_HOME/pentaho-solutions/system/
+
+# Copy in new ones
+cp $TRISANO_SOURCE_HOME/bi/bi_server_replacement_files/applicationContext-acegi-security-siteminder.xml $BI_SERVER_HOME/pentaho-solutions/system/
+cp $TRISANO_SOURCE_HOME/bi/bi_server_replacement_files/applicationContext-acegi-security.xml $BI_SERVER_HOME/pentaho-solutions/system/
+cp $TRISANO_SOURCE_HOME/bi/bi_server_replacement_files/pentaho-spring-beans.xml $BI_SERVER_HOME/pentaho-solutions/system/
+cp $TRISANO_SOURCE_HOME/bi/bi_server_replacement_files/pentaho.xml $BI_SERVER_HOME/pentaho-solutions/system/
 
 # Step 2: Copy custom jar files
 echo "Copying Trisano custom java extensions to BI Server"
 cp $TRISANO_SOURCE_HOME/bi/extensions/trisano/dist/* $BI_SERVER_HOME/tomcat/webapps/pentaho/WEB-INF/lib
+
+# Step 3: Configure BI for Postgres
+echo "Configuring BI Server to use PostgreSQL"
+# Backup originals
+cp $BI_SERVER_HOME/start-pentaho.sh $BI_SERVER_HOME/start-pentaho.sh.org
+cp $BI_SERVER_HOME/stop-pentaho.sh $BI_SERVER_HOME/stop-pentaho.sh.org
+cp $BI_SERVER_HOME/pentaho-solutions/system/quartz/quartz.properties  $BI_SERVER_HOME/pentaho-solutions/system/quartz/quartz.properties.org 
+cp $BI_SERVER_HOME/pentaho-solutions/system/hibernate/hibernate-settings.xml $BI_SERVER_HOME/pentaho-solutions/system/hibernate/hibernate-settings.xml.org
+cp $BI_SERVER_HOME/tomcat/webapps/pentaho/META-INF/context.xml $BI_SERVER_HOME/tomcat/webapps/pentaho/META-INF/context.xml.org
+
+# Copy in new ones
+cp $TRISANO_SOURCE_HOME/bi/bi_server_replacement_files/start-pentaho.sh $BI_SERVER_HOME
+cp $TRISANO_SOURCE_HOME/bi/bi_server_replacement_files/stop-pentaho.sh $BI_SERVER_HOME
+cp $TRISANO_SOURCE_HOME/bi/bi_server_replacement_files/quartz.properties $BI_SERVER_HOME/pentaho-solutions/system/quartz/
+cp $TRISANO_SOURCE_HOME/bi/bi_server_replacement_files/hibernate-settings.xml $BI_SERVER_HOME/pentaho-solutions/system/hibernate
+cp $TRISANO_SOURCE_HOME/bi/bi_server_replacement_files/context.xml $BI_SERVER_HOME/tomcat/webapps/pentaho/META-INF
+
+# Step 5: Customize admin console
+# Add Postgres JDBC driver to admin-console
+cp $BI_SERVER_HOME/tomcat/common/lib/postgresql-8.2-504.jdbc3.jar $ADMIN_CONSOLE_HOME/jdbc/
+
+
