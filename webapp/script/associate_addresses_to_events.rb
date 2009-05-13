226bb6c85de0a7ac412218bbc3a7a48de65960f6
Pete Lacey <placey@csinitiative.com>
2009-05-13 10:42:53 -0400

    Attempting to speed up address migration

--- webapp/script/associate_addresses_to_events.rb
+++ webapp/script/associate_addresses_to_events.rb
@@ -1,23 +1,34 @@
 puts 'Generating longitudinal address data'
 
-InterestedParty.find(:all).each do |ip|
-  unless ip.event.try(:address)
-    address = ip.person_entity.addresses.first
-    if address
-      address = address.clone if address.event
-      address.event_id =  ip.event_id
-      address.save!
+puts "  * Processing human events"
+i = 0
+HumanEvent.find_in_batches(:include => [ :interested_party => { :person_entity => :addresses } ], :conditions => "participations.id IS NOT NULL", :batch_size => 500 ) do |event_group|
+  i += 1
+  puts "    * Processing #{i.ordinalize} group of 500 human events"
+  event_group.each do | event |
+    unless event.try(:address)
+      address = event.interested_party.person_entity.addresses.first
+      if address
+        address = address.clone if address.event
+        address.event_id =  event.id
+        address.save!
+      end
     end
   end
 end
 
-InterestedPlace.find(:all).each do |ip|
-  unless ip.event.try(:address)
-    address = ip.place_entity.addresses.first
-    if address
-      address = address.clone if address.event
-      address.event_id = ip.event_id
-      address.save!
+i = 0
+PlaceEvent.find_in_batches(:include => [ :interested_place => { :place_entity => :addresses } ], :conditions => "participations.id IS NOT NULL", :batch_size => 500 ) do |event_group|
+  i += 1
+  puts "    * Processing #{i.ordinalize} group of 500 place events"
+  event_group.each do | event |
+    unless event.try(:address)
+      address = event.interested_place.place_entity.addresses.first
+      if address
+        address = address.clone if address.event
+        address.event_id =  event.id
+        address.save!
+      end
     end
   end
 end
