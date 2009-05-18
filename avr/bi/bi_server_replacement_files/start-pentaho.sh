76cb70cf205e98857b39eed75a582f7e3db64f29
Pete Lacey <placey@csinitiative.com>
2009-05-18 13:22:45 -0400

    Adds headless AWT option to start up. Allows Jasper to run reports when not started as the X11 session owner.

--- avr/bi/bi_server_replacement_files/start-pentaho.sh
+++ avr/bi/bi_server_replacement_files/start-pentaho.sh
@@ -13,6 +13,6 @@ if [ "$?" = 0 ]; then
   cd data
   # sh start_hypersonic.sh &
   cd ../tomcat/bin
-  export CATALINA_OPTS="-Xms256m -Xmx768m -XX:MaxPermSize=256m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000"
+  export CATALINA_OPTS="-Djava.awt.headless=true -Xms256m -Xmx768m -XX:MaxPermSize=256m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000"
   sh startup.sh
 fi
