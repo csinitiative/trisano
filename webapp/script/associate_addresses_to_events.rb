157ac52e3902633f300d3d209e765fc8217640be
Pete Lacey <placey@csinitiative.com>
2009-05-28 11:10:32 -0400

    Fixes address migration that was leaving contact addresses behind.

--- webapp/script/associate_addresses_to_events.rb
+++ webapp/script/associate_addresses_to_events.rb
@@ -2,7 +2,7 @@ puts 'Generating longitudinal address data'
 
 puts "  * Processing human events"
 i = 0
-HumanEvent.find_in_batches(:include => [ :interested_party => { :person_entity => :addresses } ], :conditions => "participations.id IS NOT NULL", :batch_size => 500 ) do |event_group|
+MorbidityEvent.find_in_batches(:include => [ :interested_party => { :person_entity => :addresses } ], :conditions => "participations.id IS NOT NULL", :batch_size => 500 ) do |event_group|
   i += 1
   puts "    * Processing #{i.ordinalize} group of 500 human events"
   event_group.each do | event |
@@ -16,6 +16,22 @@ HumanEvent.find_in_batches(:include => [ :interested_party => { :person_entity =
     end
   end
 end
+
+ContactEvent.find_in_batches(:include => [ :interested_party => { :person_entity => :addresses } ], :conditions => "participations.id IS NOT NULL", :batch_size => 500 ) do |event_group|
+  i += 1
+  puts "    * Processing #{i.ordinalize} group of 500 human events"
+  event_group.each do | event |
+    unless event.try(:address)
+      address = event.interested_party.person_entity.addresses.first
+      if address
+        address = address.clone if address.event
+        address.event_id =  event.id
+        address.save!
+      end
+    end
+  end
+end
+
 
 i = 0
 PlaceEvent.find_in_batches(:include => [ :interested_place => { :place_entity => :addresses } ], :conditions => "participations.id IS NOT NULL", :batch_size => 500 ) do |event_group|
