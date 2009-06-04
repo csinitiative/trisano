0838d3432bdb15b85f0ce095a1e941c9480cdee6
Pete Lacey <placey@csinitiative.com>
2009-06-04 09:49:35 -0400

    Reworks lab messages migration to add needed and anticipated fields
     * Exercises the right to alter the most recent migration.
     * DB rebuild is required.

--- webapp/db/migrate/20090504140839_create_lab_messages.rb
+++ webapp/db/migrate/20090504140839_create_lab_messages.rb
@@ -1,13 +1,16 @@
 class CreateLabMessages < ActiveRecord::Migration
   def self.up
-    create_table :lab_messages do |t|
+    create_table :staged_messages do |t|
       t.string :hl7_message, :limit => 10485760, :null => false
-
+      t.string :state, :null => false
+      t.string :message_tpye
+      t.text :error_detail
+      t.text :note
       t.timestamps
     end
   end
 
   def self.down
-    drop_table :lab_messages
+    drop_table :staged_messages
   end
 end
