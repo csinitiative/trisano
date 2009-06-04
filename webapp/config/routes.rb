bef133604913a5e7a8397925783dd18e80a1f466
Pete Lacey <placey@csinitiative.com>
2009-06-04 11:05:35 -0400

    Bootstrapping the staging area
     * Adds a new route: staged_messages
     * Adds minimal control and index view
     * Adds "STAGING AREA" item to main menu
     * Initial fixtures

--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -150,6 +150,8 @@ ActionController::Routing::Routes.draw do |map|
   
   map.resources :csv_fields
 
+  map.resources :staged_messages
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
