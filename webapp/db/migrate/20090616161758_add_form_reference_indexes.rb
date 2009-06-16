7994ebbfa0f0f2c7782caae253e7330d5b0043f0
Ed Copony <ecopony@yahoo.com>
2009-06-16 10:17:56 -0700

    Wrapping up 792794: True duplicate form prevention * Adds unique contraint on event_id/template_id to form references to prevent duplicate form assignment (including older versions) at the database level.
    http://www.pivotaltracker.com/story/show/792794

---
+++ webapp/db/migrate/20090616161758_add_form_reference_indexes.rb
@@ -0,0 +1,28 @@
+# Copyright (C) 2007, 2008, 2009 The Collaborative Software Foundation
+#
+# This file is part of TriSano.
+#
+# TriSano is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# TriSano is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with TriSano. If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
+
+class AddFormReferenceIndexes < ActiveRecord::Migration
+  def self.up
+    execute("CREATE INDEX f_template_id_index ON forms USING btree (template_id)")
+    execute("CREATE UNIQUE INDEX index_form_references_on_event_id_and_template_id ON form_references USING btree (event_id, template_id)")
+  end
+
+  def self.down
+    execute("DROP INDEX f_template_id_index")
+    execute("DROP INDEX index_form_references_on_event_id_and_template_id")
+  end
+end
