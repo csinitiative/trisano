1aed6ef3870d5974161f19d55d7f21a3f4fa12e2
Gregory Jones <greg@csinitiative.com>
2009-07-14 01:54:11 -0400

    Incremental commit for web api including auth (basic and siteminder) bootstrap and an example client list_usernames.rb

---
+++ web-api-client/lib/TriSanoWebApi.rb
@@ -0,0 +1,48 @@
+require 'rubygems'
+require 'mechanize'
+
+class TriSanoWebApi 
+  attr_accessor :agent
+
+  def initialize
+    @agent = WWW::Mechanize.new { |a|
+      a.user_agent = 'TriSano Web API/1.0'
+    }
+
+    @base_url = ENV['TRISANO_BASE_URL'] || raise('Missing TRISANO_BASE_URL environment variable')
+
+    if !ENV['TRISANO_API_AUTH'].nil?
+      username = ENV['TRISANO_API_USER'] || raise('Missing TRISANO_API_USER environment variable')
+      password = ENV['TRISANO_API_PASS'] || raise('Missing TRISANO_API_PASS environment variable')
+    end
+
+    if ENV['TRISANO_API_AUTH'] == 'basic' then
+      @agent.auth(username, password)
+    elsif ENV['TRISANO_API_AUTH'] == 'siteminder' then
+      auth_url = ENV['TRISANO_SM_URL'] || raise('Missing TRISANO_SM_URL environment variable')
+
+      # Generate Siteminder cookies
+      @agent.post(auth_url, { 'LoginID' => username,
+                              'Password' => password,
+                              'Dispatch' => 'loginAuth'});
+    end
+    
+  end
+
+  def get(uri)
+    @agent.get(@base_url + uri)
+  end
+
+  def post(uri)
+    @agent.post(@base_url + uri)
+  end
+
+  def delete(uri)
+    @agent.delete(@base_url + uri)
+  end
+
+  def put(uri)
+    @agent.put(@base_url + uri)
+  end
+
+end
---
+++ web-api-client/list_usernames.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH << './lib'
+require 'TriSanoWebApi.rb'
+
+trisano = TriSanoWebApi.new
+
+# this is just a test.  class names should identify the data we want more precisely.
+page = trisano.get('/users').search(".//td[@class='forminformation']/div[@class='tools']/text()[normalize-space(.)]")
+
+page.each { |u|
+  puts u.text.strip
+}
