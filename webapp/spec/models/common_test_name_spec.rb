e868e5baef4e63660d7f303bc7653954d1be0da6
Ryan L. Bell <ryan.l.bell@gmail.com>
2009-07-29 22:40:42 -0400

    Added common_test_name table and model.
    http://www.pivotaltracker.com/story/show/1007277
    
     * Decided to make the common_name field unique. Couldn't think of a
       reason it shouldn't be.

---
+++ webapp/spec/models/common_test_name_spec.rb
@@ -0,0 +1,43 @@
+# Copyright (C) 2007, 2008, 2009 The Collaborative Software Foundation
+#
+# This file is part of TriSano.
+#
+# TriSano is free software: you can redistribute it and/or modify it under the 
+# terms of the GNU Affero General Public License as published by the 
+# Free Software Foundation, either version 3 of the License, 
+# or (at your option) any later version.
+#
+# TriSano is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License 
+# along with TriSano. If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
+
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe CommonTestName do
+
+  describe 'common name' do
+
+    it 'should be unique' do
+      CommonTestName.create :common_name => 'Doit'
+      lambda {CommonTestName.create! :common_name => 'Doit'}.should raise_error
+      CommonTestName.new(:common_name => 'Doit').should_not be_valid
+      CommonTestName.new(:common_name => 'Do something else').should be_valid
+    end
+
+    it 'should not be null' do
+      lambda {CommonTestName.create!}.should raise_error
+      CommonTestName.new.should_not be_valid
+    end
+
+    it 'should not be longer then 255 chars' do
+      CommonTestName.new(:common_name => ('c' * 256)).should_not be_valid
+      CommonTestName.new(:common_name => ('c' * 255)).should be_valid
+    end
+
+  end
+
+end
