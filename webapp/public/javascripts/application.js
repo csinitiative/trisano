e4336c3beeac9c18857f01e5f342de016c255821
Pete Lacey <placey@csinitiative.com>
2009-08-05 14:50:13 -0400

    Fixes bug where Ajax 'more choices' was not prepending the right URL path.

--- webapp/public/javascripts/application.js
+++ webapp/public/javascripts/application.js
@@ -320,8 +320,8 @@ function focus_init() {
     myTabs.addListener("activeIndexChange", focus_first);
 }
 
-function get_test_type_options(select_list) {
+function get_test_type_options(select_list, url) {
   if (select_list.options[select_list.selectedIndex].text == "More choices...") {
-    new Ajax.Updater(select_list,  '/events/test_type_options', {asynchronous:true, evalScripts:true})
+    new Ajax.Updater(select_list,  url, {asynchronous:true, method: 'get'})
   }
 }
