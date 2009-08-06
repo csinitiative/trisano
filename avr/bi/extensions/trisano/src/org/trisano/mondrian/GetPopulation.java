ee52cf36fff37a8de5f0f1d235daeb37c806d8a1
Joshua Tolley <josh@endpoint.com>
2009-08-06 12:05:28 -0600

    Change population code to use multiple tables
    Now the OLAP GetPopulation UDF can use mulitple tables in the population data
    set. These tables are ranked by their assumed accuracy, and selected based on
    the highest ranked table that can provide all the dimensions needed. Code to
    actually create those tables will follow shortly.

--- avr/bi/extensions/trisano/src/org/trisano/mondrian/GetPopulation.java
+++ avr/bi/extensions/trisano/src/org/trisano/mondrian/GetPopulation.java
@@ -5,7 +5,7 @@ import mondrian.olap.type.*;
 import mondrian.spi.UserDefinedFunction;
 import org.apache.log4j.Logger;
 import java.sql.*;
-import java.util.ArrayList;
+import java.util.*;
 
 public class GetPopulation implements UserDefinedFunction {
     private static final Logger logger = Logger.getLogger(GetPopulation.class);
@@ -29,6 +29,153 @@ public class GetPopulation implements UserDefinedFunction {
     }
 
     public Object execute(Evaluator evaluator, UserDefinedFunction.Argument[] arguments) {
+        java.sql.Connection conn = null;
+        List<Map<String, String>> dimensions = new ArrayList<Map<String, String>>();
+        ResultSet rs;
+        String column_names = new String("");
+        String table_name, query;
+        Integer table_rank, population = 0;
+        ArrayList<String> params = new ArrayList<String>();
+        Boolean doneOne = false;
+
+        try {
+            conn = evaluator.getQuery().getConnection().getDataSource().getConnection();
+            PreparedStatement st =
+                conn.prepareStatement("SELECT dim_cols[?], mapping_func[?] FROM population.population_dimensions WHERE dim_name = ?");
+
+            // Get data on all necessary dimensions
+            for (Dimension d : evaluator.getCube().getDimensions()) {
+                Integer depth;
+
+                logger.debug("checking dimension " + d.getName());
+                HashMap<String, String> dimhash = new HashMap<String, String>();
+                depth = evaluator.getContext(d).getLevel().getDepth();
+
+                st.setInt(1, depth);
+                st.setInt(2, depth);
+                st.setString(3, d.getName());
+                rs = st.executeQuery();
+                if (rs.next() && depth != 0) {
+                    // rs.next() is true only if it returned something, or in
+                    // other words, only when this dimension is important to
+                    // us. We also only care when depth is nonzero
+                    dimhash.put("name", d.getName());
+                    dimhash.put("depth", depth.toString());
+                    dimhash.put("value", evaluator.getContext(d).getName());
+                    dimhash.put("column", rs.getString(1));
+                    dimhash.put("mapper", rs.getString(2));
+                    dimensions.add(dimhash);
+                    if (! column_names.equals(""))
+                        column_names += ", ";
+                    column_names += "'" + dimhash.get("column") + "'";
+                    logger.info("Dimension " + d.getName() + " is useful, so keeping. Column: " + dimhash.get("column") + "\tMapper: " + dimhash.get("mapper"));
+                    logger.debug("column names list now == " + column_names);
+                }
+            }
+
+            // Figure out what table to use
+            // TODO: Note that there's some room for SQL injection problems here, if column names are stupid
+            if (dimensions.size() != 0) {
+                query =
+                    "SELECT table_name, table_rank FROM (                \n" +
+                    "    SELECT                                          \n" +
+                    "        ppt.table_name,                             \n" +
+                    "        COUNT(*),                                   \n" +
+                    "        ppt.table_rank                              \n" +
+                    "    FROM                                            \n" +
+                    "        population.population_tables ppt            \n" +
+                    "        JOIN information_schema.columns isc         \n" +
+                    "            ON (                                    \n" +
+                    "                isc.table_name = ppt.table_name AND \n" +
+                    "                isc.table_schema = 'population'     \n" +
+                    "            )                                       \n" +
+                    "    WHERE                                           \n" +
+                    "       isc.column_name IN (" + column_names + ")    \n" +
+                    "    GROUP BY                                        \n" +
+                    "        ppt.table_name,                             \n" +
+                    "        ppt.table_rank                              \n" +
+                    "    HAVING                                          \n" +
+                    "        COUNT(*) = ?                                \n" +
+                    ") f                                                 \n" +
+                    "ORDER BY table_rank ASC LIMIT 1;";
+                logger.debug("For finding table, using query : " + query);
+                st = conn.prepareStatement(query);
+                //st.setString(1, column_names);
+                st.setInt(1, dimensions.size());
+                rs = st.executeQuery();
+                if (rs.next()) {
+                    table_name = rs.getString(1);
+                    table_rank = rs.getInt(2);
+                }
+                else {
+                    throw new RuntimeException("Couldn't find a table to use");
+                }
+            }
+            else {
+                logger.debug("No dimensions specified; using highest-ranking table");
+                st = conn.prepareStatement("SELECT table_name, table_rank FROM population.population_tables ORDER BY table_rank ASC LIMIT 1");
+                rs = st.executeQuery();
+                if (rs.next()) {
+                    table_name = rs.getString(1);
+                    table_rank = rs.getInt(2);
+                }
+                else {
+                    throw new RuntimeException("Couldn't find a default table to use");
+                }
+                if (table_name == null || table_name.equals("")) 
+                    throw new RuntimeException("Couldn't find a default table to use");
+            }
+            logger.info("Pulling data from table " + table_name + " with rank " + table_rank.toString());
+
+            // Build query
+            query = "SELECT COALESCE(SUM(population), 0) FROM population." + table_name;
+            if (dimensions.size() > 0) {
+                query += " WHERE";
+                for (Map<String, String> m : dimensions) {
+                    if (doneOne) 
+                        query += " AND";
+                    query += " " + m.get("column") + " = ";
+                    if (m.get("mapper") == null) 
+                        query += "?";
+                    else
+                        query += m.get("mapper") + "(?)";
+
+                    params.add(m.get("value"));
+                    doneOne = true;
+                }
+            }
+            
+            logger.info("Using query : " + query);
+            st = conn.prepareStatement(query);
+            int i = 1;
+            for (String s : params) {
+                st.setString(i, s);
+                i++;
+            }
+            rs = st.executeQuery();
+            rs.next();
+            population = rs.getInt(1);
+            logger.debug("Population: " + population);
+        }
+        catch (SQLException s) {
+            logger.error("JDBC Error", s);
+        }
+
+        try {
+            if (conn != null)
+                conn.close();
+        }
+        catch (SQLException s) {
+            logger.error("JDBC Error when disconnecting: ", s);
+        }
+
+        if (population == 0)
+            return null;
+        else
+            return population;
+    }
+
+/*    public Object execute(Evaluator evaluator, UserDefinedFunction.Argument[] arguments) {
         // Find the current level for dimensions that we care about for
         // population statistics, and calculate the total population given
         // those dimensions. Right now those dimensions are:
@@ -115,7 +262,7 @@ public class GetPopulation implements UserDefinedFunction {
             return null;
         else
             return population;
-    }
+    } */
 
     public String[] getReservedWords() { return null; }
 }
