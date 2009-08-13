b10499a1d7dcf0f49f1444702f0dbd51e48bad7b
Pete Lacey <placey@csinitiative.com>
2009-08-13 11:21:23 -0400

    Some lab house keeping
      * Break lab results migration into 2 parts: non-destructive and
        destructive.  The plan is to place the prod migration in between
      * Adjust test status codes to the SME's latest list
      * Fix a couple of minor errors in the core_fields list
      * Give visual clue that test_result and result_value are mutually
        exclusive

--- webapp/db/migrate/20090730164224_update_lab_results.rb
+++ webapp/db/migrate/20090730164224_update_lab_results.rb
@@ -2,11 +2,8 @@ class UpdateLabResults < ActiveRecord::Migration
  extend MigrationHelpers
 
   def self.up
-    remove_column :lab_results, :test_type
     add_column :lab_results, :test_type_id, :integer
-    remove_column :lab_results, :test_detail
-    remove_column :lab_results, :lab_result_text
-    rename_column :lab_results, :interpretation_id, :test_result_id
+    add_column :lab_results, :test_result_id, :integer
     add_column :lab_results, :result_value, :string
     add_column :lab_results, :units, :string, :limit => 50
     rename_column :lab_results, :specimen_sent_to_uphl_yn_id, :specimen_sent_to_state_id
@@ -18,11 +15,8 @@ class UpdateLabResults < ActiveRecord::Migration
   end
 
   def self.down
-    add_column :lab_results, :test_type, :string
     remove_column :lab_results, :test_type_id
-    add_column :lab_results, :test_detail, :string
-    add_column :lab_results, :lab_result_text, :text
-    rename_column :lab_results, :test_result_id, :interpretation_id
+    remove_column :lab_results, :interpretation_id
     remove_column :lab_results, :result_value
     remove_column :lab_results, :units
     rename_column :lab_results, :specimen_sent_to_state_id, :specimen_sent_to_uphl_yn_id
