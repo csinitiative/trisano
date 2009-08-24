61ab93219337b42cc58dfc672f54c471a557d700
ecopony <ecopony@yahoo.com>
2009-08-24 09:45:39 -0700

    Busting out counties from the rest of the codes for clean installs.

--- webapp/script/load_test_and_demo_data.rb
+++ webapp/script/load_test_and_demo_data.rb
@@ -15,6 +15,22 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with TriSano. If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
 
+county_codes = YAML::load_file "#{RAILS_ROOT}/db/defaults/counties.yml"
+
+# Can't simply delete all and insert as the delete may trigger a FK constraint
+Code.transaction do
+  county_codes.each do |code|
+
+    c = ExternalCode.find_or_initialize_by_code_name_and_the_code(:code_name => code['code_name'],
+      :the_code => code['the_code'],
+      :code_description => code['code_description'],
+      :sort_order => code['sort_order'])
+    
+    c.attributes = code unless c.new_record?
+    c.save!
+  end
+end
+
 # Hospitals are represented as an array of strings
 
 hospitals = YAML::load_file "#{RAILS_ROOT}/db/defaults/hospitals.yml"
@@ -106,13 +122,13 @@ User.transaction do
   state_manager_role = Role.find_by_role_name("State Manager").id
 
   data_entry_tech.update_attributes( { :role_membership_attributes => [{ :role_id => data_entry_tech_role, :jurisdiction_id => bear_river },
-                                     { :role_id => data_entry_tech_role, :jurisdiction_id => unassigned }] } )
+        { :role_id => data_entry_tech_role, :jurisdiction_id => unassigned }] } )
 
   surveillance_mgr.update_attributes( { :role_membership_attributes => [{ :role_id => surveillance_mgr_role, :jurisdiction_id => bear_river },
-                                      { :role_id => surveillance_mgr_role, :jurisdiction_id => unassigned }] } )
+        { :role_id => surveillance_mgr_role, :jurisdiction_id => unassigned }] } )
 
   investigator.update_attributes( { :role_membership_attributes => [{ :role_id => investigator_role, :jurisdiction_id => bear_river },
-                                  { :role_id => investigator_role, :jurisdiction_id => unassigned }] } )
+        { :role_id => investigator_role, :jurisdiction_id => unassigned }] } )
 
   lhd_manager.update_attributes( { :role_membership_attributes => [{ :role_id => lhd_manager_role, :jurisdiction_id => bear_river }] } )
 
@@ -123,33 +139,33 @@ end
 # Load county to jurisdiction relationships
 ExternalCode.transaction do
   [{:county_name => 'Box Elder',  :health_district => 'Bear River'},
-   {:county_name => 'Cache',      :health_district => 'Bear River'},
-   {:county_name => 'Rich',       :health_district => 'Bear River'},
-   {:county_name => 'Juab',       :health_district => 'Central Utah'},
-   {:county_name => 'Millard',    :health_district => 'Central Utah'},
-   {:county_name => 'Piute',      :health_district => 'Central Utah'},
-   {:county_name => 'Sevier',     :health_district => 'Central Utah'},
-   {:county_name => 'Sanpete',    :health_district => 'Central Utah'},
-   {:county_name => 'Wayne',      :health_district => 'Central Utah'},
-   {:county_name => 'Beaver',     :health_district => 'Southwest Utah'},
-   {:county_name => 'Garfield',   :health_district => 'Southwest Utah'},
-   {:county_name => 'Iron',       :health_district => 'Southwest Utah'},
-   {:county_name => 'Kane',       :health_district => 'Southwest Utah'},
-   {:county_name => 'Washington', :health_district => 'Southwest Utah'},
-   {:county_name => 'Davis',      :health_district => 'Davis County'},
-   {:county_name => 'Salt Lake',  :health_district => 'Salt Lake Valley'},
-   {:county_name => 'Carbon',     :health_district => 'Southeastern Utah'},
-   {:county_name => 'Emery',      :health_district => 'Southeastern Utah'},
-   {:county_name => 'Grand',      :health_district => 'Southeastern Utah'},
-   {:county_name => 'San Juan',   :health_district => 'Southeastern Utah'},
-   {:county_name => 'Summit',     :health_district => 'Summit County'},
-   {:county_name => 'Tooele',     :health_district => 'Tooele County'},
-   {:county_name => 'Uintah',     :health_district => 'TriCounty'},
-   {:county_name => 'Daggett',    :health_district => 'TriCounty'},
-   {:county_name => 'Duchesne',   :health_district => 'TriCounty'},
-   {:county_name => 'Utah',       :health_district => 'Utah County'},
-   {:county_name => 'Weber',      :health_district => 'Weber-Morgan'},
-   {:county_name => 'Morgan',     :health_district => 'Weber-Morgan'}
+    {:county_name => 'Cache',      :health_district => 'Bear River'},
+    {:county_name => 'Rich',       :health_district => 'Bear River'},
+    {:county_name => 'Juab',       :health_district => 'Central Utah'},
+    {:county_name => 'Millard',    :health_district => 'Central Utah'},
+    {:county_name => 'Piute',      :health_district => 'Central Utah'},
+    {:county_name => 'Sevier',     :health_district => 'Central Utah'},
+    {:county_name => 'Sanpete',    :health_district => 'Central Utah'},
+    {:county_name => 'Wayne',      :health_district => 'Central Utah'},
+    {:county_name => 'Beaver',     :health_district => 'Southwest Utah'},
+    {:county_name => 'Garfield',   :health_district => 'Southwest Utah'},
+    {:county_name => 'Iron',       :health_district => 'Southwest Utah'},
+    {:county_name => 'Kane',       :health_district => 'Southwest Utah'},
+    {:county_name => 'Washington', :health_district => 'Southwest Utah'},
+    {:county_name => 'Davis',      :health_district => 'Davis County'},
+    {:county_name => 'Salt Lake',  :health_district => 'Salt Lake Valley'},
+    {:county_name => 'Carbon',     :health_district => 'Southeastern Utah'},
+    {:county_name => 'Emery',      :health_district => 'Southeastern Utah'},
+    {:county_name => 'Grand',      :health_district => 'Southeastern Utah'},
+    {:county_name => 'San Juan',   :health_district => 'Southeastern Utah'},
+    {:county_name => 'Summit',     :health_district => 'Summit County'},
+    {:county_name => 'Tooele',     :health_district => 'Tooele County'},
+    {:county_name => 'Uintah',     :health_district => 'TriCounty'},
+    {:county_name => 'Daggett',    :health_district => 'TriCounty'},
+    {:county_name => 'Duchesne',   :health_district => 'TriCounty'},
+    {:county_name => 'Utah',       :health_district => 'Utah County'},
+    {:county_name => 'Weber',      :health_district => 'Weber-Morgan'},
+    {:county_name => 'Morgan',     :health_district => 'Weber-Morgan'}
   ].each do |relationship|
     begin
       code = ExternalCode.find_by_code_name_and_code_description('county', relationship[:county_name])
