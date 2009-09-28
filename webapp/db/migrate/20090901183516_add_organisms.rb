f3db5405e408b4707484aecce80a9a377b42b6d0
Ryan L. Bell <ryan.l.bell@gmail.com>
2009-09-28 12:19:26 -0400

    Organism name needed to be wider

--- webapp/db/migrate/20090901183516_add_organisms.rb
+++ webapp/db/migrate/20090901183516_add_organisms.rb
@@ -1,7 +1,7 @@
 class AddOrganisms < ActiveRecord::Migration
   def self.up
     create_table :organisms do |t|
-      t.column :organism_name, :string
+      t.column :organism_name, :string, :limit => 255
       t.column :snomed_name,   :string
       t.column :snomed_code,   :string, :limit => 50
       t.column :snomed_id,     :string, :limit => 50
