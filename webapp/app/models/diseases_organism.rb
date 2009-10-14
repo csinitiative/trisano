214a5feee04b1d977de94ae71b0f737c47733759
Ryan L. Bell <ryan.l.bell@gmail.com>
2009-10-14 15:37:12 -0400

    Improved the safety of this migration script.

--- webapp/app/models/diseases_organism.rb
+++ webapp/app/models/diseases_organism.rb
@@ -9,10 +9,12 @@ class DiseasesOrganism < ActiveRecord::Base
     def load_from_yaml(str_yaml)
       assoc = YAML.load str_yaml
       assoc.each do |key, value|
+        logger.info "Processing #{key}"
+        next if key == 'Unclassified'
         value[:organisms].each do |organism_attr|
           organism = Organism.find(:first, :conditions => ['lower(organism_name) = ?', organism_attr[:organism_name].downcase]) || Organism.create(organism_attr)
           value[:diseases].each do |disease_attr|
-            disease = Disease.first(:conditions => disease_attr)
+            disease = Disease.find_by_disease_name(disease_attr[:disease_name])
             logger.debug " #{disease_attr.inspect}"
             organism.diseases << disease unless organism.diseases.include?(disease)
           end
