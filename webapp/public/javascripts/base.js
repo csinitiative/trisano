3bad1a4a44d03c3a7331b07c766393c6c1d1208f
David Christiansen <david@bigubu.(none)>
2010-04-06 09:02:02 -0400

    Supported browsers warning is out of date

--- webapp/public/javascripts/base.js
+++ webapp/public/javascripts/base.js
@@ -7969,7 +7969,7 @@ function detect() {
  >= 2)) || ((BrowserDetect.browser == 'Explorer') &&
 (BrowserDetect.version >= 7))) not_supported = false
    if (not_supported) document.write('You are using an unsupported browser: ' +
-BrowserDetect.browser + ' ' + BrowserDetect.version + '<br>Please switch to <a href="http://getfirefox.com">FireFox 2</a> or <a href="http://www.microsoft.com/windows/downloads/ie/getitnow.mspx"s>Internet Explorer 7</a>')
+BrowserDetect.browser + ' ' + BrowserDetect.version + '<br>Please switch to <a href="http://getfirefox.com">FireFox 2 or higher</a> or <a href="http://www.microsoft.com/windows/downloads/ie/getitnow.mspx"s>Internet Explorer 7 or higher</a>')
 }
 
 /*
