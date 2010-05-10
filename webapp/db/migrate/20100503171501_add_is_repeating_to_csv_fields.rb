8489953b1f08823d957faa2b987a6e9439770c0a
Ryan L. Bell <ryan.l.bell@gmail.com>
2010-05-10 10:43:17 -0400

    using AR to update was causing errors when trying to update producuction data. changed to SQL updates instead.

--- webapp/db/migrate/20100503171501_add_is_repeating_to_csv_fields.rb
+++ webapp/db/migrate/20100503171501_add_is_repeating_to_csv_fields.rb
@@ -1,32 +1,37 @@
 class AddIsRepeatingToCsvFields < ActiveRecord::Migration
   def self.up
-    add_column :csv_fields, :collection, :string
+    ActiveRecord::Base.transaction do
+      add_column :csv_fields, :collection, :string
 
-    [["contact_event", "contact_hospital_admission_date"],
-      ["contact_event", "contact_hospital_discharge_date"],
-      ["contact_event", "contact_hospitalization_facility"],
-      ["contact_event", "contact_hospital_medical_record_no"],
-      ["morbidity_event", "patient_hospital_admission_date"],
-      ["morbidity_event", "patient_hospital_discharge_date"],
-      ["morbidity_event", "patient_hospital_medical_record_no"]].each do |csv_field|
+      [["contact_event", "contact_hospital_admission_date"],
+       ["contact_event", "contact_hospital_discharge_date"],
+       ["contact_event", "contact_hospitalization_facility"],
+       ["contact_event", "contact_hospital_medical_record_no"],
+       ["morbidity_event", "patient_hospital_admission_date"],
+       ["morbidity_event", "patient_hospital_discharge_date"],
+       ["morbidity_event", "patient_hospital_medical_record_no"]].each do |csv_field|
 
-      field = CsvField.find_by_event_type_and_long_name(csv_field[0], csv_field[1])
+        field = CsvField.find_by_event_type_and_long_name(csv_field[0], csv_field[1])
 
-      unless field.nil?
-        first_position = field.use_description.index(".first")
-        try_position = field.use_description.index(".try")
+        unless field.nil?
+          first_position = field.use_description.index(".first")
+          try_position = field.use_description.index(".try")
 
-        collection_name = field.use_description[0..first_position-1]
-        new_use_description = field.use_description[try_position+1...field.use_description.size]
+          values = {}
+          values['collection'] = field.use_description[0..first_position-1]
+          values['use_description'] = field.use_description[try_position+1...field.use_description.size]
 
-        unless field.use_code.blank?
-          new_use_code = field.use_code[try_position+1...field.use_code.size]
-          field.use_code = new_use_code
+          unless field.use_code.blank?
+            new_use_code = field.use_code[try_position+1...field.use_code.size]
+            values['use_code'] = new_use_code
+          end
+
+          execute(<<-SQL)
+            UPDATE csv_fields
+               SET #{values.map{|k,v| "#{k} = '#{v}'"}.join(", ")}
+             WHERE id = #{field.id}
+          SQL
         end
-
-        field.use_description = new_use_description
-        field.collection = collection_name
-        field.save!
       end
     end
   end
