747e7ad3981594a0ed3d6da7aff0390a8fb4f7a0
Ryan L. Bell <ryan.l.bell@gmail.com>
2010-05-26 09:48:04 -0400

    form views are all exercised to ensure that there aren't any missing translations

--- webapp/spec/views/forms/_form.html.haml_spec.rb
+++ webapp/spec/views/forms/_form.html.haml_spec.rb
@@ -15,6 +15,31 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with TriSano. If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
 
-replace_element, replace_partial = replacement_elements(@section)
-page.replace_html replace_element, :partial => replace_partial
-flash[:notice] = ""
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "/forms/_form.html.haml" do
+
+  before do
+    @form = Factory.build(:form)
+    @form.save_and_initialize_form_elements
+    assigns[:form] = @form
+    @f = mock
+    @f.stubs(:label)
+    @f.stubs(:text_field)
+    @f.stubs(:select)
+    @f.stubs(:collection_select)
+    @f.stubs(:object).returns(@form)
+  end
+
+  it "renders with short name editable" do
+    @f.expects(:text_field).with(:short_name)
+    render "forms/_form.html.haml", :locals => {:f => @f}
+  end
+
+  it "renders with short name *not* editable" do
+    @form.stubs(:short_name_editable?).returns(false)
+    render "forms/_form.html.haml", :locals => {:f => @f}
+    response.should have_tag('td', @form.short_name)
+  end
+
+end
