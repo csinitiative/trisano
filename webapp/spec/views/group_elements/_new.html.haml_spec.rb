6133b8f7474cc13b570a77e51d3cd1ae8f8b59e6
Ryan L. Bell <ryan.l.bell@gmail.com>
2010-05-27 11:42:07 -0400

    group element view specs

---
+++ webapp/spec/views/group_elements/_new.html.haml_spec.rb
@@ -0,0 +1,32 @@
+# Copyright (C) 2007, 2008, 2009, 2010 The Collaborative Software Foundation
+#
+# This file is part of TriSano.
+#
+# TriSano is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# TriSano is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with TriSano. If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
+
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "/group_elements/_new.html.haml" do
+
+  before do
+    assigns[:group_element] = Factory.create(:group_element)
+    assigns[:reference_element] = Factory.create(:view_element)
+    assigns[:library_elements] = []
+  end
+
+  it "renders" do
+    render "group_elements/_new.html.haml"
+  end
+
+end
