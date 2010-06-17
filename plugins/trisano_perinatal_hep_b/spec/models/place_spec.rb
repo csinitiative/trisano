2aca84dced8e56882c863652e53f722e29744934
ecopony <ecopony@yahoo.com>
2010-06-17 09:41:53 -0700

    Adding the new place type code method for expected delivery places.

---
+++ plugins/trisano_perinatal_hep_b/spec/models/place_spec.rb
@@ -0,0 +1,29 @@
+# Copyright (C) 2007, 2008, 2009, 2010 The Collaborative Software Foundation
+#
+# This file is part of TriSano.
+#
+# TriSano is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# TriSano is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with TriSano. If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
+
+require File.expand_path(File.dirname(__FILE__) +  '/../../../../../spec/spec_helper')
+
+describe Place, "in the Perinatal Hep B plugin" do
+  
+  it "should return expected delivery facility place types" do
+    Place.expected_delivery_type_codes.size.should == 3
+    ["H", "C", "O"].each do |place_type_code|
+      Place.expected_delivery_type_codes.include?(place_type_code).should be_true
+    end
+  end
+
+end
