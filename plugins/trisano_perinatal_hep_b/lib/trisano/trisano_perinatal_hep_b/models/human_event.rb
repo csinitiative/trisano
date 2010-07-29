10b79631988a3f2e3c0a2969c67d1c82e3109c51
ecopony <ecopony@yahoo.com>
2010-07-29 11:32:50 -0700

    Resolving an issue with validations when a disease is changed on a contact event with existing treatments.

--- plugins/trisano_perinatal_hep_b/lib/trisano/trisano_perinatal_hep_b/models/human_event.rb
+++ plugins/trisano_perinatal_hep_b/lib/trisano/trisano_perinatal_hep_b/models/human_event.rb
@@ -65,7 +65,7 @@ module Trisano
                     ::DiseaseSpecificValidation.diseases_ids_for_key(:treatment_date_required).include?(disease.id)
 
                   self.interested_party.treatments.each do |pt|
-                    unless pt.treatment.id.blank?
+                    unless pt.treatment.try(:id).blank?
                       pt.errors.add_on_blank(:treatment_date)
                       self.errors.add_to_base("#{I18n.t(:treatment_date)} #{I18n.t(:blank, :scope => 'activerecord.errors.messages')}") if pt.treatment_date.blank?
                     end
