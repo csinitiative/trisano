4c8de5ded1515657381a94b3672b71c8ebae5672
ecopony <ecopony@yahoo.com>
2010-08-16 11:14:45 -0700

    Enhanced cukes for adding and removing treatments with the p-hep-b plugin installed.

---
+++ plugins/trisano_perinatal_hep_b/features/enhanced/hep_b_contact_treatment_fields.feature
@@ -0,0 +1,24 @@
+Feature: Hep B specific contact treatment fields
+
+  Scenario: Adding multiple contact treatments at one time
+    Given I am logged in as a super user
+      And disease "Hepatitis B Pregnancy Event" exists
+      And "Hepatitis B Pregnancy Event" has disease specific core fields
+      And a morbidity event exists with the disease Hepatitis B Pregnancy Event
+      And there is a contact on the event named Davis
+    When I am on the contact event edit page
+      And I add a p-hep-b treatment "HBIG" on with a date 5 days ago
+      And I add a 2nd p-hep-b treatment "Hepatitis B Dose 1" on with a date 4 days ago
+      And I save the event
+    Then I should see the treatment "HBIG" on with a date 5 days ago
+      And I should see the treatment "Hepatitis B Dose 1" on with a date 4 days ago
+    When I am on the contact event edit page
+      And I remove the 1st treatment
+      And I save the event
+    Then I should not see the treatment "HBIG" on with a date 5 days ago
+     
+    
+
+
+
+
---
+++ plugins/trisano_perinatal_hep_b/features/enhanced_steps/treatment_steps.rb
@@ -0,0 +1,34 @@
+def add_p_hep_b_treatment(treatment, days_ago, index_string="1st")
+  index = index_string[0..1].to_i
+  @browser.click("link=Add a Treatment") unless index == 1
+  sleep(1)
+  click_core_tab(@browser, CLINICAL)
+  @browser.select("xpath=//div[@id='treatments']//div[@class='treatment'][#{index}]//select[contains(@name, 'yn')]", "Yes")
+  @browser.select("xpath=//div[@id='treatments']//div[@class='treatment'][#{index}]//select[contains(@name, 'treatment_id')]", treatment)
+  @browser.type("xpath=//div[@id='treatments']//div[@class='treatment'][#{index}]//input[contains(@name, 'treatment_date')]", (Date.today - days_ago.to_i.days).to_s)
+
+end
+
+When /^I add a p\-hep\-b treatment "([^\"]*)" on with a date (.+) days ago$/ do |treatment, days_ago|
+  add_p_hep_b_treatment(treatment, days_ago)
+end
+
+When /^I add a (.+) p\-hep\-b treatment "([^\"]*)" on with a date (.+) days ago$/ do |index_string, treatment, days_ago|
+  add_p_hep_b_treatment(treatment, days_ago, index_string)
+end
+
+Then /^I should see the treatment "([^\"]*)" on with a date (.+) days ago$/ do |treatment, days_ago|
+  assert(@browser.get_html_source.include?(treatment))
+  assert(@browser.get_html_source.include?((Date.today - days_ago.to_i.days).to_s))
+end
+
+When /^I remove the (.+) treatment$/ do |index_string|
+  click_core_tab(@browser, CLINICAL)
+  index = index_string[0..1].to_i
+  @browser.check("xpath=//div[@id='treatments']//div[@class='treatment'][#{index}]//input[contains(@name, 'delete')][2]")
+end
+
+Then /^I should not see the treatment "([^\"]*)" on with a date (.+) days ago$/ do |treatment, days_ago|
+  assert(!@browser.get_html_source.include?(treatment))
+  assert(!@browser.get_html_source.include?((Date.today - days_ago.to_i.days).to_s))
+end
