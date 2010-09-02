b18c16c2d8027165dd60d679db12ffa055cdd47c
Ryan L. Bell <ryan.l.bell@gmail.com>
2010-09-02 11:18:26 -0400

    fixing an index name

--- webapp/db/migrate/20091016151621_common_test_types_diseases.rb
+++ webapp/db/migrate/20091016151621_common_test_types_diseases.rb
@@ -22,7 +22,7 @@ class CommonTestTypesDiseases < ActiveRecord::Migration
       t.integer :common_test_type_id, :null => false
       t.timestamps
     end
-    add_index :common_test_types_diseases, [:disease_id, :common_test_type_id], :unique => true, :name => :by_common_test_type_and_disease
+    add_index :common_test_types_diseases, [:disease_id, :common_test_type_id], :unique => true, :name => 'by_common_test_type_and_disease'
   end
 
   def self.down
