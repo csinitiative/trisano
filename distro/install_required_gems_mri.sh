9500ecaf20f8979a94dcc6ceb86327fa11e1ff6e
Ross Cooperman <cooperman@gmail.com>
2010-09-09 12:05:05 -0400

    fixed install_required_gems_mri.sh script to work with a 0 argument rather than having to create an empty file named 0 in the distro/ directory.

--- distro/install_required_gems_mri.sh
+++ distro/install_required_gems_mri.sh
@@ -4,19 +4,28 @@
 #
 # This file is part of TriSano.
 #
-# TriSano is free software: you can redistribute it and/or modify it under the 
-# terms of the GNU Affero General Public License as published by the 
-# Free Software Foundation, either version 3 of the License, 
+# TriSano is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, either version 3 of the License,
 # or (at your option) any later version.
 #
-# TriSano is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# TriSano is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License 
+# You should have received a copy of the GNU Affero General Public License
 # along with TriSano. If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
 
+# install the .gem files that are required to run the system
+# Pass in 0 if you don't want sudo to be used
+
+if [ "$1" = "0" ]
+then
+  command=""
+else
+  command="sudo"
+fi
 
 . setenv.sh
 
