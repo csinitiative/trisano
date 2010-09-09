2b9f8543f25737344ca3e09b23d2acb9ad2166b7
Ryan L. Bell <ryan.l.bell@gmail.com>
2010-09-09 14:55:55 -0400

    fixing short name collision checking to only use the nested set of children when comparing to a form.

--- webapp/lib/trisano/form_element/short_name.rb
+++ webapp/lib/trisano/form_element/short_name.rb
@@ -10,16 +10,24 @@ module Trisano
           unless replacements.empty?
             Question.update(replacements.keys, replacements.values)
           end
-          results << Question.find_by_sql([<<-SQL, other_tree.tree_id, tree_id, tree_id, tree_id])
+          sql_options = {
+            :other_tree_id => other_tree.tree_id,
+            :this_tree_id => tree_id,
+            :this_id => self.id,
+            :this_lft => self.lft,
+            :this_rgt => self.rgt
+          }
+          results << Question.find_by_sql([<<-SQL, sql_options])
           SELECT a.id, a.short_name, a.question_text, fi.collision, b.lft
             FROM questions a
             JOIN form_elements b ON a.form_element_id = b.id
             LEFT JOIN (SELECT true as collision, i.short_name, i.id
                          FROM questions i
                          JOIN form_elements f ON i.form_element_id = f.id
-                        WHERE f.tree_id = ?) as fi
+                        WHERE f.tree_id = :other_tree_id) as fi
                    ON fi.short_name = a.short_name
-           WHERE b.tree_id = ?
+           WHERE b.tree_id = :this_tree_id
+             AND (b.id = :this_id OR b.lft BETWEEN :this_lft AND :this_rgt)
           UNION
           SELECT c.id, c.short_name, c.question_text, jg.collision, d.lft
             FROM questions c
@@ -27,9 +35,11 @@ module Trisano
             LEFT JOIN (SELECT true as collision, j.short_name, j.id
                          FROM questions j
                          JOIN form_elements g ON j.form_element_id = g.id
-                        WHERE g.tree_id = ?) as jg
+                        WHERE g.tree_id = :this_tree_id
+                          AND (g.id = :this_id OR g.lft BETWEEN :this_lft AND :this_rgt)) as jg
                    ON c.short_name = jg.short_name AND c.id > jg.id
-           WHERE d.tree_id = ?
+           WHERE d.tree_id = :this_tree_id
+             AND (d.id = :this_id OR d.lft BETWEEN :this_lft AND :this_rgt)
           ORDER BY lft, collision
         SQL
         end
--- webapp/spec/support/spec_helpers/library_spec_helper.rb
+++ webapp/spec/support/spec_helpers/library_spec_helper.rb
@@ -1,77 +1,12 @@
 module LibrarySpecHelper
 
-  class LibraryTree
-    class << self
-      def create_question(options={}, &block)
-        lib_tree = new
-        lib_tree.library_question(options, &block)
-        lib_tree
-      end
-
-      def create_group(options={}, &block)
-        lib_tree = new
-        lib_tree.library_group(options, &block)
-        lib_tree
-      end
-    end
-
-    def initialize
-      @tree_id = FormElement.next_tree_id
-      @nodes = []
-    end
-
-    def tree_id
-      @tree_id
-    end
-
-    def last
-      @nodes.last
-    end
-
-    def first
-      @nodes.first
-    end
-
-    def library_question(options = {}, &block)
-      options.symbolize_keys!
-      options[:tree_id] ||= @tree_id
-      options[:is_template] ||= true
-      question = Factory.build(:question_element, options)
-      question.save!
-      last.add_child(question) if last
-      @nodes.push(question)
-      instance_eval(&block) if block_given?
-    end
-
-    def library_follow_up(options={}, &block)
-      options.symbolize_keys!
-      options[:tree_id] ||= @tree_id
-      options[:is_template] ||= true
-      follow_up = Factory.build(:follow_up_element, options)
-      follow_up.save!
-      last.add_child(follow_up)
-      @nodes.push(follow_up)
-      instance_eval(&block) if block_given?
-    end
-
-    def library_group(options={}, &block)
-      options.symbolize_keys!
-      options[:tree_id] ||= @tree_id
-      options[:is_template] ||= true
-      group = Factory.build(:group_element, options)
-      group.save!
-      @nodes.push(group)
-      instance_eval(&block) if block_given?
-    end
-
-  end
-
-  def library_question(options = {}, &block)
-    LibraryTree.create_question options, &block
-  end
-
   def library_group(options = {}, &block)
-    LibraryTree.create_group options, &block
+    options.symbolize_keys!
+    options[:is_template] ||= true
+    group_element = Factory.build(:group_element, options)
+    group_element.save_and_add_to_form
+    block[group_element] if block_given?
+    group_element
   end
 
 end
