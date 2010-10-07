b0b8c9fe77562028f9315de975d491fbf8ffbea3
Jimmy Dee <jdee@csinitiative.com>
2010-10-07 10:16:19 -0500

    - small refactoring of staged_message JS - disable dangerous links when deleting a staged message

---
+++ webapp/public/javascripts/staged_messages/_staged_message.js
@@ -0,0 +1,6 @@
+$j(function(){
+  $j('.staged-message-discard-link').click(function(){
+    var staged_message = $j(this).closest('.staged-message').fadeTo('fast', 0.4)
+    $j('.staged-message-link', staged_message).removeAttr('href');
+  });
+});
