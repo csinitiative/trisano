369fddbb5ccaa87c45c9dc1f366898be12459f9e
Ed Copony <ecopony@yahoo.com>
2010-11-03 14:01:44 -0700

    Fixing up failing features after treatment dropdown changes.

--- plugins/trisano_perinatal_hep_b/features/standard_steps/hep_b_contact_treatment_fields_steps.rb
+++ plugins/trisano_perinatal_hep_b/features/standard_steps/hep_b_contact_treatment_fields_steps.rb
@@ -2,16 +2,16 @@
 Then /^I should not see the p\-hep\-b treatment fields$/ do
   response.should have_tag('#treatments') do
     with_tag('select[name=?]', 'contact_event[interested_party_attributes][treatments_attributes][0][treatment_given_yn_id]')
-    with_tag('input[name=?]', 'contact_event[interested_party_attributes][treatments_attributes][0][treatment_name]')
-    without_tag('select[name=?]', 'contact_event[interested_party_attributes][treatments_attributes][0][treatment_id]')
+    with_tag('select[name=?]', 'contact_event[interested_party_attributes][treatments_attributes][0][treatment_id]')
+    with_tag('input[name=?]', 'contact_event[interested_party_attributes][treatments_attributes][0][stop_treatment_date]')
   end
 end
 
 Then /^I should see the p\-hep\-b treatment fields$/ do
   response.should have_tag('#treatments') do
     with_tag('select[name=?]', 'contact_event[interested_party_attributes][treatments_attributes][0][treatment_given_yn_id]')
-    without_tag('input[name=?]', 'contact_event[interested_party_attributes][treatments_attributes][0][treatment_name]')
     with_tag('select[name=?]', 'contact_event[interested_party_attributes][treatments_attributes][0][treatment_id]')
+    without_tag('input[name=?]', 'contact_event[interested_party_attributes][treatments_attributes][0][stop_treatment_date]')
   end
 end
 
