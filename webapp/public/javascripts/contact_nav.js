14bc89270a6a6d2c0f204fd33ba26bc014c5ca04
Ryan L. Bell <ryan.l.bell@gmail.com>
2010-11-11 16:24:09 -0500

    [#5668549] Resets selector to a null option if nav dialog is cancelled.

--- webapp/public/javascripts/contact_nav.js
+++ webapp/public/javascripts/contact_nav.js
@@ -2,11 +2,12 @@ $j(function() {
 
   $j('.contacts_nav').change(function(event) {
     var selector = $j(this);
+    var cancelled = true;
 
     var dialogButtons = function() {
       var buttons = {};
-
       buttons[i18n.t('save_and_exit')] = function() {
+        cancelled = false;
         $j('#contacts_nav_spinner').show();
         $j(formWatcher.form).append(redirectTo(
                                       build_url_with_tab_index(
@@ -16,6 +17,7 @@ $j(function() {
       };
 
       buttons[i18n.t('leave_without_saving')] = function() {
+        cancelled = false;
         $j('#contacts_nav_spinner').show();
         formWatcher.submitted = true;
         send_url_with_tab_index(selector.val());
@@ -43,7 +45,12 @@ $j(function() {
           title: i18n.t('unsaved_changes'),
           height: 300,
           width: 600,
-          buttons: dialogButtons()
+          buttons: dialogButtons(),
+          beforeClose: function(event, ui) {
+            if (cancelled) {
+              selector.val('');
+            }
+          }
         });
       } else {
         $j('#contacts_nav_spinner').show();
