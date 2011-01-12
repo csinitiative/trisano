43481ff644525dd273a32c25cce230c1c3463a74
Ryan L. Bell <ryan.l.bell@gmail.com>
2011-01-12 16:53:00 -0500

    Unpending or removing pended specs

--- webapp/spec/controllers/event_queues_controller_spec.rb
+++ webapp/spec/controllers/event_queues_controller_spec.rb
@@ -316,7 +316,6 @@ describe EventQueuesController do
     end
 
     it "should find the event_queue requested" do
-      pending "Not  sure why this doesn't work"
       EventQueue.expects(:find).with("1").returns(@event_queue)
       do_delete
     end
