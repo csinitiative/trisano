b5774fe5c73eb1ec3f9a2f7575845c44503c33d2
Patrick Logan <patrick@patrick-HP-G71-Notebook-PC.(none)>
2011-01-12 13:38:35 -0800

    Set to 3.0 RC2

--- webapp/app/helpers/layout_helper.rb
+++ webapp/app/helpers/layout_helper.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-# Copyright (C) 2007, 2008, 2009, 2010 The Collaborative Software Foundation
+# Copyright (C) 2007, 2008, 2009, 2010, 2011 The Collaborative Software Foundation
 #
 # This file is part of TriSano.
 #
@@ -31,7 +31,7 @@ module LayoutHelper
     result << "</div>"
     result << "<div class='foottext'>"
     result << "<div class='top'>"
-    result << "<a href='https://wiki.csinitiative.com/display/tri/TriSano+-+3.0+RC1+Release+Notes'>#{t('trisano_ce')}</a>"
+    result << "<a href='https://wiki.csinitiative.com/display/tri/TriSano+-+3.0+RC2+Release+Notes'>#{t('trisano_ce')}</a>"
     result << "</div>"
     result << "<div class='bottom'>"
     result << "<a href='http://www.trisano.org/collaborate/'>#{t('collaborate')}</a>"
