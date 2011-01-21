8d25471082c02d73252a8863601fa8483deae388
Ed Copony <ecopony@yahoo.com>
2011-01-20 16:11:02 -0800

    Overriding deploy:cold for fresh TriSano installs.

--- webapp/config/deploy.rb
+++ webapp/config/deploy.rb
@@ -19,4 +19,19 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+
+  desc <<-DESC
+    Deploys a new, fresh install of the application. Assumes a running \
+    application server and database server, but no previously deployed \
+    application code or trisano application database. Deploys the \
+    application code, builds an application database, and restarts the \
+    application server.
+  DESC
+  task :cold do
+    rails_env = fetch :rails_env, 'production'
+    update
+    run "cd #{latest_release} && rake db:setup RAILS_ENV=#{rails_env}"
+    restart
+  end
+
 end
