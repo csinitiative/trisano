e3ea343dd3df797c8cd2952ba3c5e7ed5f2ac078
Ryan L. Bell <ryan.l.bell@gmail.com>
2011-03-18 11:49:13 -0400

    [#11079477] configure email for smtp, sendmail or test. Also Cap support

--- webapp/config/initializers/mailer.rb
+++ webapp/config/initializers/mailer.rb
@@ -1,1 +1,14 @@
 ActionMailer::Base.default_url_options[:host] ||= config_options[:host]
+ActionMailer::Base.default_content_type = "text/html"
+
+if ActionMailer::Base.delivery_method != :test
+  mailer_options = config_options['mailer'] || {}
+  if mailer_options == 'test'
+    ActionMailer::Base.delivery_method = mailer.to_sym
+  else
+    mailer_options.each do |k, v|
+      ActionMailer::Base.delivery_method = k
+      ActionMailer::Base.send("#{k}_settings=", v.symbolize_keys)
+    end
+  end
+end
