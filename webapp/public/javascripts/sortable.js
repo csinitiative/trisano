e0c576c984c7b69ae92c81210d12b049f519d589
Ross Cooperman <cooperman@gmail.com>
2011-04-13 12:33:14 -0400

    Added support for tables that are sorted by refreshing the table via ajax

--- webapp/public/javascripts/sortable.js
+++ webapp/public/javascripts/sortable.js
@@ -1,3 +1,21 @@
+// Handle tables that are sortable with javascript alone
 $j('table.sortable').livequery(function() {
   $j(this).tablesorter({ textExtraction: 'complex' });
+});
+
+// Handle tables that require an ajax request to fetch the newly sorted table data
+$j('.sort_link').livequery(function() {
+  $j(this).click(function(evt) {
+    // don't want the default click action to fire
+    evt.preventDefault();
+
+    // locate the target element (where the newly loaded data goes)
+    var target = $j(evt.target).parents('#' + $j(evt.target).attr('data-replace'));
+    target.html('').siblings('#loader').show();
+
+    // load the new data and insert it in the target
+    $j.get($j(evt.target).attr('href'), null, function(data, status, xhr) {
+      target.html(data).siblings('#loader').hide();
+    });
+  });
 });
