2d9f04ba1d8704c60ea8feeb95de810ee4a28475
Jimmy Dee <jimmy@dee.name>
2011-04-28 12:01:38 -0500

    [Delivers #12658565] test for blank rows in pre-CMR search

--- webapp/spec/lib/fulltext_search_sensitive_disease_spec.rb
+++ webapp/spec/lib/fulltext_search_sensitive_disease_spec.rb
@@ -50,18 +50,33 @@ describe "searching with sensitive diseases" do
     end
 
     context "for the not sensitive disease user" do
-      it "should not include any of the sensitive disease events" do
+      before :each do
         User.current_user = @not_sensitive_disease_user
+      end
+
+      it "should not include any of the sensitive disease events" do
         events = HumanEvent.find_by_name_and_bdate(:last_name => 'James')
 
         # The events that should be returned
         events.detect { |event| event.id == @not_sensitive_event.id }.should_not be_nil
         events.detect { |event| event.id == @event_without_a_disease.id }.should_not be_nil
-        
+
         # The events that shouldn't be returned
         events.detect { |event| event.id == @sensitive_event.id }.should be_nil
         events.detect { |event| event.id == @sensitive_event_secondary.id }.should be_nil
         events.detect { |event| event.id == @sensitive_event_out_of_jurisdiction.id }.should be_nil
+      end
+
+      it "shows blank rows when a patient has only sensitive-disease events" do
+        create_morbidity_event(
+          :patient => 'Simon',
+          :disease => @sensitive_disease,
+          :jurisdiction => @bear_cub_river
+        )
+
+        events = HumanEvent.find_by_name_and_bdate(:last_name => 'Simon')
+        events.count.should == 1
+        events.first['event_id'].should == nil
       end
     end
   end
