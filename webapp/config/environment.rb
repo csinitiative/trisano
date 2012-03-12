f6dc9a34b37d89ad7e009cec0a9d9d5bcdfd8797
Brian Buchalter <brianb@endpoint.com>
2012-03-12 09:46:50 -0400

    Update Rails enviornment requirements to support rubygems > 1.6.0

--- webapp/config/environment.rb
+++ webapp/config/environment.rb
@@ -8,6 +8,7 @@
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
+require 'thread'
 require File.join(File.dirname(__FILE__), 'boot')
 
 # REMOVE WHEN UPGRADING PAST RAILS 2.3.4
