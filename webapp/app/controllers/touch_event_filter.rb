adb7f8b96ed421488f067dcdf042645d4ce41b6d
Brian Buchalter <brianb@endpoint.com>
2012-03-14 14:45:17 -0400

    Fix TouchEventFilter to actually update the updated_at timestamp on the event record

--- webapp/app/controllers/touch_event_filter.rb
+++ webapp/app/controllers/touch_event_filter.rb
@@ -1,5 +1,18 @@
 class TouchEventFilter
   def self.filter(controller)
-    controller.instance_variable_get("@event").save
+    touch controller.instance_variable_get("@event")
+  end
+
+  def self.touch(object, attribute = nil)
+    current_time = Time.current
+
+    if attribute
+      object.write_attribute(attribute, current_time)
+    else
+      object.write_attribute('updated_at', current_time) if object.respond_to?(:updated_at)
+      object.write_attribute('updated_on', current_time) if object.respond_to?(:updated_on)
+    end
+  
+    object.save!
   end
 end
