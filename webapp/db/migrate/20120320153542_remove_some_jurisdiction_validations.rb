352b443432a88d64715504b87f82b802d8236ad6
Joshua Tolley <josh@endpoint.com>
2012-03-20 10:22:34 -0600

    Fix and/or remove some jurisdiction validations

---
+++ webapp/db/migrate/20120320153542_remove_some_jurisdiction_validations.rb
@@ -0,0 +1,28 @@
+class RemoveSomeJurisdictionValidations < ActiveRecord::Migration
+  def self.up
+    execute <<-SQL
+DROP TRIGGER IF EXISTS validate_jurisdiction ON event_queues;
+DROP TRIGGER IF EXISTS validate_jurisdiction ON external_codes;
+CREATE OR REPLACE FUNCTION validate_jurisdiction() RETURNS TRIGGER AS $validate_jurisdiction$
+DECLARE
+    msg TEXT;
+    i INTEGER;
+BEGIN
+    IF NEW.jurisdiction_id IS NOT NULL AND NOT EXISTS (
+        SELECT 1 FROM places p
+            JOIN places_types pt ON (pt.place_id = p.id)
+            JOIN codes c ON (c.the_code = 'J' AND c.id = pt.type_id)
+        WHERE p.entity_id = NEW.jurisdiction_id
+    ) THEN
+        RAISE EXCEPTION 'Error. Place with entity id % is not a jurisdiction.', NEW.jurisdiction_id;
+        RETURN NULL;
+    END IF;
+    RETURN NEW;
+END;
+$validate_jurisdiction$ LANGUAGE plpgsql;
+    SQL
+  end
+
+  def self.down
+  end
+end
