3c2abb2aafd626939fd5e93d6db5916a4edfde6d
Brian Buchalter <brianb@endpoint.com>
2012-05-09 14:19:20 -0400

    Add check to assessment event core fields migration to prevent migration when assessment core fields are already defined.

--- webapp/db/migrate/20120508172146_populate_assessment_event_core_fields.rb
+++ webapp/db/migrate/20120508172146_populate_assessment_event_core_fields.rb
@@ -2,20 +2,29 @@ class PopulateAssessmentEventCoreFields < ActiveRecord::Migration
   # We must collect the existing core fields for morbidyt events and then duplicate them for 
   # assessment events
   def self.up
-    morbidity_core_fields = CoreField.find(:all, :conditions => ["event_type = ?", "morbidity_event"])
+    assessment_core_fields = CoreField.find(:all, :conditions => ["event_type = ?", "assessment_event"])
+    
+    # We only want to load assessment core fields from morbidity core fields
+    # unless they've been defined.
+    if assessment_core_fields.empty?
 
-    morbidity_core_fields.each do |field|
-      new_field = core_field_to_hash(field)
-      new_field['event_type'].gsub!("morbidity", "assessment")
-      new_field['key'].gsub!("morbidity", "assessment")
-      new_field['parent_key'].gsub!("morbidity", "assessment") unless new_field['parent_key'].nil?
+      morbidity_core_fields = CoreField.find(:all, :conditions => ["event_type = ?", "morbidity_event"])
 
-      new_field_formatted = new_field.to_yaml
-      new_field_formatted.gsub!("--- \n", "- ")
-      new_field_formatted.gsub!("\n", "\n  ")
+      morbidity_core_fields.each do |field|
+        new_field = core_field_to_hash(field)
+        new_field['event_type'].gsub!("morbidity", "assessment")
+        new_field['key'].gsub!("morbidity", "assessment")
+        new_field['parent_key'].gsub!("morbidity", "assessment") unless new_field['parent_key'].nil?
 
-      CoreField.load!([new_field])
+        new_field_formatted = new_field.to_yaml
+        new_field_formatted.gsub!("--- \n", "- ")
+        new_field_formatted.gsub!("\n", "\n  ")
 
+        CoreField.load!([new_field])
+
+      end
+    else
+      say "Assessment core fields already defined.  No action taken."
     end
   end
 
