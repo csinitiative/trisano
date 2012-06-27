e011cc4e48a218e4857351d13a6ad46b046fa43f
Brian Buchalter <brianb@endpoint.com>
2012-06-27 16:11:48 -0400

    Update deploy

--- webapp/config/deploy.rb
+++ webapp/config/deploy.rb
@@ -9,8 +9,6 @@ extend Capistrano::Helpers::SiteConfig
 default_run_options[:pty] = true
 set :application, "TriSano"
 set :deploy_to, "/opt/csi/#{application}"
-set :stages, %w(vagrant tomcat)
-set :default_stage, "vagrant"
 
 set :repository, "."
 set :scm, :none
