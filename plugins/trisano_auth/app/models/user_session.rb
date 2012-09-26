56fd2f658a80d0d24d604f1cecbcaf52c48a806f
Marina Lohova <marina@endpoint.com>
2012-09-25 17:06:11 -0700

    10877 When using the TriSano authorization plugin, Username accounts will be suspended/locked after three unsuccessful logon attempts

--- plugins/trisano_auth/app/models/user_session.rb
+++ plugins/trisano_auth/app/models/user_session.rb
@@ -5,6 +5,8 @@
 class UserSession < Authlogic::Session::Base
   reloadable!
   logout_on_timeout true
+  consecutive_failed_logins_limit 3
+  failed_login_ban_for 2.hours
   single_access_allowed_request_types :all
   params_key "api_key"
 end
