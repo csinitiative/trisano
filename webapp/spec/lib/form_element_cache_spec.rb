csinitiative/trisano

commit e21edc062b308e5912414adcdc874964ddddb152

Refactor dyamic_question

webapp/lib/form_element_cache.rb
@@ -31,6 +31,15 @@ class FormElementCache
     @children = {}
     @full_set = load_full_set
   end
+
+  def has_children_for?(element)
+    children(element).empty?
+  end
+
+  def has_value_set_for?(element)
+    value_set = children_by_type("ValueSetElement", element).first
+    !value_set.nil? || !form_elements_cache.children(value_set).empty?
+  end
   
   private
   

webapp/app/helpers/dynamic_question_builder.rb
@@ -1,0 +1,21 @@
+class DynamicQuestionBuilder
+
+  def initialize(options={})
+    @question_element = options.fetch(:question_element)
+    @form_elements_cache = options.fetch(:form_elements_cache)
+  end
+
+  def question_is_multi_valued_and_has_value_set?
+    if question.is_multi_valued?
+      @form_elements_cache.has_children_for?(@question_element) &&
+      @form_elements_cache.has_value_set_for?(@question_element)
+    end    
+  end   
+
+
+  private
+
+  def question
+    @question_element.question
+  end
+end

webapp/app/helpers/extended_form_builder.rb
@@ -58,20 +58,12 @@ class ExtendedFormBuilder < ActionView::Helpers::FormBuilder
 
   # TODO: refactor me!
   def dynamic_question(form_elements_cache, question_element, event, index, html_options = {})
+    builder = DynamicQuestionBuilder.new :question_element => question_element,
+                                         :form_elements_cache => form_elements_cache
+    return "" unless builder.question_is_multi_valued_and_has_value_set?
     id = html_options[:id]
     result = ""
     question = question_element.question
-
-    if question.is_multi_valued?
-      if form_elements_cache.children(question_element).empty?
-        return ""
-      else
-        value_set = form_elements_cache.children_by_type("ValueSetElement", question_element).first
-        if (value_set.nil? || form_elements_cache.children(value_set).empty?)
-          return ""
-        end
-      end
-    end
 
     index = @object.id.nil? ? index : @object.id
     html_options[:index] = index

webapp/app/helpers/form_builder_dsl_helper.rb
@@ -336,16 +336,14 @@ module FormBuilderDslHelper
     if (f.nil?)
       fields_for(@event) do |f|
         f.fields_for(:new_answers, @answer_object, :builder => ExtendedFormBuilder) do |answer_template|
-          result << answer_template.dynamic_question(form_elements_cache, element, @event, "", {:id => "investigator_answer_#{h(element.id)}"})
-          result << render_help_text(element) unless question.help_text.blank?
+          result << answer_template_dynamic_question(answer_template, form_elements_cache, question_element, "", question)
         end
       end
     else
       prefix = @answer_object.new_record? ? "new_answers" : "answers"
       index = @answer_object.new_record? ? "" : @form_index += 1
       f.fields_for(prefix, @answer_object, :builder => ExtendedFormBuilder) do |answer_template|
-        result << answer_template.dynamic_question(form_elements_cache, element, @event, index, {:id => "investigator_answer_#{h(element.id)}"})
-        result << render_help_text(element) unless question.help_text.blank?
+        result << answer_template_dynamic_question(answer_template, form_elements_cache, element, index, question)
       end
     end
 
@@ -367,6 +365,13 @@ module FormBuilderDslHelper
     #rescue
     #logger.warn("Formbuilder rendering: #{$!.message}")
     #return "Could not render question element (#{element.id})"
+  end
+
+  def answer_template_dynamic_question(answer_template, form_elements_cache, element, index, question)
+    result = ""
+    result << answer_template.dynamic_question(form_elements_cache, element, @event, index, {:id => "investigator_answer_#{h(element.id)}"})
+    result << render_help_text(element) unless question.help_text.blank?
+    result
   end
 
   def render_investigator_follow_up(form_elements_cache, element, f)

webapp/spec/helpers/dynamic_question_builder_spec.rb
@@ -1,0 +1,81 @@
+require File.expand_path("../../../app/helpers/dynamic_question_builder", __FILE__)
+describe DynamicQuestionBuilder do
+  describe "#question_is_multi_valued_and_has_value_set?" do
+    let(:question_element) { stub :question_element, :question => question }
+    let(:form_elements_cache) { stub :form_elements_cache }
+    let(:builder) { DynamicQuestionBuilder.new(:question_element => question_element, :form_elements_cache => form_elements_cache) } 
+    let(:result) { builder.question_is_multi_valued_and_has_value_set? }
+    
+    context "question is not multi valued" do
+
+     let(:question) { stub :question, :is_multi_valued? => false }
+
+      it "return false" do
+        result.should be_false
+      end
+    end #not multi valued question
+
+    context "question is multi valued" do
+
+       let(:question) { stub :question, :is_multi_valued? => true }
+ 
+
+       context "question element has children" do
+
+         before do
+       
+           # TODO: form_elements_cache.has_children_for?(question_element)
+           form_elements_cache.stub!(:has_children_for?).with(question_element).and_return(true)
+ 
+         end
+
+         context "question element has value set" do
+
+           before do
+             # TODO: form_elements_cache.has_children_for?(question_element)
+             form_elements_cache.stub!(:has_value_set_for?).with(question_element).and_return(true)
+           end 
+
+           it "returns true" do
+             result.should be_true
+           end
+
+
+         end
+
+         context "question element has no value set" do
+
+           before do
+             # TODO: form_elements_cache.has_children_for?(question_element)
+             form_elements_cache.stub!(:has_value_set_for?).with(question_element).and_return(false)
+           end 
+
+           it "returns false" do
+             result.should be_false
+           end
+
+
+         end
+         
+       end #has children
+
+       context "question element has no children" do
+
+         before do
+       
+           # TODO: form_elements_cache.has_children_for?(question_element)
+           form_elements_cache.stub!(:has_children_for?).with(question_element).and_return(false)
+ 
+         end
+
+         it "returns false" do
+
+           result.should be_false
+
+         end
+
+       end # no children 
+     
+    end #multi valued question
+  end
+end

webapp/spec/helpers/extended_form_builder_spec.rb
@@ -295,4 +295,5 @@ describe ExtendedFormBuilder::CorePath do
 
     it_should_behave_like "a complex core path"
   end
+
 end

commit 03af8eb83dafe996ffc2631dd40ecf4bc690b003

Fix has_value_set? bug introduced by e21edc06

webapp/lib/form_element_cache.rb
@@ -33,12 +33,13 @@ class FormElementCache
   end
 
   def has_children_for?(element)
-    children(element).empty?
+    !children(element).empty?
   end
 
   def has_value_set_for?(element)
     value_set = children_by_type("ValueSetElement", element).first
-    !value_set.nil? || !form_elements_cache.children(value_set).empty?
+    return false if value_set.nil?
+    has_children_for?(value_set)
   end
   
   private

webapp/spec/lib/form_element_cache_spec.rb
@@ -52,6 +52,14 @@ describe FormElementCache do
     @question_3 = Question.create(:question_text => "?", :data_type => "single_line_text", :short_name => "q")
     @question_element_3 = QuestionElement.create(:tree_id => tree_id, :form_id => 1, :question => @question_3)
     @section_element.add_child(@question_element_3)
+
+    @question_4 = Question.create(:question_text => "Multi-Value", :data_type => "radio_button", :short_name => "multi")
+    @question_element_4 = QuestionElement.create(:tree_id => tree_id, :form_id => 1, :question => @question_4)
+    @value_set_4 = ValueSetElement.create(:name => "Val Set", :tree_id => tree_id)
+    @question_element_4.add_child(@value_set_4)
+    @value_4 = ValueElement.create(:tree_id => tree_id)
+    @value_set_4.add_child(@value_4)
+    @section_element.add_child(@question_element_4)
     
     @event = Event.new(:id => 1)
     @event.answers << @answer_1 = Answer.new(:event_id => 1, :question_id => @question.id, :text_answer => "What?")
@@ -99,7 +107,7 @@ describe FormElementCache do
     @form_element_cache = FormElementCache.new(@form_base_element)
     children = @form_element_cache.all_children(@form_base_element)
     children.is_a?(Array).should be_true
-    children.size.should == 9
+    children.size.should == 12
   end
   
   it "should return all follow ups by core path" do
@@ -137,7 +145,19 @@ describe FormElementCache do
 
   it "should return questions with shortnames as exportable" do
     @form_element_cache = FormElementCache.new(@form_base_element)
-    @form_element_cache.exportable_questions.size.should eql(4)
+    @form_element_cache.exportable_questions.size.should eql(5)
+  end
+ 
+  describe "#has_children_for?" do
+    it "returns true when children are found" do
+      @form_element_cache.has_children_for?(@form_base_element).should be_true
+    end
+  end
+  
+  describe "#has_value_set_for?" do
+    it "returns true when values are found" do
+      @form_element_cache.has_value_set_for?(@question_element_4).should be_true
+    end
   end
   
 end