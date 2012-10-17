9ea481ec5cd9d21cd52ceaa0528d52744508727e
Brian Buchalter <brianb@endpoint.com>
2012-10-17 14:28:32 -0400

    Fix dynamic question spec to work with full test suite

--- webapp/spec/helpers/dynamic_question_builder_spec.rb
+++ webapp/spec/helpers/dynamic_question_builder_spec.rb
@@ -1,15 +1,14 @@
 require File.expand_path("../../../app/helpers/dynamic_question_builder", __FILE__)
+require 'mocha'
 describe DynamicQuestionBuilder do
   describe "#question_is_multi_valued_and_has_value_set?" do
-    let(:question_element) { stub :question_element, :question => question }
-    let(:form_elements_cache) { stub :form_elements_cache }
-    let(:builder) { DynamicQuestionBuilder.new(:question_element => question_element, :form_elements_cache => form_elements_cache) } 
-    let(:result) { builder.question_is_multi_valued_and_has_no_value_set? }
+     let(:question_element) { stub(:question => question) }
+     let(:form_elements_cache) { stub }
+     let(:builder) { DynamicQuestionBuilder.new(:question_element => question_element, :form_elements_cache => form_elements_cache) }
+     let(:result) { builder.question_is_multi_valued_and_has_no_value_set? }
     
     context "question is not multi valued" do
-
-     let(:question) { stub :question, :is_multi_valued? => false }
-
+      let(:question) { stub(:is_multi_valued? => false) }
       it "return false" do
         result.should be_false
       end
@@ -17,65 +16,47 @@ describe DynamicQuestionBuilder do
 
     context "question is multi valued" do
 
-       let(:question) { stub :question, :is_multi_valued? => true }
+       let(:question) { stub(:is_multi_valued? => true) }
  
 
        context "question element has children" do
-
          before do
-       
-           # TODO: form_elements_cache.has_children_for?(question_element)
-           form_elements_cache.stub!(:has_children_for?).with(question_element).and_return(true)
- 
+           form_elements_cache.stubs(:has_children_for?).with(question_element).returns(true)
          end
 
+
          context "question element has value set" do
-
            before do
-             # TODO: form_elements_cache.has_children_for?(question_element)
-             form_elements_cache.stub!(:has_value_set_for?).with(question_element).and_return(true)
+             form_elements_cache.stubs(:has_value_set_for?).with(question_element).returns(true)
            end 
-
            it "returns false" do
              result.should be_false
            end
-
-
          end
 
+
          context "question element has no value set" do
-
            before do
-             # TODO: form_elements_cache.has_children_for?(question_element)
-             form_elements_cache.stub!(:has_value_set_for?).with(question_element).and_return(false)
+             form_elements_cache.stubs(:has_value_set_for?).with(question_element).returns(false)
            end 
-
            it "returns true" do
              result.should be_true
            end
-
-
          end
-         
        end #has children
 
+
+
+
        context "question element has no children" do
-
          before do
-       
-           # TODO: form_elements_cache.has_children_for?(question_element)
-           form_elements_cache.stub!(:has_value_set_for?).with(question_element).and_return(false)
- 
+           form_elements_cache.stubs(:has_value_set_for?).with(question_element).returns(false)
          end
-
          it "returns true" do
-
            result.should be_true
-
          end
 
        end # no children 
-     
     end #multi valued question
   end
 end
