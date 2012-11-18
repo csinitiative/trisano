csinitiative/trisano

commit 0ff309fbeef72f6cd66e5f8d44c4567d4688830b

Revise help text features to ignore repeaters

webapp/features/shared_step_definitions/event_steps.rb
@@ -283,17 +283,20 @@ Given /^all core fields have help text$/ do
   ActiveRecord::Base.connection.execute(<<-SQL)
     UPDATE core_field_translations a
     SET help_text = (
-      SELECT key || ' help text' FROM core_fields b
-       WHERE a.core_field_id = b.id)
+      SELECT key || ' help text' WHERE a.core_field_id = b.id)
+    FROM core_fields b
     WHERE locale = '#{I18n.locale}'
   SQL
 end
 
 Given /^all core field configs for a (.+) have help text$/ do |event_type|
-  CoreField.find_all_by_event_type(event_type.gsub(" ", "_")).each do |core_field|
-    core_field.help_text = core_field.key << " help text"
-    core_field.save!
-  end
+  ActiveRecord::Base.connection.execute(<<-SQL)
+    UPDATE core_field_translations a
+    SET help_text = (
+      SELECT key || ' help text' WHERE a.core_field_id = b.id)
+    FROM core_fields b
+    WHERE a.locale = '#{I18n.locale}' AND b.event_type = '#{event_type}'
+  SQL
 end
 
 Given /^the event has a lab$/i do

webapp/features/enhanced_step_definitions/core_field_help_steps.rb
@@ -22,9 +22,10 @@ When /^I fill in enough (.+) data to enable all core fields to show up in show m
   @browser.type("#{event_type}[address_attributes][street_number]", "12")
 
   # Lab name and type for contact and morbidity events
-  if event_type == "morbidity_event" || event_type == "contact_event"
-    common_test_type = CommonTestType.first.nil? ? CommonTestType.create(:common_name => "Common Test Type") : CommonTestType.first
+  if event_type == "morbidity_event" || event_type == "contact_event" || event_type == "assessment_event"
+    common_test_type = CommonTestType.find_or_create_by_common_name("Common Test Type")
     add_lab_result(@browser, { :lab_name => "Labby", :lab_test_type => common_test_type.common_name })
+    add_hospital(@browser, {:name => "Allen Memorial Hospital"}, index = 1)
   end
 end
 
@@ -32,7 +33,7 @@ Then /^I should see help text for all (.+) core fields in (.+) mode$/ do |event_
   html_source = @browser.get_html_source
 
   @core_fields ||= CoreField.all(:conditions => [<<-SQL, event_type.gsub(" ", "_"), false, %w(section tab event)])
-    event_type = ? and disease_specific = ? and field_type NOT IN (?)
+    event_type = ? and disease_specific = ? and field_type NOT IN (?) and repeater = FALSE
   SQL
   @core_fields.each do |core_field|
     # Ignore lab result fields in show mode

commit 30dd3e308ae2fdc24865ac4c95d72814c1488a79

Fix regression caused by 0ff309fb
This change caused vendor/trisano/trisano_outbreaks/features/standard/outbreak_core_fields_help.feature:6
to fail. This seems to be releated to the memoization of help text features.
It only affected outbreak events.

webapp/features/shared_step_definitions/event_steps.rb
@@ -289,12 +289,10 @@ Given /^all core fields have help text$/ do
 end
 
 Given /^all core field configs for a (.+) have help text$/ do |event_type|
-  ActiveRecord::Base.connection.execute(<<-SQL)
-    UPDATE core_field_translations a
-    SET help_text = (b.key || ' help text')
-    FROM core_fields b
-    WHERE a.locale = '#{I18n.locale}' AND b.event_type = '#{event_type.gsub(" ","_")}' AND a.core_field_id = b.id
-  SQL
+  CoreField.find_all_by_event_type(event_type.gsub(" ", "_")).each do |core_field|
+    core_field.help_text = core_field.key << " help text"
+    core_field.save!
+  end
 end
 
 Given /^the event has a lab$/i do