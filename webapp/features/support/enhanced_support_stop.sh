275d8f786f32856c6e8126709489ddbe38e2a0d5
Brian Buchalter <brianb@endpoint.com>
2012-11-26 13:11:08 -0400

    Don't kill websever when debugging

--- webapp/features/support/enhanced_support_stop.sh
+++ webapp/features/support/enhanced_support_stop.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+USE_DEBUGGER=$1
+
+if [ "$USE_DEBUGGER" = "true" ]
+then
+PIDS=`ps aux | grep -i "sel\|Xvfb" | grep -v grep | awk '{print $2}'`
+else
 PIDS=`ps aux | grep -i "-e feature -p 8080 -P\|sel\|Xvfb" | grep -v grep | awk '{print $2}'`
+fi
+
+
 echo $PIDS
 kill $PIDS
