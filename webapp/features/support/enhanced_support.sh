2d91cb91d19136b52154a3922c4c8ad6ff8a1624
Brian Buchalter <brianb@endpoint.com>
2012-11-27 07:08:52 -0400

    Revise enhanced support scripts for easier debugging

--- webapp/features/support/enhanced_support.sh
+++ webapp/features/support/enhanced_support.sh
@@ -3,12 +3,7 @@
 SHOW_FIREFOX=$1
 USE_DEBUGGER=$2
 
-if [ "$USE_DEBUGGER" = "true" ]
-then
-features/support/enhanced_support_stop.sh true
-else
 features/support/enhanced_support_stop.sh 
-fi
 
 Xvfb :99 -ac -extension GLX > log/xvfb.log 2>&1 &
 
@@ -23,8 +18,7 @@ fi
 
 if [ "$USE_DEBUGGER" = "true" ]
 then
-#bundle exec script/server -e feature -p 8080 -P /trisano --debugger &
-echo "Please start bundle exec script/server -e feature -p 8080 -P /trisano --debugger in another window"
+  bundle exec script/server -e feature -p 8080 -P /trisano --debugger -d 
 else
-bundle exec script/server -e feature -p 8080 -P /trisano > log/selenium_server.log &
+  bundle exec script/server -e feature -p 8080 -P /trisano -d
 fi
--- webapp/features/support/enhanced_support_stop.sh
+++ webapp/features/support/enhanced_support_stop.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-USE_DEBUGGER=$1
-
-if [ "$USE_DEBUGGER" = "true" ]
-then
-PIDS=`ps aux | grep -i "sel\|Xvfb" | grep -v grep | awk '{print $2}'`
-else
-PIDS=`ps aux | grep -i "-e feature -p 8080 -P\|sel\|Xvfb" | grep -v grep | awk '{print $2}'`
-fi
-
-
+echo "Killing web server, selenium, Xvfb, Firefox"
+PIDS=`ps aux | grep -i "-e feature -p 8080 -P /trisano\|sel\|Xvfb\|firefox-36/firefox-bin" | grep -v grep | awk '{print $2}'`
 echo $PIDS
 kill $PIDS
