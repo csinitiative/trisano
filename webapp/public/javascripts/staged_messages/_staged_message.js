e1675143bbe84425b28df9fb79fbef8cab9e7a41
Marina Lohova <marina@endpoint.com>
2012-11-29 13:01:56 -0800

    PT 40238697 Modify current Raw option in Staging Area to display HL7 message in pop-up window

--- webapp/public/javascripts/staged_messages/_staged_message.js
+++ webapp/public/javascripts/staged_messages/_staged_message.js
@@ -1,6 +1,19 @@
 $j(function(){
   $j('.staged-message-discard-link').click(function(){
-    var staged_message = $j(this).closest('.staged-message').fadeTo('fast', 0.4)
+    var staged_message = $j(this).closest('.staged-message').fadeTo('fast', 0.4);
     $j('.staged-message-link', staged_message).removeAttr('href');
+  });
+  var dialog;
+  if (dialog == undefined) {
+    var newDiv = $j(document.createElement('div'));
+    newDiv.attr('class','hl7-dialog');
+    newDiv.attr('style', 'display:none');
+    dialog = newDiv.dialog({title:"HL7 Message", width:750, height: 250, autoOpen: false});
+  }
+  $j(".hl7-link").click(function(e) {
+      dialog.html('<textarea>'+ $j(this).attr("data") +'</textarea>');
+      dialog.dialog('open');
+      e.stopPropagation();
+      e.preventDefault();
   });
 });
