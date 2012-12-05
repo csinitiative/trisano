61189473515715ca4b71bf9a4c026f0454973763
Marina Lohova <marina@endpoint.com>
2012-12-04 16:17:40 -0800

    IE8 Fix: PT 40238697 Modify current Raw option in Staging Area to display HL7 message in pop-up window

--- webapp/public/javascripts/staged_messages/_staged_message.js
+++ webapp/public/javascripts/staged_messages/_staged_message.js
@@ -3,16 +3,11 @@ $j(function(){
     var staged_message = $j(this).closest('.staged-message').fadeTo('fast', 0.4);
     $j('.staged-message-link', staged_message).removeAttr('href');
   });
-  var dialog;
-  if (dialog == undefined) {
-    var newDiv = $j(document.createElement('div'));
-    newDiv.attr('class','hl7-dialog');
-    newDiv.attr('style', 'display:none');
-    dialog = newDiv.dialog({title:"HL7 Message", width:750, height: 250, autoOpen: false});
-  }
   $j(".hl7-link").click(function(e) {
-      dialog.html('<textarea>'+ $j(this).attr("data") +'</textarea>');
-      dialog.dialog('open');
+      var title = $j(this).parents(".staged-message").find(".header").text().strip();
+      var msg = $j(this).attr("data");
+      var w = window.open('', '_blank','width=750,height=300,resizable=yes,directories=0,titlebar=0,scrollbars=1,toolbar=0,location=0,menubar=0');
+      w.document.write("<html><body><head><title>" + title + "</title></head>" + msg + "</body></html>");
       e.stopPropagation();
       e.preventDefault();
   });
