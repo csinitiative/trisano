47495b472c4ef230809d4bf88580a2671f515f0c
Brian Buchalter <brianb@endpoint.com>
2012-12-05 15:09:33 -0400

    Remove unneeded object initialization for repeaters

--- webapp/app/controllers/assessment_events_controller.rb
+++ webapp/app/controllers/assessment_events_controller.rb
@@ -77,10 +77,6 @@ class AssessmentEventsController < EventsController
           [@event, @event.contact_child_events].flatten.all? { |event| event.set_primary_entity_on_secondary_participations }
           @event.add_note(@event.instance_eval(@event.states(@event.state).meta[:note_text]))
         end
-        @event.hospitalization_facilities.each do |hospital|
-          hospital.create_hospitals_participation unless hospital.hospitals_participation
-        end
-        @event.reload
         @event.create_form_answers_for_repeating_form_elements
         @event.try(:address).try(:establish_canonical_address)
         flash[:notice] = t("ae_created")
