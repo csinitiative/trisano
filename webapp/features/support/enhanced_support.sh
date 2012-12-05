600f9e7d53f4442ba014af25464a6346e0e252d0
Brian Buchalter <brianb@endpoint.com>
2012-12-05 11:22:09 -0400

    Use single arg for debugging enhanced features

--- webapp/features/support/enhanced_support.sh
+++ webapp/features/support/enhanced_support.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-SHOW_FIREFOX=$1
-USE_DEBUGGER=$2
+USE_DEBUGGER=$1
 
 features/support/enhanced_support_stop.sh 
 
 Xvfb :99 -ac -extension GLX > log/xvfb.log 2>&1 &
 
-if [ "$SHOW_FIREFOX" = "true" ]
+if [ "$USE_DEBUGGER" = "debug" ]
 then
 echo "Loading selenium on 4444, showing Firefox"
 bundle exec selenium -port 4444 -firefoxProfileTemplate './features/support/firefox-36-profile' > log/selenium_java.log 2>&1 &
@@ -16,7 +15,7 @@ echo "Loading selenium on 4444, hiding Firefox"
 DISPLAY=:99 bundle exec selenium -port 4444 > log/selenium_java.log 2>&1 &
 fi
 
-if [ "$USE_DEBUGGER" = "true" ]
+if [ "$USE_DEBUGGER" = "debug" ]
 then
   echo "Loading web server on 8080 with debugger"
   bundle exec script/server -e feature -p 8080 -P /trisano --debugger -d 
