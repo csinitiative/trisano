bad6b657da0891085bad2c92848b6ba51a878d44
Marina Lohova <marina@endpoint.com>
2012-12-16 14:49:59 -0800

    ZD 11181 Changes to Electronic lab report to accommodate changing data

--- webapp/public/javascripts/staged_messages/_staged_message.js
+++ webapp/public/javascripts/staged_messages/_staged_message.js
@@ -3,12 +3,4 @@ $j(function(){
     var staged_message = $j(this).closest('.staged-message').fadeTo('fast', 0.4);
     $j('.staged-message-link', staged_message).removeAttr('href');
   });
-  $j(".hl7-link").click(function(e) {
-      var title = $j(this).parents(".staged-message").find(".header").text().strip();
-      var msg = $j(this).attr("data");
-      var w = window.open('', '_blank','width=750,height=300,resizable=yes,directories=0,titlebar=0,scrollbars=1,toolbar=0,location=0,menubar=0');
-      w.document.write("<html><body><head><title>" + title + "</title></head>" + msg + "</body></html>");
-      e.stopPropagation();
-      e.preventDefault();
-  });
 });
