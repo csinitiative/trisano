9d795c1ceca9839062335a316d250c7ddd65ef71
Brian Buchalter <brianb@endpoint.com>
2012-12-26 22:20:23 -0400

    Update full test suite script

--- webapp/script/enhanced_support_stop.sh
+++ webapp/script/enhanced_support_stop.sh
@@ -12,6 +12,11 @@ function kill_pids {
   fi
 }
 
+echo -n "Killing consoles: "
+PIDS=`ps aux | grep -i "sh -c irb" | grep -v grep | awk '{print $2}'`
+kill_pids $PIDS
+PIDS=`ps aux | grep -i "irb" | grep -v grep | awk '{print $2}'`
+kill_pids $PIDS 
 
 echo -n "Killing web server on 8080: "
 PIDS=`ps aux | grep -i "-e feature -p 8080 -P /trisano" | grep -v grep | awk '{print $2}'`
