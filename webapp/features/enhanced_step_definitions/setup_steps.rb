e6af9f28a7af870f152cd286d44bc464c114775b
Brian Buchalter <brianb@endpoint.com>
2013-01-08 16:46:05 -0400

    Update TriSano license on all Ruby and JavaScript files
    Also organize JavaScript into vendor / trisano folders.
    Includes library to help with future license management.

--- webapp/features/enhanced_step_definitions/setup_steps.rb
+++ webapp/features/enhanced_step_definitions/setup_steps.rb
@@ -1,3 +1,19 @@
+# Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, 2013 The Collaborative Software Foundation
+#
+# This file is part of TriSano.
+#
+# TriSano is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# TriSano is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with TriSano. If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
 Given /^I have real world data$/ do
   sql_data_file_path = File.join(RAILS_ROOT,"features","support","kdhe_prod_20120629.obfu.sql")
   raise "Real world data not found at #{sql_data_file_path}" unless File.exist?(sql_data_file_path)
--- webapp/public/javascripts/staged_messages/_staged_message.js
+++
@@ -1,6 +0,0 @@
-$j(function(){
-  $j('.staged-message-discard-link').click(function(){
-    var staged_message = $j(this).closest('.staged-message').fadeTo('fast', 0.4)
-    $j('.staged-message-link', staged_message).removeAttr('href');
-  });
-});
